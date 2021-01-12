2eeacf9a4baa81741c6d45e4bfeb68376d61b8e2
fg <fg@mindee.co>
2021-01-12 15:47:25 +0100

    test: Added unittest to check compression order in terms of size

--- test/test_models.py
+++ test/test_models.py
@@ -1,3 +1,5 @@
+import pytest
+
 from doctr import models
 
 import sys
@@ -5,7 +7,8 @@ from tensorflow.keras import layers
 from tensorflow.keras.models import Sequential
 
 
-def _mock_model():
+@pytest.fixture(scope="module")
+def mock_model():
     _layers = [
         layers.Conv2D(8, 3, activation='relu', padding='same', input_shape=(224, 224, 3)),
         layers.GlobalAveragePooling2D(),
@@ -15,25 +18,27 @@ def _mock_model():
     return Sequential(_layers)
 
 
-def test_convert_to_tflite():
-    # Define a mock model
-    mock_model = _mock_model()
-
-    tflite_model = models.utils.convert_to_tflite(mock_model)
-    assert isinstance(tflite_model, bytes)
-
-
-def test_convert_to_fp16():
-    # Define a mock model
-    mock_model = _mock_model()
-
-    fp16_model = models.utils.convert_to_fp16(mock_model)
-    assert isinstance(fp16_model, bytes)
+@pytest.fixture(scope="module")
+def test_convert_to_tflite(mock_model):
+    serialized_model = models.utils.convert_to_tflite(mock_model)
+    assert isinstance(serialized_model, bytes)
+    return serialized_model
 
 
-def test_quantize_model():
-    # Define a mock model
-    mock_model = _mock_model()
+@pytest.fixture(scope="module")
+def test_convert_to_fp16(mock_model):
+    serialized_model = models.utils.convert_to_fp16(mock_model)
+    assert isinstance(serialized_model, bytes)
+    return serialized_model
 
-    int_model = models.utils.quantize_model(mock_model, (224, 224, 3))
-    assert isinstance(int_model, bytes)
+
+@pytest.fixture(scope="module")
+def test_quantize_model(mock_model):
+    serialized_model = models.utils.quantize_model(mock_model, (224, 224, 3))
+    assert isinstance(serialized_model, bytes)
+    return serialized_model
+
+
+def test_export_sizes(test_convert_to_tflite, test_convert_to_fp16, test_quantize_model):
+    assert sys.getsizeof(test_convert_to_tflite) > sys.getsizeof(test_convert_to_fp16)
+    assert sys.getsizeof(test_convert_to_fp16) > sys.getsizeof(test_quantize_model)
