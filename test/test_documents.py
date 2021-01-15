3db484cf5a27a711416b62a66af1da9cae60570b
charlesmindee <charles@mindee.co>
2021-01-15 12:54:19 +0100

    refacto: :recycle: test documents whithout bytes

--- test/test_documents.py
+++ test/test_documents.py
@@ -1,5 +1,6 @@
 import requests
 import pytest
+import numpy as np
 from io import BytesIO
 
 from doctr import documents
@@ -19,16 +20,29 @@ def mock_pdf(tmpdir_factory):
 
 
 def test_pdf_reader_with_pix(mock_pdf, num_pixels=2000000):
-    shapes, raw_images, documents_names = documents.reader.read_documents(
+    documents_imgs, documents_names, documents_shapes = documents.reader.read_documents(
         filepaths=[mock_pdf],
         num_pixels=num_pixels)
-    for doc_shapes, doc_images, doc_names in zip(shapes, raw_images, documents_names):
-        for shape, raw_image, document_name in zip(doc_shapes, doc_images, doc_names):
+    for doc_shapes, doc_images, doc_names in zip(documents_shapes, documents_img, documents_names):
+        for shape, image, document_name in zip(doc_shapes, doc_images, doc_names):
             assert isinstance(shape, tuple)
             assert isinstance(document_name, str)
-            assert isinstance(raw_image, bytes)
+            assert isinstance(image, np.ndarray)
             assert shape[0] * shape[1] <= 1.003 * num_pixels
             assert shape[0] * shape[1] >= 0.997 * num_pixels
+
+
+def test_pdf_reader(mock_pdf):
+    documents_imgs, documents_names, documents_shapes = documents.reader.read_documents(
+        filepaths=[mock_pdf],
+        num_pixels=None)
+    for doc_shapes, doc_images, doc_names in zip(documents_shapes, documents_img, documents_names):
+        for shape, image, document_name in zip(doc_shapes, doc_images, doc_names):
+            assert isinstance(shape, tuple)
+            assert isinstance(document_name, str)
+            assert isinstance(image, np.ndarray)
+            assert shape[0] * shape[1] <= DEFAULT_RES_MAX
+            assert shape[0] * shape[1] >= DEFAULT_RES_MIN
 
 
 def test_pdf_reader(mock_pdf):
