fa90738dfc611482b9f63d4ff98304c931ec9623
fg <fg@mindee.co>
2021-01-19 17:49:01 +0100

    test: Reflected changes on preprocessor

--- test/test_models.py
+++ test/test_models.py
@@ -1,15 +1,14 @@
 import pytest
-import requests
 from io import BytesIO
-
-from doctr import models
-
 import sys
+import math
+import requests
+import numpy as np
 from tensorflow.keras import layers
 from tensorflow.keras.models import Sequential
 
-from doctr.models.preprocessor import Preprocessor
-from doctr import documents
+from doctr import models
+from doctr.documents import read_pdf
 from test_documents import mock_pdf
 
 
@@ -50,12 +49,21 @@ def test_export_sizes(test_convert_to_tflite, test_convert_to_fp16, test_quantiz
     assert sys.getsizeof(test_convert_to_fp16) > sys.getsizeof(test_quantize_model)
 
 
-def test_preprocess_documents(mock_pdf, num_docs=10, batch_size=3):  # noqa: F811
-    docs = documents.reader.read_documents(
-        filepaths=[mock_pdf for _ in range(num_docs)])
-    preprocessor = Preprocessor(out_size=(600, 600), normalization=True, mode='symmetric', batch_size=batch_size)
-    batched_docs, docs_indexes, pages_indexes = preprocessor(docs)
-    assert len(docs_indexes) == len(pages_indexes)
-    assert docs_indexes[-1] + 1 == num_docs
-    if num_docs > batch_size:
-        assert all(len(batch) == batch_size for batches in batched_docs[:-1] for batch in batches)
+def test_preprocess_documents(mock_pdf):  # noqa: F811
+    num_docs = 3
+    batch_size = 4
+    docs = [read_pdf(mock_pdf) for _ in range(num_docs)]
+    processor = models.preprocessor.Preprocessor(output_size=(600, 600), batch_size=batch_size)
+    batched_docs = processor(docs)
+
+    # Number of batches
+    assert len(batched_docs) == math.ceil(8 * num_docs / batch_size)
+    # Total number of samples
+    assert sum(batch.shape[0] for batch in batched_docs) == 8 * num_docs
+    # Batch size
+    assert all(batch.shape[0] == batch_size for batch in batched_docs[:-1])
+    assert batched_docs[-1].shape[0] == batch_size if (8 * num_docs) % batch_size == 0 else (8 * num_docs) % batch_size
+    # Data type
+    assert all(batch.dtype == np.float32 for batch in batched_docs)
+    # Image size
+    assert all(batch.shape[1:] == (600, 600, 3) for batch in batched_docs)
