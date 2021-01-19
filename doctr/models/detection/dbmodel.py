75792ad99a90c27d4025832d3d614982bab929e5
charlesmindee <charles@mindee.co>
2021-01-19 16:59:23 +0100

    add: resnet18

--- doctr/models/detection/dbmodel.py
+++ doctr/models/detection/dbmodel.py
@@ -2,146 +2,189 @@
 # This program is licensed under the Apache License version 2.
 # See LICENSE or go to <https://www.apache.org/licenses/LICENSE-2.0.txt> for full license details.
 
-import numpy as np
 import tensorflow as tf
 from tensorflow import keras
 from tensorflow.keras import layers
 
-
-def up_scale_addition(x_small, x_big):
-    x = layers.UpSampling2D(size=(2, 2),
-                        interpolation='nearest')(x_small)
-    x = layers.Add()([x, x_big])
-    return x
- 
-
-def conv_up(x, n:int, up:int):
-    x = layers.Conv2D(filters=n, 
-                    kernel_size=(3,3),
-                    strides=(1,1),
-                    padding='same')(x)
-    x = layers.BatchNormalization()(x)
-    x = layers.Activation('relu')(x)
-
-    if up > 0:
-        x = layers.UpSampling2D(size=(up, up),
-                        interpolation='nearest')(x)
-    return x 
+from typing import List, Union, Optional, Tuple, Dict
 
 
-def reduce_channel(feat_maps, n):
-    thin_feat_maps = [0, 0, 0, 0]
-    for i in range(len(feat_maps)):
-        thin_feat_maps[i] = layers.Conv2D(filters=n,
-                                kernel_size=(1, 1),
-                                strides=1)(feat_maps[i])
-    return thin_feat_maps
+class DBModel(DetectionModel, keras.Model):
+
+    def __init__(
+        self,
+        backbone: str = "resnet18",
+        shape: Tuple[int, int] = (600, 600),
+        channels: int = 128,
+    ) -> None:
+        """
+        Backbone : choice between a light (resnet18) backbone and a heavy (resnet30) backbone
+        """
+
+        self.backbone = backbone
+        self.shape = shape
+        self.channels = channels
+
+    def upsampling_addition(
+        self,
+        x_small: tf.Tensor,
+        x_big: tf.Tensor
+    ) -> tf.Tensor:
+        """
+        Performs Upsampling x2 on x_small and element-wise addition x_small + x_big
+        """
+
+        x = layers.UpSampling2D(size=(2, 2), interpolation='nearest')(x_small)
+        x = layers.Add()([x, x_big])
+        return x
+
+    def conv_upsampling(
+        self,
+        up: int = 0,
+    ) -> layers.Layer:
+        """
+        Module which performs a 3x3 convolution followed by up-sampling
+        up: dilatation factor to scale the convolution output before concatenation
+        """
+
+        model = keras.Sequential(
+            [
+                layers.Conv2D(filters=self.channels, kernel_size=(3,3), strides=(1,1), padding='same'),
+                layers.BatchNormalization(),
+                layers.Activation('relu'),
+            ]
+        )
+        if up > 0:
+            model.add(layers.UpSampling2D(size=(up, up), interpolation='nearest'))
+
+        return model
+
+    def reduce_channel(
+        self,
+        feat_maps: List[tf.Tensor],
+    ) -> List[tf.Tensor]:
+        """
+        Set channels for all tensors of the feat_maps list to n, performing a 1x1 conv
+        """
+
+        new_feat_maps = [0, 0, 0, 0]
+        for i in range(len(feat_maps)):
+            new_feat_maps[i] = layers.Conv2D(filters=self.channels, kernel_size=(1, 1), strides=1)(feat_maps[i])
+
+        return new_feat_maps
 
 
-def pyramid_module(x, n:int):
+    def pyramid_module(
+        self,
+        x: List[tf.Tensor],
+    ) -> tf.Tensor:
+        """
+        Implements Pyramidal module as described in paper, 
+        input: List of features maps (from resnet backbone)
+        output: concatenated features
+        """
 
-    x1, x2, x3, x4 = x[0], x[1], x[2], x[3]
+        x1, x2, x3, x4 = x[0], x[1], x[2], x[3]
 
-    y1 = up_scale_addition(x4, x3)
-    y2 = up_scale_addition(y1, x2)
-    y3 = up_scale_addition(y2, x1)
+        y1 = self.upsampling_addition(x4, x3)
+        y2 = self.upsampling_addition(y1, x2)
+        y3 = self.upsampling_addition(y2, x1)
 
-    z1 = conv_up(y3, n, up=0)
-    z2 = conv_up(y2, n, up=2)
-    z3 = conv_up(y1, n, up=4)
-    z4 = conv_up(x4, n, up=8)
+        z1 = self.conv_upsampling(self.channels, up=0)(y3)
+        z2 = self.conv_upsampling(self.channels, up=2)(y2)
+        z3 = self.conv_upsampling(self.channels, up=4)(y1)
+        z4 = self.conv_upsampling(self.channels, up=8)(x4)
 
-    features_concat = layers.Concatenate()([z1, z2, z3, z4])
+        features_concat = layers.Concatenate()([z1, z2, z3, z4])
 
-    return features_concat
-
-
-def get_p_map(features):
-    """
-
-    get probability or treshold map function from features
-
-    """
-    p = layers.Conv2D(filters=64,
-                    kernel_size=(3, 3),
-                    padding='same',
-                    use_bias=False, name="p_map1")(features)
-    p = layers.BatchNormalization(name="p_map2")(p)
-    p = layers.Activation('relu')(p)
-
-    p = layers.Conv2DTranspose(filters=64,
-                            kernel_size=(2, 2),
-                            strides=(2, 2),
-                            use_bias=False, name="p_map3")(p)
-    p = layers.BatchNormalization(name="p_map4")(p)
-    p = layers.Activation('relu')(p)
-
-    p = layers.Conv2DTranspose(filters=1,
-                            kernel_size=(2, 2), 
-                            strides=(2, 2), name="p_map5")(p)
-    p = layers.Activation('sigmoid')(p)
-
-    return p
- 
-def get_t_map(features):
-    """
-
-    get probability or treshold map function from features
-
-    """
-    t = layers.Conv2D(filters=64,
-                    kernel_size=(3, 3),
-                    padding='same',
-                    use_bias=False, name="t_map1")(features)
-    t = layers.BatchNormalization(name="t_map2")(t)
-    t = layers.Activation('relu')(t)
-
-    t = layers.Conv2DTranspose(filters=64,
-                            kernel_size=(2, 2),
-                            strides=(2, 2),
-                            use_bias=False, name="t_map3")(t)
-    t = layers.BatchNormalization(name="t_map4")(t)
-    t = layers.Activation('relu')(t)
-
-    t = layers.Conv2DTranspose(filters=1,
-                            kernel_size=(2, 2), 
-                            strides=(2, 2), name="t_map5")(t)
-    t = layers.Activation('sigmoid')(t)
-
-    return t
+        return features_concat
 
 
-def get_approximate_binary_map(p, t):
-    b_hat = layers.Lambda(lambda x: 1 / (1 + tf.exp(-50. * (x[0] - x[1]))), name="approx_bin_map")([p, t])
-    return b_hat
+    def get_p_map(
+        self,
+    ) -> layers.Layer:
+        """
+        get probability or treshold map function from features
+        """
 
-   
-def build_dbnet(img_h, img_w, n:int, backbone:str):
-    """
+        model = keras.Sequential(
+            [
+                layers.Conv2D(filters=64, kernel_size=(3, 3), padding='same', use_bias=False, name="p_map1"),
+                layers.BatchNormalization(name="p_map2"),
+                layers.Activation('relu'),
+                layers.Conv2DTranspose(filters=64, kernel_size=(2, 2), strides=(2, 2), use_bias=False, name="p_map3"),
+                layers.BatchNormalization(name="p_map4"),
+                layers.Activation('relu'),
+                layers.Conv2DTranspose(filters=1, kernel_size=(2, 2), strides=(2, 2), name="p_map5"),
+                layers.Activation('sigmoid'),
+            ]
+        )
 
-    build db net as described in https://arxiv.org/pdf/1911.08947.pdf 
-    backbone: choose between 'resnet18' (light) or resnet50 (heavy)
-
-    """
-    dbnet_input = keras.Input(shape=(img_h, img_w, 3,), name="img")
-
-    if backbone == 'resnet18':
-        resnet = build_resnet18(img_h, img_w)
+        return model
     
-    if backbone == 'resnet50':
-        resnet = build_resnet50(img_h, img_w)
+    def get_t_map(
+        self,
+    ) -> layers.Layer:
+        """
+        get treshold map function from features
+        """
 
-    features = resnet(dbnet_input)
-    thin_features = reduce_channel(features, n)
-    concat_features = pyramid_module(thin_features, n)
+        model = keras.Sequential(
+            [
+                layers.Conv2D(filters=64, kernel_size=(3, 3), padding='same', use_bias=False, name="t_map1"),
+                layers.BatchNormalization(name="t_map2"),
+                layers.Activation('relu'),
+                layers.Conv2DTranspose(filters=64, kernel_size=(2, 2), strides=(2, 2), use_bias=False, name="t_map3"),
+                layers.BatchNormalization(name="t_map4"),
+                layers.Activation('relu'),
+                layers.Conv2DTranspose(filters=1, kernel_size=(2, 2), strides=(2, 2), name="t_map5"),
+                layers.Activation('sigmoid'),
+            ]
+        )
+        return model
 
-    probability_map = get_p_map(concat_features)
-    treshold_map = get_t_map(concat_features)
 
-    approx_binary_map = get_approximate_binary_map(probability_map, treshold_map)
+    def get_approximate_binary_map(
+        self,
+        p: tf.Tensor,
+        t: tf.Tensor
+    ) -> tf.Tensor:
+        """
+        Compute approximate binary map as described in paper, from threshold map t and probability map p
+        """
 
-    dbnet_training = keras.Model(inputs=dbnet_input, outputs=[probability_map, treshold_map, approx_binary_map], name="dbnet_training")
-    dbnet_inference = keras.Model(inputs=dbnet_input, outputs=probability_map, name="dbnet_inference")
+        b_hat = layers.Lambda(lambda x: 1 / (1 + tf.exp(-50. * (x[0] - x[1]))), name="approx_bin_map")([p, t])
+        return b_hat
 
-    return dbnet_training, dbnet_inference
+    def __call__(
+        self,
+    ) -> Tuple[keras.Model, keras.Model]:
+        """
+        Returns a tuple of keras DB models : training and inference models
+        At inference time, we remove the threshold branch to fasten computation
+        """
+
+        db_input = keras.Input(shape=(self.shape[0], self.shape[1], 3,), name="img")
+
+        if self.backbone == 'resnet18':
+            resnet = build_resnet18(img_h, img_w)
+        
+        if self.backbone == 'resnet50':
+            resnet = build_resnet50(img_h, img_w)
+
+        features = resnet(dbnet_input)
+        reduced_channel_feat = self.reduce_channel(features)
+        concat_features = self.pyramid_module(reduce_channel_feat)
+
+        probability_map = self.get_p_map()(concat_features)
+        treshold_map = self.get_t_map()(concat_features)
+
+        approx_binary_map = self.get_approximate_binary_map(probability_map, treshold_map)
+
+        dbnet_training = keras.Model(
+            inputs=dbnet_input, outputs=[probability_map, treshold_map, approx_binary_map], name="dbnet_training"
+            )
+        dbnet_inference = keras.Model(inputs=dbnet_input, outputs=probability_map, name="dbnet_inference")
+
+        return dbnet_training, dbnet_inference
 
---
+++ doctr/models/detection/resnet18.py
@@ -0,0 +1,171 @@
+# Copyright (C) 2021, Mindee.
+# This program is licensed under the Apache License version 2.
+# See LICENSE or go to <https://www.apache.org/licenses/LICENSE-2.0.txt> for full license details.
+
+import tensorflow as tf
+from tensorflow import keras
+from tensorflow.keras import layers
+from typing import Tuple, List, Union, Optional, Dict
+
+
+class Resnet18(layers.Layer):
+
+    def __init__(
+        self,
+        shape: Tuple[int, int] = (600, 600),
+    ) -> None:
+
+        self.shape = shape
+
+    def __call__(
+        self,
+    ) -> layers.Layer:
+
+    def resnet_layer(
+        self,
+        channels: int,
+        k_size: int = 3,
+        strides: int
+    ) -> layers.Layer:
+
+        model = keras.Sequential(
+            [
+
+            ]
+        )
+        if strides > 1:
+            shortcut = layers.Conv2D(filters=n,
+                                    kernel_size=(1, 1),
+                                    strides=strides)(x)
+            shortcut = layers.BatchNormalization()(shortcut)
+        else:
+            shortcut = layers.Lambda(lambda x: x)(x)
+
+        model = keras.Sequential(
+            [
+
+            ]
+        )
+        layers.Conv2D(filters=n, kernel_size=k_size, strides=strides, padding="same", use_bias=False),
+        layers.BatchNormalization(),
+        layers.Activation('relu'),
+        layers.Conv2D(filters=n, kernel_size=k_size, strides=1, padding="same", use_bias=False),
+        layers.BatchNormalization(),
+        
+        x = layers.Activation('relu')(x + shortcut)
+        return x
+        
+
+    def resnet_bottleneck_layer(x, n, k_size, strides, force_shortcut=None):
+        if strides > 1 or force_shortcut:
+            shortcut = layers.Conv2D(filters=n,
+                                    kernel_size=(1, 1),
+                                    strides=strides)(x)
+        else:
+            shortcut = layers.Lambda(lambda x: x)(x)
+
+        x = layers.Conv2D(filters=n//4,
+                        kernel_size=1,
+                        strides=strides,
+                        padding="same",
+                        use_bias=False)(x)
+        x = layers.BatchNormalization()(x)
+        x = layers.Activation('relu')(x)
+
+        x = layers.Conv2D(filters=n//4,
+                        kernel_size=k_size,
+                        strides=1,
+                        padding="same",
+                        use_bias=False)(x)
+        x = layers.BatchNormalization()(x)
+        x = layers.Activation('relu')(x)
+
+        x = layers.Conv2D(filters=n,
+                        kernel_size=1,
+                        strides=1,
+                        padding="same",
+                        use_bias=False)(x)
+        x = layers.BatchNormalization()(x)
+        x = layers.Activation('relu')(x + shortcut)
+        return x
+
+
+
+    def resnet18_block(x, n):
+        x = resnet_layer(x, n, k_size=3, strides=2)
+        x = resnet_layer(x, n, k_size=3, strides=1)
+        return x
+
+    def first_block_resnet18(x, n):
+        x = resnet_layer(x, n, k_size=3, strides=1)
+        x = resnet_layer(x, n, k_size=3, strides=1)
+        return x
+
+    def resnet50_block(x, n, n_layers):
+        x = resnet_bottleneck_layer(x, n, k_size=3, strides=2)
+        for _ in range(1, n_layers):
+            x = resnet_bottleneck_layer(x, n, k_size=3, strides=1)
+        return x
+
+    def first_block_resnet50(x, n):
+        x = resnet_bottleneck_layer(x, n, k_size=3, strides=1, force_shortcut=True)
+        x = resnet_bottleneck_layer(x, n, k_size=3, strides=1)
+        x = resnet_bottleneck_layer(x, n, k_size=3, strides=1)
+        return x
+
+
+    def build_resnet18(img_h, img_w):
+        """
+
+        resnet18 backbone as described in https://arxiv.org/pdf/1512.03385.pdf 
+        outputs 4 features maps (conv_2, conv_3, conv_4, conv_5) for segmentation pyramidal module
+
+        """
+        resnet_input = keras.Input(shape=(img_h, img_w, 3,), name="img")
+
+        x = layers.Conv2D(filters=64,
+                        kernel_size=7,
+                        strides=2,
+                        padding="same",
+                        use_bias=False)(resnet_input)
+
+        x = layers.MaxPool2D(pool_size=(2,2),
+                            strides=(2,2))(x)
+
+        x1 = first_block_resnet18(x, 64)
+        x2 = resnet18_block(x1, 128)
+        x3 = resnet18_block(x2, 256)
+        x4 = resnet18_block(x3, 512)
+
+        resnet18 = keras.Model(resnet_input, [x1, x2, x3, x4], name="resnet18")
+
+        return resnet18
+
+
+    def build_resnet50(img_h, img_w):
+        """
+
+        resnet50 backbone as described in https://arxiv.org/pdf/1512.03385.pdf 
+        outputs 4 features maps (conv_2, conv_3, conv_4, conv_5) for segmentation pyramidal module
+
+        """
+        resnet_input = keras.Input(shape=(img_h, img_w, 3,), name="img")
+        
+        x = layers.Conv2D(filters=64,
+                        kernel_size=7,
+                        strides=2,
+                        padding="same",
+                        use_bias=False)(resnet_input)
+
+        x = layers.MaxPool2D(pool_size=(2,2),
+                            strides=(2,2))(x)
+
+        x1 = first_block_resnet50(x, 256)
+        x2 = resnet50_block(x1, 512, n_layers=4)
+        x3 = resnet50_block(x2, 1024, n_layers=6)
+        x4 = resnet50_block(x3, 2048, n_layers=3)
+
+        resnet50 = keras.Model(resnet_input, [x1, x2, x3, x4], name="resnet50")
+
+        return resnet50
+
