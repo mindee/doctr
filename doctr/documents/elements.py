fa3cd78f573856c5df5a6c37537c4d680b4a8141
fg <fg@mindee.co>
2021-01-20 14:04:42 +0100

    feat: Added Artefact elements

--- doctr/documents/elements.py
+++ doctr/documents/elements.py
@@ -7,28 +7,25 @@ from typing import Tuple, Dict, List, Any, Optional
 from doctr.utils.geometry import resolve_enclosing_bbox
 from doctr.utils.typing import BoundingBox, Polygon4P
 
-__all__ = ['Element', 'Word', 'Line', 'Block', 'Page', 'Document']
+__all__ = ['Element', 'Word', 'Artefact', 'Line', 'Block', 'Page', 'Document']
 
 
 class Element:
-    """Implements an abstract document element with exporting and text rendering capabilities
+    """Implements an abstract document element with exporting and text rendering capabilities"""
 
-    Args:
-        children: list of children elements
-    """
-
-    _children_name: str = "children"
+    _children_names: List[str] = []
     _exported_keys: List[str] = []
 
-    def __init__(self, children: List[Any] = []) -> None:
-        self.children = children
+    def __init__(self, **kwargs: Any) -> None:
+        for k, v in kwargs.items():
+            setattr(self, k, v)
 
     def export(self) -> Dict[str, Any]:
         """Exports the object into a nested dict format"""
 
         export_dict = {k: getattr(self, k) for k in self._exported_keys}
-        if any(self.children):
-            export_dict[self._children_name] = [c.export() for c in self.children]
+        for children_name in self._children_names:
+            export_dict[children_name] = [c.export() for c in getattr(self, children_name)]
 
         return export_dict
 
@@ -42,8 +39,8 @@ class Word(Element):
     Args:
         value: the text string of the word
         confidence: the confidence associated with the text prediction
-        geometry: bounding box of the word in format ((xmin, ymin), (xmax, ymax)) where coordinates
-        are relative to the page's size
+        geometry: bounding box of the word in format ((xmin, ymin), (xmax, ymax)) where coordinates are relative to
+        the page's size
     """
 
     _exported_keys: List[str] = ["value", "confidence", "geometry"]
@@ -59,17 +56,40 @@ class Word(Element):
         return self.value
 
 
+class Artefact(Element):
+    """Implements a non-textual element
+
+    Args:
+        artefact_type: the type of artefact
+        confidence: the confidence of the type prediction
+        geometry: bounding box of the word in format ((xmin, ymin), (xmax, ymax)) where coordinates are relative to
+            the page's size.
+    """
+
+    _exported_keys: List[str] = ["geometry", "type", "confidence"]
+
+    def __init__(self, artefact_type: str, confidence: float, geometry: BoundingBox) -> None:
+        super().__init__()
+        self.geometry = geometry
+        self.type = artefact_type
+        self.confidence = confidence
+
+    def render(self) -> str:
+        """Renders the full text of the element"""
+        return f"[{self.type.upper()}]"
+
+
 class Line(Element):
     """Implements a line element as a collection of words
 
     Args:
         words: list of word elements
-        geometry: bounding box of the word in format ((xmin, ymin), (xmax, ymax)) where coordinates
-        are relative to the page's size. If not specified, it will be resolved by default to the smallest bounding box
-        enclosing all words in it.
+        geometry: bounding box of the word in format ((xmin, ymin), (xmax, ymax)) where coordinates are relative to
+            the page's size. If not specified, it will be resolved by default to the smallest bounding box enclosing
+            all words in it.
     """
 
-    _children_name: str = "words"
+    _children_names: List[str] = ["words"]
     _exported_keys: List[str] = ["geometry"]
 
     def __init__(
@@ -81,41 +101,45 @@ class Line(Element):
         if geometry is None:
             geometry = resolve_enclosing_bbox([w.geometry for w in words])
 
-        super().__init__(words)
+        super().__init__(words=words)
         self.geometry = geometry
 
     def render(self) -> str:
         """Renders the full text of the element"""
-        return " ".join(w.render() for w in self.children)
+        return " ".join(w.render() for w in self.words)
 
 
 class Block(Element):
-    """Implements a block element as a collection of lines
+    """Implements a block element as a collection of lines and artefacts
 
     Args:
         lines: list of line elements
-        geometry: bounding box of the word in format ((xmin, ymin), (xmax, ymax)) where coordinates
-        are relative to the page's size. If not specified, it will be resolved by default to the smallest bounding box
-        enclosing all blocks in it.
+        artefacts: list of artefacts
+        geometry: bounding box of the word in format ((xmin, ymin), (xmax, ymax)) where coordinates are relative to
+            the page's size. If not specified, it will be resolved by default to the smallest bounding box enclosing
+            all lines and artefacts in it.
     """
 
-    _children_name: str = "lines"
+    _children_names: List[str] = ["lines", "artefacts"]
     _exported_keys: List[str] = ["geometry"]
 
     def __init__(
         self,
-        lines: List[Word],
+        lines: List[Word] = [],
+        artefacts: List[Artefact] = [],
         geometry: Optional[BoundingBox] = None,
     ) -> None:
         # Resolve the geometry using the smallest enclosing bounding box
         if geometry is None:
-            geometry = resolve_enclosing_bbox([word.geometry for line in lines for word in line.children])
-        super().__init__(lines)
+            line_boxes = [word.geometry for line in lines for word in line.words]
+            artefact_boxes = [artefact.geometry for artefact in artefacts]
+            geometry = resolve_enclosing_bbox(line_boxes + artefact_boxes)
+        super().__init__(lines=lines, artefacts=artefacts)
         self.geometry = geometry
 
     def render(self, line_break: str = '\n') -> str:
         """Renders the full text of the element"""
-        return line_break.join(line.render() for line in self.children)
+        return line_break.join(line.render() for line in self.lines)
 
 
 class Page(Element):
@@ -125,12 +149,11 @@ class Page(Element):
         blocks: list of block elements
         page_idx: the index of the page in the input raw document
         dimensions: the page size in pixels in format (width, height)
-        geometry: bounding box of the word in format ((xmin, ymin), (xmax, ymax)) where coordinates
-        are relative to the page's size. If not specified, it will be resolved by default to the smallest bounding box
-        enclosing all blocks in it.
+        orientation: a dictionary with the value of the rotation angle in degress and confidence of the prediction
+        language: a dictionary with the language value and confidence of the prediction
     """
 
-    _children_name: str = "blocks"
+    _children_names: List[str] = ["blocks"]
     _exported_keys: List[str] = ["page_idx", "dimensions", "orientation", "language"]
 
     def __init__(
@@ -141,7 +164,7 @@ class Page(Element):
         orientation: Dict[str, Any],
         language: Dict[str, Any],
     ) -> None:
-        super().__init__(blocks)
+        super().__init__(blocks=blocks)
         self.page_idx = page_idx
         self.dimensions = dimensions
         self.orientation = orientation
@@ -149,19 +172,24 @@ class Page(Element):
 
     def render(self, block_break: str = '\n\n') -> str:
         """Renders the full text of the element"""
-        return block_break.join(b.render() for b in self.children)
+        return block_break.join(b.render() for b in self.blocks)
 
 
 class Document(Element):
+    """Implements a document element as a collection of pages
 
-    _children_name: str = "pages"
+    Args:
+        pages: list of page elements
+    """
+
+    _children_names: List[str] = ["pages"]
 
     def __init__(
         self,
         pages: List[Page],
     ) -> None:
-        super().__init__(pages)
+        super().__init__(pages=pages)
 
     def render(self, page_break: str = '\n\n\n\n') -> str:
         """Renders the full text of the element"""
-        return page_break.join(p.render() for p in self.children)
+        return page_break.join(p.render() for p in self.pages)
