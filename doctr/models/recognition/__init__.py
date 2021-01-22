40fec07f821b41fcf8afab34385d4f00a53c6fbb
charlesmindee <charles@mindee.co>
2021-01-22 17:54:37 +0100

    feat: :sparkles: added ctc decoder fn

--- doctr/models/recognition/__init__.py
+++ doctr/models/recognition/__init__.py
@@ -1,3 +1,4 @@
 from .crnn import *
 from ._utils import *
 from .model import *
+from .postprocessor import *s
---
+++ doctr/models/recognition/postprocessor.py
@@ -0,0 +1,84 @@
+# Copyright (C) 2021, Mindee.
+
+# This program is licensed under the Apache License version 2.
+# See LICENSE or go to <https://www.apache.org/licenses/LICENSE-2.0.txt> for full license details.
+
+import tensorflow as tf
+import tensorflow.keras.backend as K
+from typing import Tuple, List, Union
+
+__all__ = ['ctc_decoder']
+
+
+def ctc_decoder(
+    logits: tf.Tensor,
+    num_classes: int
+    ) -> tf.Tensor:
+    """
+    Decode logits with CTC decoder from keras backend
+
+    Args:
+        logits: raw output of the model
+        num_classes: number of classes of the model
+
+    Returns:
+        decoded logits
+
+    """
+    batch_len = tf.cast(tf.shape(logits)[0], dtype="int32")
+    sequence_len = tf.cast(tf.shape(logits)[1], dtype="int32")    
+    
+    # computing prediction with ctc decoder
+    decoded_logits = K.ctc_decode(
+        tf.nn.softmax(logits), sequence_len * tf.ones(shape=(batch_len,), dtype="int32"), greedy=True
+    )
+    _predictions = tf.squeeze(decoded_logits[0])
+    
+    # masking -1 of CTC with num_classes (embedding <eos>)
+    pred_shape = tf.shape(_predictions)
+    mask_eos = num_classes * tf.ones(pred_shape, dtype="int32")
+    mask_1 = -1 * tf.ones(pred_shape, dtype="int32")
+    predictions = tf.where(mask_1 != _predictions, _predictions, mask_eos)
+
+    return predictions
+
+def postprocessor(
+    num_classes: int,
+    logits: tf.Tensor,
+    label_to_idx: dict,
+    ignore_case: bool = False,
+    ignore_accents: bool = False
+) -> tf.Tensor:
+    """
+    Postprocess raw prediction of the model (logits) to str
+    
+    Args:
+        num_classes: number of classes of the model
+        pred: raw output of the model
+        label_to_idx: dictionnary mapping alphabet labels to idx of the model classes 
+        ignore_case: if True, ignore case of letters
+        ignore_accents: if True, ignore accents of letters
+    
+    Returns:
+        a tensor of tf.strings predictions
+
+    """ 
+    # decode ctc for ctc models
+    predictions = decode_ctc_to_classes(logits, num_classes)
+
+    label_mapping = label_to_idx
+    label_to_idx['<eos>'] = num_classes
+    label, _ = zip(*sorted(label_to_idx.items(), key=lambda x: x[1]))
+    tf_label_to_idx = tf.constant(value=label, dtype=tf.string, shape=[num_classes + 1], name='dic_idx_label')
+
+    _decoded_strings_pred = tf.strings.reduce_join(inputs=tf.nn.embedding_lookup(tf_label_to_idx, predictions), axis=-1)
+    _decoded_strings_pred = tf.strings.split(_decoded_strings_pred, "<eos>")
+    decoded_strings_pred = tf.sparse.to_dense(_decoded_strings_pred.to_sparse(), default_value = 'not valid')[:, 0]
+    
+    if ignore_case: 
+        decoded_strings_pred = tf.strings.lower(decoded_strings_pred)
+    
+    if ignore_accents:
+        raise NotImplementedError
+
+    return decoded_strings_pred
