d77ecf67760234accadc01f3c26d915b9ce4ae71
fg <fg@mindee.co>
2021-01-26 19:06:07 +0100

    style: Fixed typing

--- doctr/models/detection/core.py
+++ doctr/models/detection/core.py
@@ -87,7 +87,8 @@ class DetectionPredictor:
     ) -> List[np.ndarray]:
 
         processed_batches = self.pre_processor(pages)
-        out = [self.model(tf.convert_to_tensor(batch)).numpy() for batch in processed_batches]
+        out = [self.model(tf.convert_to_tensor(batch)).numpy()  # type: ignore[union-attr]
+               for batch in processed_batches]
         out = [self.post_processor(batch) for batch in out]
         out = [boxes for batch in out for boxes in batch]
 
