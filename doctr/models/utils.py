0fabeda5c54c41c2d6cdca260b728f892a8cc1fa
fg-mindee <fg@mindee.co>
2021-01-28 12:06:16 +0100

    feat: Add models utils
    Split export and model building utils

--- doctr/models/utils.py
+++ doctr/models/utils.py
@@ -4,69 +4,103 @@
 # See LICENSE or go to <https://www.apache.org/licenses/LICENSE-2.0.txt> for full license details.
 
 import logging
-from pathlib import Path
-import numpy as np
+import re
+import os
+import hashlib
 import tensorflow as tf
-from typing import Tuple
+from tensorflow.keras import layers, Model
+from tensorflow.keras.utils import get_file
+from typing import Optional, List, Any
 
 logging.getLogger("tensorflow").setLevel(logging.DEBUG)
 
 
-__all__ = ['convert_to_tflite', 'convert_to_fp16', 'quantize_model']
+__all__ = ['load_pretrained_params', 'conv_sequence', 'IntermediateLayerGetter']
 
 
-def convert_to_tflite(tf_model: tf.keras.Model) -> bytes:
-    """Converts a model to TFLite format
-
-    Args:
-        tf_model: a keras model
-
-    Returns:
-        bytes: the model
-    """
-    converter = tf.lite.TFLiteConverter.from_keras_model(tf_model)
-    return converter.convert()
+# matches bfd8deac from resnet18-bfd8deac.ckpt
+HASH_REGEX = re.compile(r'-([a-f0-9]*)\.')
 
 
-def convert_to_fp16(tf_model: tf.keras.Model) -> bytes:
-    """Converts a model to half precision
+def load_pretrained_params(
+    model: Model,
+    url: Optional[str] = None,
+    hash_prefix: Optional[str] = None,
+) -> None:
+    """Load a set of parameters onto a model
 
     Args:
-        tf_model: a keras model
-
-    Returns:
-        bytes: the serialized FP16 model
+        model: the keras model to be loaded
+        url: URL of the set of parameters
+        hash_prefix: first characters of SHA256 expected hash
     """
-    converter = tf.lite.TFLiteConverter.from_keras_model(tf_model)
 
-    converter.optimizations = [tf.lite.Optimize.DEFAULT]
-    converter.target_spec.supported_types = [tf.float16]
-    return converter.convert()
+    if url is None:
+        logging.warning("Invalid model URL, using default initialization.")
+    else:
+        filename = url.rpartition('/')[-1]
+        params_path = get_file(filename, url)
+
+        # Check hash in file name
+        if hash_prefix is None:
+            r = HASH_REGEX.search(filename)
+            hash_prefix = r.group(1) if r else None
+
+        if isinstance(hash_prefix, str):
+            # Hash the file
+            with open(params_path, 'rb') as f:
+                sha_hash = hashlib.sha256(f.read()).hexdigest()
+
+            # Compare to expected hash
+            if sha_hash[:len(hash_prefix)] != hash_prefix:
+                # Remove file
+                os.remove(params_path)
+                raise ValueError(f"corrupted download, the hash of {url} does not match its expected value")
+
+        # Load weights
+        model.load_weights(params_path)
 
 
-def quantize_model(tf_model: tf.keras.Model, input_shape: Tuple[int, int, int]) -> bytes:
-    """Quantize a Tensorflow model
+def conv_sequence(
+    out_channels: int,
+    activation: str = None,
+    bn: bool = False,
+    padding: str = 'same',
+    **kwargs: Any,
+) -> List[layers.Layer]:
+    """Builds a convolutional-based layer sequence
 
     Args:
-        tf_model: a keras model
-        input_shape: shape of the expected input tensor (excluding batch dimension) with channel last order
+        out_channels: number of output channels
+        activation: activation to be used (default: no activation)
+        bn: should a batch normalization layer be added
+        padding: padding scheme
 
     Returns:
-        bytes: the serialized quantized model
+        list of layers
     """
-    converter = tf.lite.TFLiteConverter.from_keras_model(tf_model)
+    conv_seq = [layers.Conv2D(out_channels, padding=padding, **kwargs)]
 
-    converter.optimizations = [tf.lite.Optimize.DEFAULT]
+    if bn:
+        conv_seq.append(layers.BatchNormalization())
 
-    # Float fallback for operators that do not have an integer implementation
-    def representative_dataset():
-        for _ in range(100):
-            data = np.random.rand(1, *input_shape)
-            yield [data.astype(np.float32)]
+    if isinstance(activation, str):
+        conv_seq.append(layers.Activation(activation))
 
-    converter.representative_dataset = representative_dataset
-    converter.target_spec.supported_ops = [tf.lite.OpsSet.TFLITE_BUILTINS_INT8]
-    converter.inference_input_type = tf.int8
-    converter.inference_output_type = tf.int8
+    return conv_seq
 
-    return converter.convert()
+
+class IntermediateLayerGetter(Model):
+    """Implements an intermediate layer getter
+
+    Args:
+        model: the model to extract feature maps from
+        layer_names: the list of layers to retrieve the feature map from
+    """
+    def __init__(
+        self,
+        model: Model,
+        layer_names: List[str]
+    ) -> None:
+        intermediate_fmaps = [model.get_layer(layer_name).output for layer_name in layer_names]
+        super().__init__(model.input, outputs=intermediate_fmaps)
