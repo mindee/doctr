2abfbe88e3983a76c2fa43f006f9e3537a378c80
fg-mindee <fg@mindee.co>
2021-02-02 18:19:23 +0100

    refactor: Moved RecognitionPreProcessor into core

--- doctr/models/recognition/core.py
+++ doctr/models/recognition/core.py
@@ -10,7 +10,66 @@ import numpy as np
 
 from ..preprocessor import PreProcessor
 
-__all__ = ['RecognitionPostProcessor', 'RecognitionModel', 'RecognitionPredictor']
+__all__ = ['RecognitionPreProcessor', 'RecognitionPostProcessor', 'RecognitionModel', 'RecognitionPredictor']
+
+
+class RecognitionPreProcessor(PreProcessor):
+    """Implements a recognition preprocessor
+
+    Example::
+        >>> from doctr.documents import read_pdf
+        >>> from doctr.models import RecoPreprocessor
+        >>> processor = RecoPreprocessor(output_size=(600, 600), batch_size=8)
+        >>> processed_doc = processor([read_pdf("path/to/your/doc.pdf")])
+
+    Args:
+        output_size: expected size of each page in format (H, W)
+        batch_size: the size of page batches
+        mean: mean value of the training distribution by channel
+        std: standard deviation of the training distribution by channel
+    """
+
+    def __init__(
+        self,
+        output_size: Tuple[int, int],
+        batch_size: int,
+        mean: Tuple[float, float, float] = (.5, .5, .5),
+        std: Tuple[float, float, float] = (1., 1., 1.),
+        interpolation: str = 'bilinear',
+    ) -> None:
+
+        super().__init__(output_size, batch_size, mean, std, interpolation)
+
+    def resize(
+        self,
+        x: tf.Tensor,
+    ) -> tf.Tensor:
+        """Resize images using tensorflow backend.
+        The images is resized to (output_height, width) where width is computed as follow :
+            - If (preserving aspect-ratio width) output_height/image_height * image_width < output__width :
+                resize to (output_height, output_height/image_height * image_width)
+            - Else :
+                resize to (output_height, output_width)
+        Pads the image source with 0 to the right to match target_width and to the bottom to match target_height
+
+        Args:
+            x: image as a tf.Tensor
+
+        Returns:
+            the processed image after being resized
+        """
+        image_shape = tf.shape(x)
+        image_height = tf.cast(image_shape[0], dtype=tf.float32)
+        image_width = tf.cast(image_shape[1], dtype=tf.float32)
+
+        scale = self.output_size[0] / image_height
+        max_width = tf.cast(self.output_size[1], tf.int32)
+        new_width = tf.minimum(tf.cast(scale * image_width, dtype=tf.int32), max_width)
+
+        resized = tf.image.resize(x, [self.output_size[0], new_width], method=self.interpolation)
+        padded = tf.image.pad_to_bounding_box(resized, 0, 0, self.output_size[0], self.output_size[1])
+
+        return padded
 
 
 class RecognitionModel(keras.Model):
@@ -53,7 +112,7 @@ class RecognitionPredictor:
 
     def __init__(
         self,
-        pre_processor: PreProcessor,
+        pre_processor: RecognitionPreProcessor,
         model: RecognitionModel,
         post_processor: RecognitionPostProcessor,
     ) -> None:
