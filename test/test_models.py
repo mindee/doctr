2eb07d2e770c973f28f125c9b88b173e57609d84
fg-mindee <fg@mindee.co>
2021-02-08 14:12:47 +0100

    test: Optimized tests

--- test/test_models.py
+++ test/test_models.py
@@ -70,7 +70,7 @@ def test_detpreprocessor(mock_pdf):  # noqa: F811
     num_docs = 3
     batch_size = 4
     docs = [read_pdf(mock_pdf) for _ in range(num_docs)]
-    processor = models.DetectionPreProcessor(output_size=(600, 600), batch_size=batch_size)
+    processor = models.DetectionPreProcessor(output_size=(512, 512), batch_size=batch_size)
     batched_docs = processor([page for doc in docs for page in doc])
 
     # Number of batches
@@ -83,10 +83,10 @@ def test_detpreprocessor(mock_pdf):  # noqa: F811
     # Data type
     assert all(batch.dtype == tf.float32 for batch in batched_docs)
     # Image size
-    assert all(batch.shape[1:] == (600, 600, 3) for batch in batched_docs)
+    assert all(batch.shape[1:] == (512, 512, 3) for batch in batched_docs)
     # Test with non-full last batch
     batch_size = 16
-    processor = models.DetectionPreProcessor(output_size=(600, 600), batch_size=batch_size)
+    processor = models.DetectionPreProcessor(output_size=(512, 512), batch_size=batch_size)
     batched_docs = processor([page for doc in docs for page in doc])
     assert batched_docs[-1].shape[0] == (8 * num_docs) % batch_size
 
@@ -118,11 +118,11 @@ def test_recopreprocessor(mock_pdf):  # noqa: F811
 
 def test_dbpostprocessor():
     postprocessor = models.DBPostProcessor()
-    mock_batch = tf.random.uniform(shape=[8, 600, 600, 1], minval=0, maxval=1)
+    mock_batch = tf.random.uniform(shape=[2, 512, 512, 1], minval=0, maxval=1)
     out = postprocessor(mock_batch)
     # Batch composition
     assert isinstance(out, list)
-    assert len(out) == 8
+    assert len(out) == 2
     assert all(isinstance(sample, np.ndarray) for sample in out)
     assert all(sample.shape[1] == 5 for sample in out)
     # Relative coords
@@ -132,11 +132,11 @@ def test_dbpostprocessor():
 def test_db_resnet50():
     model = models.db_resnet50(pretrained=True)
     assert isinstance(model, tf.keras.Model)
-    dbinput = tf.random.uniform(shape=[8, 1024, 1024, 3], minval=0, maxval=1)
+    dbinput = tf.random.uniform(shape=[2, 1024, 1024, 3], minval=0, maxval=1)
     # test prediction model
     dboutput_notrain = model(dbinput)
     assert isinstance(dboutput_notrain, tf.Tensor)
-    assert dboutput_notrain.numpy().shape == (8, 1024, 1024, 1)
+    assert dboutput_notrain.numpy().shape == (2, 1024, 1024, 1)
     assert np.all(dboutput_notrain.numpy() > 0) and np.all(dboutput_notrain.numpy() < 1)
     # test training model
     dboutput_train = model(dbinput, training=True)
@@ -144,7 +144,7 @@ def test_db_resnet50():
     assert len(dboutput_train) == 3
     assert all(np.all(np.logical_and(out_map.numpy() >= 0, out_map.numpy() <= 1)) for out_map in dboutput_train)
     # batch size
-    assert all(out.numpy().shape == (8, 1024, 1024, 1) for out in dboutput_train)
+    assert all(out.numpy().shape == (2, 1024, 1024, 1) for out in dboutput_train)
 
 
 def test_extract_crops(mock_pdf):  # noqa: F811
