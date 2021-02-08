ab45a83d1cdcb4ce645b6f36f7a26c170eb8158a
fg-mindee <fg@mindee.co>
2021-02-08 16:07:15 +0100

    fix: Fixed weights URL

--- doctr/models/recognition/sar.py
+++ doctr/models/recognition/sar.py
@@ -22,7 +22,7 @@ default_cfgs: Dict[str, Dict[str, Any]] = {
         'post_processor': 'SARPostProcessor',
         'vocab': ('3K}7eé;5àÎYho]QwV6qU~W"XnbBvcADfËmy.9ÔpÛ*{CôïE%M4#ÈR:g@T$x?0î£|za1ù8,OG€P-'
                   'kçHëÀÂ2É/ûIJ\'j(LNÙFut[)èZs+&°Sd=Ï!<â_Ç>rêi`l'),
-        'url': 'https://github.com/publicMindee/doctr/releases/download/v0.1-models/sar_vgg16_bn-1aaf65b5.zip'
+        'url': 'https://github.com/mindee/doctr/releases/download/v0.1-models/sar_vgg16_bn-1aaf65b5.zip'
     },
 }
 
