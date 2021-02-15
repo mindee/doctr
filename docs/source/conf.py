a542f419cd8814484f96f5c5c95339cf40c9870d
fg-mindee <fg@mindee.co>
2021-02-15 17:34:54 +0100

    style: Removed exra  blank line

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -87,4 +87,3 @@ html_theme_options = {
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = []
-
