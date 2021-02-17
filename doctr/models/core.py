7fd251e823cf6b56bc0d1916b449d2c73d2e1489
fg-mindee <fg@mindee.co>
2021-02-17 12:06:53 +0100

    refactor: Reorganized postprocessing with a class

--- doctr/models/core.py
+++ doctr/models/core.py
@@ -5,13 +5,13 @@
 
 
 import numpy as np
-from typing import List, Any
+from typing import List, Any, Tuple
 from .detection import DetectionPredictor
 from .recognition import RecognitionPredictor
 from ._utils import extract_crops
 from ..documents.elements import Word, Line, Block, Page, Document
 
-__all__ = ['OCRPredictor']
+__all__ = ['OCRPredictor', 'DocumentBuilder']
 
 
 class OCRPredictor:
@@ -30,57 +30,7 @@ class OCRPredictor:
 
         self.det_predictor = det_predictor
         self.reco_predictor = reco_predictor
-
-    @staticmethod
-    def _build_blocks(boxes: np.ndarray, char_sequences: List[str]) -> List[Block]:
-        """Gather independent words in structured blocks
-
-        Args:
-            boxes: bounding boxes of all detected words of the page, of shape (N, 4)
-            char_sequences: list of all detected words of the page, of shape N
-
-        Returns:
-            list of block elements
-        """
-
-        if boxes.shape[0] != len(char_sequences):
-            raise ValueError(f"Incompatible argument lengths: {boxes.shape[0]}, {len(char_sequences)}")
-
-        # Sort bounding boxes from top to bottom, left to right
-        idxs = (boxes[:, 0] + boxes[:, 1] * (boxes[:, 2] - boxes[:, 0])).argsort()
-        # Try to arrange them in lines
-        lines = []
-        words: List[int] = []
-        for idx in idxs:
-
-            if len(words) == 0:
-                words = [idx]
-                continue
-            # Check horizontal gaps
-            horz_gap = boxes[idx, 0] - boxes[words[-1], 0] < 0.5 * (boxes[words[-1], 2] - boxes[words[-1], 0])
-            # Check vertical gaps
-            vert_gap = abs(boxes[idx, 3] - boxes[words[-1], 3]) > 0.5 * (boxes[words[-1], 3] - boxes[words[-1], 1])
-
-            if horz_gap or vert_gap:
-                lines.append(words)
-                words = []
-
-            words.append(idx)
-
-        if len(words) > 0:
-            lines.append(words)
-
-        blocks = [
-            Block([Line(
-                [Word(
-                    char_sequences[idx],
-                    boxes[idx, 4],
-                    ((boxes[idx, 0], boxes[idx, 1]), (boxes[idx, 2], boxes[idx, 3]))
-                ) for idx in line]
-            )]) for line in lines
-        ]
-
-        return blocks
+        self.doc_builder = DocumentBuilder()
 
     def __call__(
         self,
@@ -99,6 +49,143 @@ class OCRPredictor:
 
         # Reorganize
         num_pages = [len(doc) for doc in documents]
+        results = self.doc_builder(boxes, char_sequences, num_pages, [page.shape[:2] for page in pages])
+
+        return results
+
+
+class DocumentBuilder:
+    """Implements a document builder
+
+    Args:
+        resolve_lines: whether words should be automatically grouped into lines
+        resolve_blocks: whether lines should be automatically grouped into blocks
+        paragraph_break: relative length of the minimum space separating paragraphs
+    """
+
+    def __init__(
+        self,
+        resolve_lines: bool = False,
+        resolve_blocks: bool = False,
+        paragraph_break: float = 0.15
+    ) -> None:
+
+        self.resolve_lines = resolve_lines
+
+        if resolve_blocks:
+            raise NotImplementedError
+
+        self.paragraph_break = paragraph_break
+
+    @staticmethod
+    def _sort_boxes(boxes: np.ndarray) -> np.ndarray:
+        """Sort bounding boxes from top to bottom, left to right
+
+        Args:
+            boxes: bounding boxes of shape (N, 4)
+
+        Returns:
+            indices of ordered boxes of shape (N,)
+        """
+
+        return (boxes[:, 0] + boxes[:, 3] / np.median(boxes[:, 3] - boxes[:, 1])).argsort()
+
+    def _resolve_lines(self, boxes: np.ndarray, idxs: np.ndarray) -> List[List[int]]:
+        """Uses ordered boxes to group them in lines
+
+        Args:
+            boxes: bounding boxes of shape (N, 4)
+            idxs: indices of ordered boxes of shape (N,)
+
+        Returns:
+            nested list of box indices
+        """
+
+        # Try to arrange them in lines
+        lines = []
+        # Add the first word anyway
+        words: List[int] = [idxs[0]]
+        for idx in idxs[1:]:
+            line_break = True
+
+            prev_box = boxes[words[-1]]
+            # Reduced vertical diff
+            if boxes[idx, 1] < prev_box[[1, 3]].mean():
+                # Words horizontally ordered and close
+                if (boxes[idx, 0] - prev_box[2]) < self.paragraph_break:
+                    line_break = False
+
+            if line_break:
+                lines.append(words)
+                words = []
+
+            words.append(idx)
+
+        # Use the remaining words to form the last line
+        if len(words) > 0:
+            lines.append(words)
+
+        return lines
+
+    def _build_blocks(self, boxes: np.ndarray, char_sequences: List[str]) -> List[Block]:
+        """Gather independent words in structured blocks
+
+        Args:
+            boxes: bounding boxes of all detected words of the page, of shape (N, 4)
+            char_sequences: list of all detected words of the page, of shape N
+
+        Returns:
+            list of block elements
+        """
+
+        if boxes.shape[0] != len(char_sequences):
+            raise ValueError(f"Incompatible argument lengths: {boxes.shape[0]}, {len(char_sequences)}")
+
+        # Sort bounding boxes from top to bottom, left to right
+        idxs = self._sort_boxes(boxes[:, :4])
+
+        # Decide whether we try to form lines
+        if self.resolve_lines:
+            lines = self._resolve_lines(boxes[:, :4], idxs)
+        else:
+            # One line for all words
+            lines = [idxs]
+
+        # No automatic line grouping yet --> 1 block for all lines
+        blocks = [
+            Block(
+                [Line(
+                    [Word(
+                        char_sequences[idx],
+                        boxes[idx, 4],
+                        ((boxes[idx, 0], boxes[idx, 1]), (boxes[idx, 2], boxes[idx, 3]))
+                    ) for idx in line]
+                ) for line in lines]
+            )
+        ]
+
+        return blocks
+
+    def __call__(
+        self,
+        boxes: List[np.ndarray],
+        char_sequences: List[str],
+        num_pages: List[int],
+        page_shapes: List[Tuple[int, int]]
+    ) -> List[Document]:
+        """Re-arrange detected words into structured blocks
+
+        Args:
+            boxes: list of localization predictions for all words, of shape (N, 5)
+            char_sequences: list of all word values, of size N
+            num_pages: number of pages for each document
+            page_shape: shape of each page
+
+        Returns:
+            list of documents
+        """
+
+        # Check the number of crops for each page
         num_crops = [_boxes.shape[0] for _boxes in boxes]
         page_idx, crop_idx = 0, 0
         results = []
@@ -113,7 +200,7 @@ class OCRPredictor:
                             char_sequences[crop_idx: crop_idx + num_crops[page_idx]]
                         ),
                         page_idx,
-                        pages[page_idx].shape[:2],
+                        page_shapes[page_idx],
                     )
                 )
                 crop_idx += num_crops[page_idx]
