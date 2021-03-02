341f3f993400de08259a84495e093a49edad9a52
charlesmindee <charles@mindee.co>
2021-03-02 11:50:30 +0100

    feat: add sar teacher forcing

--- doctr/models/recognition/sar.py
+++ doctr/models/recognition/sar.py
@@ -19,6 +19,7 @@ __all__ = ['SAR', 'SARPostProcessor', 'sar_vgg16_bn']
 default_cfgs: Dict[str, Dict[str, Any]] = {
     'sar_vgg16_bn': {
         'backbone': 'vgg16_bn', 'rnn_units': 512, 'max_length': 40, 'num_decoders': 2, 'teacher_forcing': False,
+        'input_shape': (64, 256, 3),
         'post_processor': 'SARPostProcessor',
         'vocab': ('3K}7eé;5àÎYho]QwV6qU~W"XnbBvcADfËmy.9ÔpÛ*{CôïE%M4#ÈR:g@T$x?0î£|za1ù8,OG€P-'
                   'kçHëÀÂ2É/ûIJ\'j(LNÙFut[)èZs+&°Sd=Ï!<â_Ç>rêi`l'),
