7527c3995cff0c489c35ddf6bc53d55ffc02c195
charlesmindee <charles@mindee.co>
2021-03-05 17:08:10 +0100

    add: url crnn

--- doctr/models/recognition/crnn.py
+++ doctr/models/recognition/crnn.py
@@ -21,7 +21,7 @@ default_cfgs: Dict[str, Dict[str, Any]] = {
                       'post_processor': 'CTCPostProcessor',
                       'vocab': ('3K}7eé;5àÎYho]QwV6qU~W"XnbBvcADfËmy.9ÔpÛ*{CôïE%M4#ÈR:g@T$x?0î£|za1ù8,OG€P-'
                                 'kçHëÀÂ2É/ûIJ\'j(LNÙFut[)èZs+&°Sd=Ï!<â_Ç>rêi`l'),
-                      'url': 'https://github.com/mindee/doctr/releases/download/v0.1-models/crnn_vgg16bn-f1d4cc4c.zip'},
+                      'url': 'https://github.com/mindee/doctr/releases/download/v0.1-models/crnn_vgg16bn-b37097a8.zip'},
 }
 
 
