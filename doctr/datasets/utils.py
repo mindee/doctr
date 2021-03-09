e0661e64821d108cb1f9f90aa08fe4024a7e1387
charlesmindee <charles@mindee.co>
2021-03-09 09:45:41 +0100

    refacto: PR comments

--- doctr/datasets/utils.py
+++ doctr/datasets/utils.py
@@ -6,26 +6,24 @@
 import string
 import unicodedata
 import numpy as np
-from typing import Dict
+from typing import Dict, List
 
-__all__ = ['translate', 'encode_sequence', 'decode_sequence']
+__all__ = ['translate', 'encode_sequence', 'decode_sequence', 'VOCABS']
 
 
-vocabs: Dict[str, str] = {
+VOCABS: Dict[str, str] = {
     'digits': string.digits,
     'ascii_letters': string.ascii_letters,
     'punctuation': string.punctuation,
     'currency': '£€¥¢฿',
     'latin': string.digits + string.ascii_letters + string.punctuation + '°',
     'french': string.digits + string.ascii_letters + string.punctuation + '°' + 'àâéèêëîïôùûçÀÂÉÈËÎÏÔÙÛÇ' + '£€¥¢฿',
-    'tfrecords_training': '3K}7eé;5àÎYho]QwV6qU~W"XnbBvcADfËmy.9ÔpÛ*{CôïE%M4#ÈR:g@T$x?0î'
-                          '£|za1ù8,OG€P-kçHëÀÂ2É/ûIJ\'j(LNÙFut[)èZs+&°Sd=Ï!<â_Ç>rêi`l'
 }
 
 
 def translate(
     input_string: str,
-    vocab: str,
+    vocab_name: str,
 ) -> str:
     """Translate a string input in a given vocabulary
 
@@ -36,8 +34,8 @@ def translate(
     Returns:
         A string translated in a given vocab"""
 
-    if vocab not in vocabs.keys():
-        raise AttributeError("output vocabulary must be in vocabs dictionnary")
+    if VOCABS.get(vocab) is None:
+        raise KeyError("output vocabulary must be in vocabs dictionnary")
 
     translated = ''
     for char in input_string:
@@ -58,7 +56,7 @@ def translate(
 def encode_sequence(
     input_string: str,
     mapping: str,
-) -> np.array:
+) -> List[str]:
     """Given a predefined mapping, encode the string to a sequence of numbers
 
     Args:
@@ -66,14 +64,10 @@ def encode_sequence(
         mapping: vocabulary (string), the encoding is given by the indexing of the character sequence
 
     Returns:
-        A tensor encoding the input_string"""
+        A list encoding the input_string"""
 
-    encoded = []
-    for char in input_string:
-        if char not in mapping:
-            raise AttributeError("Input string vocabulary does not match encoding vocabulary")
-        encoded.append(mapping.index(char))
-    return np.array(encoded)
+    encoded = list(map(lambda x: mapping.index(x), input_string))
+    return encoded
 
 
 def decode_sequence(
@@ -89,9 +83,7 @@ def decode_sequence(
     Returns:
         A string, decoded from input_array"""
 
-    if not input_array.dtype == np.int_ or max(input_array) >= len(mapping):
-        raise AttributeError("Input must be an array of int, with max less than mapping size")
-    decoded = ''
-    for index in list(input_array):
-        decoded += mapping[index]
+    if not input_array.dtype == np.int_ or input_array.max() >= len(mapping):
+        raise AssertionError("Input must be an array of int, with max less than mapping size")
+    decoded = ''.join(mapping[idx] for idx in input_array)
     return decoded
