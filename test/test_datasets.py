38a0b9203598c72f67650d78e08755a05d9e91c0
fg-mindee <fg@mindee.co>
2021-03-12 15:55:36 +0100

    test: Added unittests for FUNSD

---
+++ test/test_datasets.py
@@ -0,0 +1,24 @@
+import pytest
+import os
+import numpy as np
+from doctr import datasets
+
+
+@pytest.mark.parametrize(
+    "dataset_name, size",
+    [
+        ['FUNSD', 149],
+    ],
+)
+def test_dataset(dataset_name, size):
+
+    with pytest.raises(ValueError):
+        datasets.__dict__[dataset_name](download=False)
+
+    ds = datasets.__dict__[dataset_name](download=True)
+
+    assert len(ds) == size
+    assert repr(ds) == f"{dataset_name}()"
+    img_name, target = ds[0]
+    assert isinstance(img_name, str) and os.path.exists(os.path.join(ds.root, img_name))
+    assert isinstance(target, dict)
