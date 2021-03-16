949ddbd14f1bdf2bedee79096a3d33ac47104338
charlesmindee <charles@mindee.co>
2021-03-16 17:44:05 +0100

    add: resnet sar to zoo

--- doctr/models/zoo.py
+++ doctr/models/zoo.py
@@ -9,10 +9,11 @@ from .detection import zoo as det_zoo
 from .recognition import zoo as reco_zoo
 
 
-__all__ = ["ocr_db_sar", "ocr_db_crnn"]
+__all__ = ["ocr_db_sar", "ocr_db_crnn", "ocr_db_sar_resnet"]
 
 default_cfgs: Dict[str, Dict[str, Any]] = {
     'ocr_db_sar': {'detection': 'db_resnet50_predictor', 'recognition': 'sar_vgg16_bn_predictor'},
+    'ocr_db_sar_resnet': {'detection': 'db_resnet50_predictor', 'recognition': 'sar_resnet31_predictor'},
     'ocr_db_crnn': {'detection': 'db_resnet50_predictor', 'recognition': 'crnn_vgg16_bn_predictor'},
 }
 
@@ -47,6 +48,27 @@ def ocr_db_sar(pretrained: bool = False, **kwargs: Any) -> OCRPredictor:
     """
 
     return _predictor('ocr_db_sar', pretrained, **kwargs)
+
+
+def ocr_db_sar_resnet(pretrained: bool = False, **kwargs: Any) -> OCRPredictor:
+    """End-to-end OCR architecture using a DBNet with a ResNet-50 backbone for localization, and SAR with a VGG-16BN
+    backbone as text recognition architecture.
+
+    Example::
+        >>> import numpy as np
+        >>> from doctr.models import ocr_db_sar
+        >>> model = ocr_db_sar(pretrained=True)
+        >>> input_page = (255 * np.random.rand(600, 800, 3)).astype(np.uint8)
+        >>> out = model([[input_page]])
+
+    Args:
+        pretrained (bool): If True, returns a model pre-trained on ImageNet
+
+    Returns:
+        OCR predictor
+    """
+
+    return _predictor('ocr_db_sar_resnet', pretrained, **kwargs)
 
 
 def ocr_db_crnn(pretrained: bool = False, **kwargs: Any) -> OCRPredictor:
