3d4acea0a4aab3743b667a4ed1de8b465af8dfc1
charlesmindee <charles@mindee.co>
2021-03-23 14:35:44 +0100

    refacto: data loader

--- doctr/datasets/loaders/detection/core.py
+++ doctr/datasets/loaders/detection/core.py
@@ -23,21 +23,15 @@ def load_annotation(
         img_name: name of the image to find the corresponding json annotation
 
     Returns:
-        A tuple of 2 lists: a list of polygons to draw on the segmentation gt map,
-        and a list of ambiguous polygons to mask during the training
-
+        A tuple of 2 lists: a list of polygons and a boolean vector to mask suspicious polygons
     """
     with open(os.path.join(labels_path, img_name + '.json')) as f:
         labels = json.load(f)
 
-    polys = [[[int(x), int(y)] for [x, y] in polygon] for polygon in labels["boxes_1"] + labels["boxes_2"]]
-    polys_mask = [[[int(x), int(y)] for [x, y] in polygon] for polygon in labels["boxes_3"]]
+    polys = [[[int(x), int(y)] for [x, y] in polygon] for polygon in labels["boxes_1"] + labels["boxes_2"] + labels["boxes_3"]]
+    to_masks = [False for polygon in labels["boxes_1"] + labels["boxes_2"]] + [True for polygon in labels["boxes_3"]]
 
-    return polys, polys_mask
-
-
-def resize(image, input_size):
-    return cv2.resize(image, input_size, interpolation=cv2.INTER_AREA)
+    return polys, to_masks
 
 
 class DataGenerator(tf.keras.utils.Sequence):
@@ -66,10 +60,12 @@ class DataGenerator(tf.keras.utils.Sequence):
         self.shuffle = shuffle
         self.on_epoch_end()
         self.min_size_box = 3
+        self.std_rgb = (0.264, 0.274, 0.287)
+        self.mean_rgb = (0.798, 0.785, 0.772)
 
     def __len__(self):
         # Denotes the number of batches per epoch
-        return int(np.floor(len(os.listdir(self.images_path)) / self.batch_size))
+        return int(np.ceil(len(os.listdir(self.images_path)) / self.batch_size))
 
     def on_epoch_end(self):
         # Updates indexes after each epoch
@@ -80,9 +76,9 @@ class DataGenerator(tf.keras.utils.Sequence):
     def __getitem__(
         self,
         index: int
-    ) -> Tuple[tf.Tensor, np.array, np.array]:
+    ) -> Tuple[tf.Tensor, tf.Tensor, tf.Tensor]:
         # Get one batch of data
-        indexes = self.indexes[index * self.batch_size:(index + 1) * self.batch_size]
+        indexes = self.indexes[index * self.batch_size:min(self.__len__() - 1, (index + 1) * self.batch_size)]
         # Find list of paths
         list_paths = [os.listdir(self.images_path)[k] for k in indexes]
         # Generate data
@@ -91,7 +87,7 @@ class DataGenerator(tf.keras.utils.Sequence):
     def __data_generation(
         self,
         list_paths: List[str],
-    ) -> Tuple[tf.Tensor, np.array, np.array]:
+    ) -> Tuple[tf.Tensor, tf.Tensor, tf.Tensor]:
         # Init batch arrays
         batch_images = np.empty((self.batch_size, *self.input_size, 3))
         batch_gts = np.empty((self.batch_size, *self.input_size))
@@ -106,6 +102,7 @@ class DataGenerator(tf.keras.utils.Sequence):
                 polys, polys_mask = [], []
 
             image = cv2.imread(os.path.join(self.images_path, image_name))
+            image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
             h, w = image.shape[:2]
 
             # Initialize mask and gt
@@ -113,35 +110,38 @@ class DataGenerator(tf.keras.utils.Sequence):
             mask = np.ones((h, w), dtype=np.float32)
 
             # Draw each polygon on gt
-            for poly in polys:
+            for poly, to_mask in zip(polys, to_masks):
                 poly = np.array(poly)
+                if to_mask is True:
+                    cv2.fillPoly(mask, poly.astype(np.int32)[np.newaxis, :, :], 0)
+                    continue
                 height = max(poly[:, 1]) - min(poly[:, 1])
                 width = max(poly[:, 0]) - min(poly[:, 0])
-                # generate true and mask
                 if min(height, width) < self.min_size_box:
                     cv2.fillPoly(mask, poly.astype(np.int32)[np.newaxis, :, :], 0)
                     continue
                 cv2.fillPoly(gt, [poly.astype(np.int32)], 1)
 
-            # Fill mask
-            for poly in polys_mask:
-                poly = np.array(poly)
-                cv2.fillPoly(mask, poly.astype(np.int32)[np.newaxis, :, :], 0)
-
-            # Resize and batch
+            # Cast
             image = tf.cast(image, tf.float32)
+            gt = tf.cast(gt, tf.float32)
+            mask = tf.cast(mask, tf.float32)
+            # Resize
             image = tf.image.resize(image, [*self.input_size], method='bilinear')
-            gt = resize(gt, self.input_size)
-            mask = resize(mask, self.input_size)
-
+            gt = tf.image.resize(gt, [*self.input_size], method='bilinear')
+            mask = tf.image.resize(mask, [*self.input_size], method='bilinear')
+            # Batch
             batch_images[index, ] = image
             batch_gts[index, ] = gt
             batch_masks[index, ] = mask
 
-        # Normalize
+        # Stack batches
         batch_images = tf.stack(batch_images, axis=0)
-        std = tf.cast((0.287, 0.274, 0.264), tf.float32)
-        mean = tf.cast((0.772, 0.785, 0.798), tf.float32)
+        batch_gts = tf.stack(batch_gts, axis=0)
+        batch_masks = tf.stack(batch_masks, axis=0)
+        # Normalize
+        std = tf.cast(self.std_rgb, tf.float32)
+        mean = tf.cast(self.mean_rgb, tf.float32)
         batch_images = tf.cast(batch_images, tf.float32) * (std / 255) - (mean / std)
 
         return batch_images, batch_gts, batch_masks
