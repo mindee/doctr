1c4beec03bf7121f3a08ed87f6404770e5e8d82a
fg-mindee <fg@mindee.co>
2021-03-25 15:26:07 +0100

    test: Fixed typo

--- test/test_models.py
+++ test/test_models.py
@@ -160,6 +160,6 @@ def test_classification_architectures(arch_name, top_implemented, input_shape, o
 )
 def test_zoo_models(arch_name):
     # Model
-    predictor = models.ocr_model(arch_name, pretrained=True)
+    predictor = models.ocr_predictor(arch_name, pretrained=True)
     # Output checks
     assert isinstance(predictor, models.OCRPredictor)
