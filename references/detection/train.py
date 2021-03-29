3a63fe92e12ef6d9ebf7d8d6d848de5f8bbd61b0
Charles Gaillard <charles@mindee.co>
2021-03-29 18:44:29 +0200

    feat: add detection training script (#178)
    * feat: add benchmark + changed cfgs
    
    * work in progress
    
    * feat: add core recognition data loader
    
    * feat: add reco training script
    
    * merged
    
    * fix: typo
    
    * feat: add recognition script
    
    * fix: removed deprecated files
    
    * feat: add readme
    
    * fox: loss flake8
    
    * fix: flake8 loss
    
    * refacto: data arch
    
    * feat: add detection training script
    
    * fix: flake8 ./
    
    * fix: codefactor
    
    * fix: typos
    
    * fix: tuple arg

--- references/detection/train.py
+++ references/detection/train.py
@@ -4,51 +4,47 @@
 # See LICENSE or go to <https://www.apache.org/licenses/LICENSE-2.0.txt> for full license details.
 
 import os
-import json
 import datetime
 import numpy as np
 import tensorflow as tf
 from collections import deque
 
-from doctr.models import recognition
+from doctr.models import detection
 from doctr.utils import metrics
-from doctr.datasets import RecognitionDataGenerator, VOCABS
+from doctr.datasets import DetectionDataGenerator
 
 
 def main(args):
 
     # Load both train and val data generators
-    train_dataset = RecognitionDataGenerator(
+    train_dataset = DetectionDataGenerator(
         input_size=args.input_size,
         batch_size=args.batch_size,
         images_path=os.path.join(args.data_path, 'train'),
-        labels_path=os.path.join(args.data_path, 'train_labels.json')
+        labels_path=os.path.join(args.data_path, 'train_labels')
     )
 
-    val_dataset = RecognitionDataGenerator(
+    val_dataset = DetectionDataGenerator(
         input_size=args.input_size,
         batch_size=args.batch_size,
         images_path=os.path.join(args.data_path, 'val'),
-        labels_path=os.path.join(args.data_path, 'val_labels.json')
+        labels_path=os.path.join(args.data_path, 'val_labels')
     )
 
     # Optimizer
     optimizer = tf.keras.optimizers.Adam(learning_rate=args.learning_rate, clipnorm=5)
 
     # Load doctr model
-    model = recognition.__dict__[args.model](pretrained=False, input_shape=(args.input_size, 4 * args.input_size))
+    model = detection.__dict__[args.model](pretrained=False, input_shape=(args.input_size, args.input_size, 3))
 
     # Tf variable to log steps
     step = tf.Variable(0, dtype="int64")
 
     # Metrics
-    val_metric = metrics.ExactMatch()
+    val_metric = metrics.LocalizationConfusion()
 
-    # Postprocessor to decode output (to feed metric during val step)
-    if args.postprocessor == 'sar':
-        postprocessor = recognition.SARPostProcessor(vocab=VOCABS["french"])
-    else:
-        postprocessor = recognition.CTCPostProcessor(vocab=VOCABS["french"])
+    # Postprocessor to decode output (to feed metric during val step with boxes)
+    postprocessor = detection.DBPostProcessor()
 
     # Tensorboard to monitor training
     current_time = datetime.datetime.now().strftime("%Y%m%d-%H%M%S")
@@ -57,39 +53,50 @@ def main(args):
     train_summary_writer = tf.summary.create_file_writer(train_log_dir)
     val_summary_writer = tf.summary.create_file_writer(val_log_dir)
 
-    def train_step(x, y):
+    output_shape = (args.batch_size, args.input_size, args.input_size, 3)
+
+    def train_step(x):
         with tf.GradientTape() as tape:
-            if args.teacher_forcing is True:
-                train_logits = model(x, y, training=True)
-            else:
-                train_logits = model(x, training=True)
-            train_loss = train_logits.sum()  # FIXME
+            images, boxes, to_masks = x
+            [proba_map, thresh_map, bin_map] = model(images, training=True)
+            gts, masks, thresh_gts, thresh_masks = model.compute_target(output_shape, boxes, to_masks)
+            train_loss = model.compute_loss(proba_map, bin_map, thresh_map, gts, masks, thresh_gts, thresh_masks)
         grads = tape.gradient(train_loss, model.trainable_weights)
         optimizer.apply_gradients(zip(grads, model.trainable_weights))
         return train_loss
 
-    def test_step(x, y):
-        val_logits = model(x, training=False)
-        val_loss = train_logits.sum()  # FIXME
-        decoded = postprocessor(val_logits)
-        val_metric.update(gts=y, preds=decoded)
+    def test_step(x):
+        # If we want to compute val loss, we need to pass training=True to have a thresh_map
+        images, boxes, to_masks = x
+        [proba_map, thresh_map, bin_map] = model(images, training=True)
+        gts, masks, thresh_gts, thresh_masks = model.compute_target(output_shape, boxes, to_masks)
+        val_loss = model.compute_loss(proba_map, bin_map, thresh_map, gts, masks, thresh_gts, thresh_masks)
+        decoded = postprocessor(proba_map)
+        # Compute metric
+        for boxes_gt, boxes_pred in zip(boxes, decoded):
+            boxes_pred = np.array(boxes_pred)[:, :-1]  # Remove scores
+            boxes_gt = np.array([[
+                np.array(box).min(axis=0), np.array(box).min(axis=1),
+                np.array(box).max(axis=0), np.array(box).max(axis=1)
+            ] for box in boxes_gt])  # From a list of [x, y] coords to xmin, ymin, xmax, ymax format
+            val_metric.update(gts=boxes_gt, preds=boxes_pred)
         return val_loss
 
     # Create loss queue
     loss_q = deque(maxlen=100)
 
     # Training loop
-    for epoch in range(args.epochs):
+    for _ in range(args.epochs):
         # Iterate over the batches of the dataset
-        for batch_step, x_batch_train, y_batch_train in enumerate(train_dataset):
-            train_loss = train_step(x_batch_train, y_batch_train)
+        for batch_step, batch in enumerate(train_dataset):
+            train_loss = train_step(batch)
             # Update steps
             step.assign_add(args.batch_size)
             # Add loss to queue
             loss_q.append(np.mean(train_loss))
             # Log loss and save weights every 100 batch step
             if batch_step % 100 == 0:
-                model.save_weights('./checkpointsreco/weights')
+                model.save_weights('./checkpointsdet/weights')
                 # Compute loss
                 loss = sum(loss_q) / len(loss_q)
                 with train_summary_writer.as_default():
@@ -97,8 +104,8 @@ def main(args):
 
         # Validation loop at the end of each epoch
         loss_val = []
-        for x_batch_val, y_batch_val in val_dataset:
-            val_loss = test_step(x_batch_val, y_batch_val)
+        for batch in val_dataset:
+            val_loss = test_step(batch)
             loss_val.append(np.mean(val_loss))
         mean_loss = sum(loss_val) / len(loss_val)
         #tensorboard
@@ -116,11 +123,9 @@ def parse_args():
 
     parser.add_argument('model', type=str, help='text-recognition model to train')
     parser.add_argument('--epochs', type=int, default=10, help='number of epochs to train the model on')
-    parser.add_argument('--batch_size', type=int, default=64, help='batch size for training')
-    parser.add_argument('--input_size', type=int, default=32, help='input size H for the model, W = 4*H')
+    parser.add_argument('--batch_size', type=int, default=2, help='batch size for training')
+    parser.add_argument('--input_size', type=int, default=1024, help='model input size, H = W)')
     parser.add_argument('--learning_rate', type=float, default=0.001, help='learning rate for the optimizer (Adam)')
-    parser.add_argument('--postprocessor', type=str, default='crnn', help='postprocessor, either crnn or sar')
-    parser.add_argument('--teacher_forcing', type=bool, default=False, help='if True, teacher forcing during training')
     parser.add_argument('--data_path', type=str, help='path to data folder')
     args = parser.parse_args()
 
