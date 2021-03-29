86aea61aaaeadff80238befbb6e9860a7f3e1f04
fg-mindee <fg@mindee.co>
2021-03-29 16:31:29 +0200

    fix: Added gpu growth in scripts by default

--- scripts/analyze.py
+++ scripts/analyze.py
@@ -3,6 +3,13 @@
 # This program is licensed under the Apache License version 2.
 # See LICENSE or go to <https://www.apache.org/licenses/LICENSE-2.0.txt> for full license details.
 
+
+import tensorflow as tf
+
+gpu_devices = tf.config.experimental.list_physical_devices('GPU')
+if any(gpu_devices):
+    tf.config.experimental.set_memory_growth(gpu_devices[0], True)
+
 import matplotlib.pyplot as plt
 from doctr.models import ocr_predictor
 from doctr.documents import DocumentFile
