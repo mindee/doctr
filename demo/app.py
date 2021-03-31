e24e34868ce6baaca17598dd2807b752c7fdf24b
fg-mindee <fg@mindee.co>
2021-03-31 15:53:59 +0200

    feat: Added task visualizaion in demo app

--- demo/app.py
+++ demo/app.py
@@ -10,52 +10,55 @@ import matplotlib.pyplot as plt
 os.environ["TF_CPP_MIN_LOG_LEVEL"] = "2"
 
 import tensorflow as tf
+import cv2
 
 gpu_devices = tf.config.experimental.list_physical_devices('GPU')
 if any(gpu_devices):
     tf.config.experimental.set_memory_growth(gpu_devices[0], True)
 
 from doctr.documents import DocumentFile
-from doctr.models import detection_predictor
+from doctr.models import ocr_predictor
+from doctr.utils.visualization import visualize_page
 
 DET_ARCHS = ["db_resnet50"]
 RECO_ARCHS = ["crnn_vgg16_bn", "crnn_resnet31", "sar_vgg16_bn", "sar_resnet31"]
 
+ARCH_NAME = "db_resnet50crnn_vgg16_bn"
 
 def main():
+
+    # Wide mode
+    st.set_page_config(layout="wide")
 
     # Designing the interface
     st.title("DocTR: Document Text Recognition")
     # For newline
     st.write('\n')
+    # Set the columns
+    cols = st.beta_columns((1, 1, 1))
+    cols[0].header("Input document")
+    cols[1].header("Text segmentation")
+    cols[-1].header("OCR output")
 
     # Sidebar
-    st.sidebar.title("Model selection")
-    det_arch = st.sidebar.selectbox("Text detection model", DET_ARCHS)
-
-    predictor = detection_predictor(det_arch, pretrained=True)
-
-    # reco_arch = st.sidebar.selectbox("Text recognition model", RECO_ARCHS)
-    # predictor = detection_predictor(reco_arch, pretrained=True)
-
+    # File selection
     st.sidebar.title("Document selection")
     # Disabling warning
     st.set_option('deprecation.showfileUploaderEncoding', False)
     # Choose your own image
     uploaded_file = st.sidebar.file_uploader("Upload files", type=['pdf', 'png', 'jpeg', 'jpg'])
-
-    # Set the two columns
-    col1, col2 = st.beta_columns((1, 1))
-    col1.header("Input page")
-    col2.header("Text detection output")
-
     if uploaded_file is not None:
-
         if uploaded_file.name.endswith('.pdf'):
             doc = DocumentFile.from_pdf(uploaded_file.read())
         else:
             doc = DocumentFile.from_images(uploaded_file.read())
-        col1.image(doc[0], 'First page', use_column_width=True)
+        cols[0].image(doc[0], "First page", use_column_width=True)
+
+    # Model selection
+    st.sidebar.title("Model selection")
+    det_arch = st.sidebar.selectbox("Text detection model", DET_ARCHS)
+    reco_arch = st.sidebar.selectbox("Text recognition model", RECO_ARCHS)
+
 
     # For newline
     st.sidebar.write('\n')
@@ -66,18 +69,26 @@ def main():
             st.sidebar.write("Please upload a document")
 
         else:
+            with st.spinner('Loading model...'):
+                predictor = ocr_predictor(det_arch, reco_arch, pretrained=True)
 
             with st.spinner('Analyzing...'):
 
                 # Forward the image to the model
-                processed_batches = predictor.pre_processor(doc)
-                seg_map = predictor.model(processed_batches[0])[0]
-
+                processed_batches = predictor.det_predictor.pre_processor(doc)
+                seg_map = predictor.det_predictor.model(processed_batches[0])[0]
+                seg_map = cv2.resize(seg_map.numpy(), (doc[0].shape[1], doc[0].shape[0]),
+                                     interpolation=cv2.INTER_LINEAR)
                 # Plot the raw heatmap
                 fig, ax = plt.subplots()
                 ax.imshow(seg_map)
                 ax.axis('off')
-                col2.pyplot(fig, use_column_width=True)
+                cols[1].pyplot(fig)
+
+                # OCR
+                out = predictor(doc)
+                fig = visualize_page(out.pages[0].export(), doc[0], interactive=False)
+                cols[-1].pyplot(fig)
 
 
 if __name__ == '__main__':
