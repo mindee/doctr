c48bcd0fe927e5d9b5855676d04a0889b5de95d7
F-G Fernandez <76527547+fg-mindee@users.noreply.github.com>
2021-03-31 16:14:09 +0200

    feat: Added both localization & end-to-end visualization in demo app (#188)
    * refactor: Removed unused variable
    
    * feat: Added non-interactive visualization
    
    * refactor: Added interactive arg to element show
    
    * test: Updated unittests
    
    * test: Removed unused import
    
    * refactor: Refactored analyze script
    
    * feat: Added options to visualization
    
    * feat: Added task visualizaion in demo app
    
    * docs: Updated demo illustration in README
    
    * style: Fixed lint

--- test/test_utils_visualization.py
+++ test/test_utils_visualization.py
@@ -1,5 +1,4 @@
 import numpy as np
-import matplotlib.pyplot as plt
 
 from doctr.utils import visualization
 from test_documents_elements import _mock_pages
@@ -9,4 +8,4 @@ def test_visualize_page():
     pages = _mock_pages()
     image = np.ones((300, 200, 3))
     visualization.visualize_page(pages[0].export(), image, words_only=False)
-    plt.show(block=False)
+    visualization.visualize_page(pages[0].export(), image, words_only=True, interactive=False)
