2156c118ec77920f936fe2463a29bc191a89011a
fg-mindee <fg@mindee.co>
2021-04-07 16:44:45 +0200

    feat: Added abstract dataloader

--- doctr/datasets/loader.py
+++ doctr/datasets/loader.py
@@ -13,10 +13,9 @@ __all__ = ["DataLoader"]
 
 def default_collate(samples):
 
-    images, boxes, flags = zip(*samples)
-    images = tf.stack(images, axis=0)
+    images, targets = zip(*samples)
 
-    return images, list(boxes), list(flags)
+    return tf.stack(images, axis=0), tf.stack(targets, axis=0)
 
 
 class DataLoader:
@@ -26,35 +25,35 @@ class DataLoader:
         shuffle: bool = True,
         batch_size: int = 1,
         drop_last: bool = False,
-        collate_fn: Optional[] = None
     ) -> None:
-        self.data = dataset
+        self.dataset = dataset
         self.shuffle = shuffle
         self.batch_size = batch_size
-        self.num_batches = math.floor(len(self.data) / batch_size) if drop_last else math.ceil(len(self.data) / batch_size)
-        self.collate_fn = default_collate if collate_fn is None else collate_fn
+        self.num_batches = math.floor(len(self.dataset) / batch_size) if drop_last else math.ceil(len(self.dataset) / batch_size)
+        self.collate_fn = self.dataset.collate_fn if hasattr(self.dataset, 'collate_fn') else default_collate
         self.reset()
 
     def reset(self) -> None:
         # Updates indices after each epoch
-        self.indices = np.arange(len(self.data))
+        self._num_yielded = 0
+        self.indices = np.arange(len(self.dataset))
         if self.shuffle is True:
             np.random.shuffle(self.indices)
 
     def __iter__(self):
-        self._num_yielded = 0
+        self.reset()
         return self
 
     def __next__(self):
-        if self._num_yielded < self.num_batches:
+        if self._num_yielded <= self.num_batches:
             # Get next indices
-            indices = self.indices[self._num_yielded * self.batch_size: min(len(self.data), (self._num_yielded + 1) * self.batch_size)]
+            indices = self.indices[self._num_yielded * self.batch_size: min(len(self.dataset), (self._num_yielded + 1) * self.batch_size)]
 
-            samples = map(self.data.__getitem__, indices)
+            samples = map(self.dataset.__getitem__, indices)
 
             batch_data = self.collate_fn(samples)
 
             self._num_yielded += 1
             return batch_data
-
-        raise StopIteration()
+        else:
+            raise StopIteration
