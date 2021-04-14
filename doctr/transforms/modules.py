4424ed71747469e975b5400d7533e4c8a419fdc6
fg-mindee <fg@mindee.co>
2021-04-14 16:51:09 +0200

    feat: Added normalization and lambda transformations

--- doctr/transforms/modules.py
+++ doctr/transforms/modules.py
@@ -4,12 +4,12 @@
 # See LICENSE or go to <https://www.apache.org/licenses/LICENSE-2.0.txt> for full license details.
 
 import tensorflow as tf
-from typing import List, Any, Tuple
+from typing import List, Any, Tuple, Callable
 
 from doctr.utils.repr import NestedObject
 
 
-__all__ = ['Compose', 'Resize']
+__all__ = ['Compose', 'Resize', 'Normalize', 'LambdaTransformation']
 
 
 class Compose(NestedObject):
@@ -60,3 +60,48 @@ class Resize(NestedObject):
     def __call__(self, img: tf.Tensor) -> tf.Tensor:
         img = tf.image.resize(img, self.output_size, method=self.method)
         return img
+
+
+class Normalize(NestedObject):
+    """Normalize a tensor to a Gaussian distribution for each channel
+
+    Example::
+        >>> from doctr.transforms import Normalize
+        >>> import tensorflow as tf
+        >>> transfo = Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
+        >>> out = transfo(tf.random.uniform(shape=[8, 64, 64, 3], minval=0, maxval=1))
+
+    Args:
+        mean: average value per channel
+        std: standard deviation per channel
+    """
+    def __init__(self, mean: Tuple[float, float, float], std: Tuple[float, float, float]) -> None:
+        self.mean = tf.constant(mean, dtype=tf.float32)
+        self.std = tf.constant(std, dtype=tf.float32)
+
+    def extra_repr(self) -> str:
+        return f"mean={self.mean.numpy().tolist()}, std={self.std.numpy().tolist()}"
+
+    def __call__(self, img: tf.Tensor) -> tf.Tensor:
+        img -= self.mean
+        img /= self.std
+        return img
+
+
+class LambdaTransformation(NestedObject):
+    """Normalize a tensor to a Gaussian distribution for each channel
+
+    Example::
+        >>> from doctr.transforms import LambdaTransformation
+        >>> import tensorflow as tf
+        >>> transfo = LambdaTransformation(lambda x: x/ 255.)
+        >>> out = transfo(tf.random.uniform(shape=[8, 64, 64, 3], minval=0, maxval=1))
+
+    Args:
+        fn: the function to be applied to the input tensor
+    """
+    def __init__(self, fn: Callable[[tf.Tensor], tf.Tensor]) -> None:
+        self.fn = fn
+
+    def __call__(self, img: tf.Tensor) -> tf.Tensor:
+        return self.fn(img)
