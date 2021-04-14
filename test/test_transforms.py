b2dc4d98c1a0f6e6a71a9a11e9a20e4a5393b4f4
fg-mindee <fg@mindee.co>
2021-04-14 16:51:24 +0200

    test: Updated unittests of transforms

--- test/test_transforms.py
+++ test/test_transforms.py
@@ -1,3 +1,5 @@
+import pytest
+
 import tensorflow as tf
 from doctr import transforms as T
 
@@ -15,9 +17,37 @@ def test_resize():
 def test_compose():
 
     output_size = (16, 16)
-    transfo = T.Compose([Resize((32, 32)), Resize(output_size)])
+    transfo = T.Compose([T.Resize((32, 32)), T.Resize(output_size)])
     input_t = tf.random.uniform(shape=[64, 64, 3], minval=0, maxval=1)
     out = transfo(input_t)
 
     assert out.shape[:2] == output_size
     assert len(repr(transfo).split("\n")) == 6
+
+
+@pytest.mark.parametrize(
+    "input_shape",
+    [
+        [8, 32, 32, 3],
+        [32, 32, 3],
+        [32, 3],
+    ],
+)
+def test_normalize(input_shape):
+    mean, std = [0.5, 0.5, 0.5], [0.5, 0.5, 0.5]
+    transfo = T.Normalize(mean, std)
+    input_t = tf.cast(tf.fill(input_shape, 1), dtype=tf.float32)
+
+    out = transfo(input_t)
+
+    assert tf.reduce_all(out == 1)
+    assert repr(transfo) == f"Normalize(mean={mean}, std={std})"
+
+
+def test_lambatransformation():
+
+    transfo = T.LambdaTransformation(lambda x: x / 2)
+    input_t = tf.cast(tf.fill([8, 32, 32, 3], 1), dtype=tf.float32)
+    out = transfo(input_t)
+
+    assert tf.reduce_all(out == 0.5)
