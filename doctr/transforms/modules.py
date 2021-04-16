d2b2afeb5317fb707a4548bba6af8fb3d36d68aa
charlesmindee <charles@mindee.co>
2021-04-16 14:14:49 +0200

    fix: unused import

--- doctr/transforms/modules.py
+++ doctr/transforms/modules.py
@@ -3,7 +3,6 @@
 # This program is licensed under the Apache License version 2.
 # See LICENSE or go to <https://www.apache.org/licenses/LICENSE-2.0.txt> for full license details.
 
-import cv2
 import tensorflow as tf
 from typing import List, Any, Tuple, Callable
 
