95813419ae26539feac0897cf0a5f42236a4f9a6
fg-mindee <fg@mindee.co>
2021-04-30 11:14:14 +0200

    feat: Added test only option

--- references/detection/train.py
+++ references/detection/train.py
@@ -24,6 +24,62 @@ from doctr.datasets import DetectionDataset, DataLoader
 from doctr import transforms as T
 
 
+def fit_one_epoch(model, train_loader, batch_transforms, optimizer, loss_q, mb, step):
+    train_iter = iter(train_loader)
+    # Iterate over the batches of the dataset
+    for batch_step in progress_bar(range(train_loader.num_batches), parent=mb):
+        images, targets = next(train_iter)
+
+        boxes = [target['boxes'] for target in targets]
+        flags = [target['flags'] for target in targets]
+        images = batch_transforms(images)
+
+        with tf.GradientTape() as tape:
+            model_output = model(images, training=True)
+            train_loss = model.compute_loss(model_output, boxes, flags)
+        grads = tape.gradient(train_loss, model.trainable_weights)
+        optimizer.apply_gradients(zip(grads, model.trainable_weights))
+
+        mb.child.comment = f'Training loss: {train_loss.numpy():.6}'
+        # Update steps
+        step.assign_add(args.batch_size)
+        # Add loss to queue
+        loss_q.append(np.mean(train_loss))
+        # Log loss and save weights every 100 batch step
+        if batch_step % 100 == 0:
+            # Compute loss
+            loss = sum(loss_q) / len(loss_q)
+            with tb_writer.as_default():
+                tf.summary.scalar('train_loss', loss, step=step)
+
+
+def evaluate(model, val_loader, batch_transforms, val_metric):
+    # Reset val metric
+    val_metric.reset()
+    # Validation loop
+    val_loss, batch_cnt = 0, 0
+    val_iter = iter(val_loader)
+    for images, targets in val_iter:
+        boxes = [target['boxes'] for target in targets]
+        flags = [target['flags'] for target in targets]
+        images = batch_transforms(images)
+        # If we want to compute val loss, we need to pass training=True to have a thresh_map
+        model_output = model(images, training=True)
+        loss = model.compute_loss(model_output, boxes, flags)
+        decoded = model.postprocessor(model_output)
+        # Compute metric
+        for boxes_gt, boxes_pred in zip(boxes, decoded):
+            boxes_pred = np.array(boxes_pred)[:, :-1]  # Remove scores
+            val_metric.update(gts=boxes_gt, preds=boxes_pred)
+
+        val_loss += loss.numpy()
+        batch_cnt += 1
+
+    val_loss /= batch_cnt
+    recall, precision, mean_iou = val_metric.summary()
+    return val_loss, recall, precision, mean_iou
+
+
 def main(args):
 
     print(args)
@@ -81,6 +137,13 @@ def main(args):
     # Postprocessor to decode output (to feed metric during val step with boxes)
     postprocessor = model.postprocessor
 
+    if args.test_only:
+        print("Running evaluation")
+        val_loss, recall, precision, mean_iou = evaluate(model, val_loader, batch_transforms, val_metric)
+        print(f"Validation loss: {val_loss:.6} (Recall: {recall:.2%} | Precision: {precision:.2%} | "
+              f"Mean IoU: {mean_iou:.2%})")
+        return
+
     # Tensorboard to monitor training
     current_time = datetime.datetime.now().strftime("%Y%m%d-%H%M%S")
     exp_name = f"{args.model}_{current_time}" if args.name is None else args.name
@@ -96,54 +159,9 @@ def main(args):
     # Training loop
     mb = master_bar(range(args.epochs))
     for epoch in mb:
-        train_iter = iter(train_loader)
-        # Iterate over the batches of the dataset
-        for batch_step in progress_bar(range(train_loader.num_batches), parent=mb):
-            images, targets = next(train_iter)
-
-            boxes = [target['boxes'] for target in targets]
-            flags = [target['flags'] for target in targets]
-            images = batch_transforms(images)
-
-            with tf.GradientTape() as tape:
-                model_output = model(images, training=True)
-                train_loss = model.compute_loss(model_output, boxes, flags)
-            grads = tape.gradient(train_loss, model.trainable_weights)
-            optimizer.apply_gradients(zip(grads, model.trainable_weights))
-
-            mb.child.comment = f'Training loss: {train_loss.numpy():.6}'
-            # Update steps
-            step.assign_add(args.batch_size)
-            # Add loss to queue
-            loss_q.append(np.mean(train_loss))
-            # Log loss and save weights every 100 batch step
-            if batch_step % 100 == 0:
-                # Compute loss
-                loss = sum(loss_q) / len(loss_q)
-                with tb_writer.as_default():
-                    tf.summary.scalar('train_loss', loss, step=step)
-
+        fit_one_epoch(model, train_loader, batch_transforms, optimizer, loss_q, mb, step)
         # Validation loop at the end of each epoch
-        val_loss, batch_cnt = 0, 0
-        val_iter = iter(val_loader)
-        for images, targets in val_iter:
-            boxes = [target['boxes'] for target in targets]
-            flags = [target['flags'] for target in targets]
-            images = batch_transforms(images)
-            # If we want to compute val loss, we need to pass training=True to have a thresh_map
-            model_output = model(images, training=True)
-            loss = model.compute_loss(model_output, boxes, flags)
-            decoded = postprocessor(model_output)
-            # Compute metric
-            for boxes_gt, boxes_pred in zip(boxes, decoded):
-                boxes_pred = np.array(boxes_pred)[:, :-1]  # Remove scores
-                val_metric.update(gts=boxes_gt, preds=boxes_pred)
-
-            val_loss += loss.numpy()
-            batch_cnt += 1
-
-        val_loss /= batch_cnt
-        recall, precision, mean_iou = val_metric.summary()
+        val_loss, recall, precision, mean_iou = evaluate(model, val_loader, batch_transforms, val_metric)
         if val_loss < min_loss:
             print(f"Validation loss decreased {min_loss:.6} --> {val_loss:.6}: saving state...")
             model.save_weights(f'./{exp_name}/weights')
@@ -174,6 +192,7 @@ def parse_args():
     parser.add_argument('--lr', type=float, default=0.001, help='learning rate for the optimizer (Adam)')
     parser.add_argument('-j', '--workers', type=int, default=4, help='number of workers used for dataloading')
     parser.add_argument('--resume', type=str, default=None, help='Path to your checkpoint')
+    parser.add_argument("--test-only", dest='test_only', action='store_true', help="Run the validation loop")
     args = parser.parse_args()
 
     return args
