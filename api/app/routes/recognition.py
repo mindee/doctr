49243fda7b978252ac91cbb5001ac67b4cd62826
fg-mindee <fg@mindee.co>
2021-05-05 17:40:19 +0200

    refactor: Removed unused imports

--- api/app/routes/recognition.py
+++ api/app/routes/recognition.py
@@ -3,8 +3,7 @@
 # This program is licensed under the Apache License version 2.
 # See LICENSE or go to <https://www.apache.org/licenses/LICENSE-2.0.txt> for full license details.
 
-from typing import List
-from fastapi import APIRouter, UploadFile, File, HTTPException, status
+from fastapi import APIRouter, UploadFile, File
 
 import tensorflow as tf
 
