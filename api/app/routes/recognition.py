22578d208444d7f67653515f4353f6fd9f511c17
fg-mindee <fg@mindee.co>
2021-05-07 13:27:59 +0200

    refactor: Refactored detection and recognition routes

--- api/app/routes/recognition.py
+++ api/app/routes/recognition.py
@@ -5,18 +5,9 @@
 
 from fastapi import APIRouter, UploadFile, File
 
-import tensorflow as tf
-
-gpu_devices = tf.config.experimental.list_physical_devices('GPU')
-if any(gpu_devices):
-    tf.config.experimental.set_memory_growth(gpu_devices[0], True)
-
-from doctr.models import recognition_predictor
-
+from app.vision import decode_image, reco_predictor
 from app.schemas import RecognitionOut
 
-
-reco_predictor = recognition_predictor(pretrained=True)
 
 router = APIRouter()
 
@@ -24,6 +15,6 @@ router = APIRouter()
 @router.post("/", response_model=RecognitionOut, status_code=200, summary="Perform text recognition")
 async def text_recognition(file: UploadFile = File(...)):
     """Runs DocTR text recognition model to analyze the input"""
-    img = tf.io.decode_image(file.file.read())
+    img = decode_image(file.file.read())
     out = reco_predictor(img[None, ...], training=False)
     return RecognitionOut(value=out[0])
