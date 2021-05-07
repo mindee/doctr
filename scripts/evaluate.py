f3b1f6d1740f76456a78d8c83b2b1a9fcaf60fb8
charlesmindee <charles@mindee.co>
2021-05-07 17:44:30 +0200

    add: tests

--- scripts/evaluate.py
+++ scripts/evaluate.py
@@ -44,7 +44,6 @@ def main(args):
 
             # Forward
             out = model([page])
-            # Crop GT
             crops = extract_crops(page, gt_boxes)
             reco_out = model.reco_predictor(crops)
 
@@ -69,7 +68,7 @@ def main(args):
             e2e_metric.update(gt_boxes, np.asarray(pred_boxes), gt_labels, pred_labels)
 
     # Unpack aggregated metrics
-    print(f"Model Evaluation (model= {args.detection} + {args.recognition}, dataset='FUNSD')")
+    print(f"Model Evaluation (model= {args.detection} + {args.recognition}, dataset={args.dataset})")
     recall, precision, mean_iou = det_metric.summary()
     print(f"Text Detection - Recall: {recall:.2%}, Precision: {precision:.2%}, Mean IoU: {mean_iou:.2%}")
     acc = reco_metric.summary()
@@ -86,8 +85,8 @@ def parse_args():
     parser.add_argument('detection', type=str, help='Text detection model to use for analysis')
     parser.add_argument('recognition', type=str, help='Text recognition model to use for analysis')
     parser.add_argument('--iou', type=float, default=0.5, help='IoU threshold to match a pair of boxes')
-    parser.add_argument('-dataset', type=str, default='FUNSD', help='choose a dataset: FUNSD, CORD')
-    parser.add_argument('-path', type=str, default=None, help='Only for local sets, path to your OCRDataset')
+    parser.add_argument('--dataset', type=str, default='FUNSD', help='choose a dataset: FUNSD, CORD')
+    parser.add_argument('--path', type=str, default=None, help='Only for local sets, path to your OCRDataset')
     args = parser.parse_args()
 
     return args
