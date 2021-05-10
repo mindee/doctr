ccc84c6a395638a8362cf0442f1913172bebc0fb
Charles Gaillard <charles@mindee.co>
2021-05-10 11:28:45 +0200

    feat: add testset in datasets (#244)
    * feat: add testset
    
    * feat: add testset to script
    
    * feat: add testset to script
    
    * feat: add testset to script
    
    * refacto: ocrdataset
    
    * refacto: scripts
    
    * fix: sroie
    
    * add: tests
    
    * refacto: separate files
    
    * fix: est
    
    * fix: script
    
    * feat:add in docs
    
    * refacto: 2 args for ocrdataset

--- doctr/datasets/funsd.py
+++ doctr/datasets/funsd.py
@@ -10,7 +10,6 @@ from pathlib import Path
 from typing import List, Dict, Any, Tuple, Optional, Callable
 import tensorflow as tf
 
-from doctr.documents.reader import read_img
 from .core import VisionDataset
 
 __all__ = ['FUNSD']
