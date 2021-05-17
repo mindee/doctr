75ae2f2b47d0a0e9985e728e7e45f8277b3a3100
Charles Gaillard <charles@mindee.co>
2021-05-17 15:08:17 +0200

    feat: add BarCodeDetector (#260)
    * feat: add BarCodeDetector
    
    * fix: rel coor
    
    * fix: rel coor
    
    * refacto: param init
    
    * fix: flake8

--- test/test_artefacts.py
+++ test/test_artefacts.py
@@ -1,6 +1,14 @@
 import os
-from doctr.models.artefacts import FaceDetector
+from doctr.models.artefacts import BarCodeDetector, FaceDetector
 from doctr.documents import DocumentFile
+
+
+def test_qr_code_detector(mock_image_folder):
+    detector = BarCodeDetector()
+    for img in os.listdir(mock_image_folder):
+        image = DocumentFile.from_images(os.path.join(mock_image_folder, img))[0]
+        barcode = detector(image)
+        assert len(barcode) == 0
 
 
 def test_face_detector(mock_image_folder):
