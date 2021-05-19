d082ae2c7e19615c3cd0874afaf7c355d158065f
charlesmindee <charles@mindee.co>
2021-05-19 15:48:26 +0200

    fix: remove test

--- test/test_artefacts.py
+++ test/test_artefacts.py
@@ -1,5 +1,5 @@
 import os
-from doctr.models.artefacts import BarCodeDetector, FaceDetector, QRCodeDetector
+from doctr.models.artefacts import QRCodeDetector
 from doctr.documents import DocumentFile
 
 
@@ -9,19 +9,3 @@ def test_qr_code_detector(mock_image_folder):
         image = DocumentFile.from_images(os.path.join(mock_image_folder, img))[0]
         qrcode = detector(image)
         assert qrcode is None
-
-
-def test_bar_code_detector(mock_image_folder):
-    detector = BarCodeDetector()
-    for img in os.listdir(mock_image_folder):
-        image = DocumentFile.from_images(os.path.join(mock_image_folder, img))[0]
-        barcode = detector(image)
-        assert len(barcode) == 0
-
-
-def test_face_detector(mock_image_folder):
-    detector = FaceDetector(n_faces=1)
-    for img in os.listdir(mock_image_folder):
-        image = DocumentFile.from_images(os.path.join(mock_image_folder, img))[0]
-        faces = detector(image)
-        assert len(faces) <= 1
