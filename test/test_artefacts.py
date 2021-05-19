68661a2e62b34ff76125736c4bf570f2ee737e0b
charlesmindee <charles@mindee.co>
2021-05-19 16:22:55 +0200

    fix: cv2

--- test/test_artefacts.py
+++ test/test_artefacts.py
@@ -1,10 +1,27 @@
 import os
-from doctr.models.artefacts import QRCodeDetector
 import numpy as np
+from doctr.models.artefacts import BarCodeDetector, FaceDetector, QRCodeDetector
+from doctr.documents import DocumentFile
 
 
 def test_qr_code_detector(mock_image_folder):
     detector = QRCodeDetector()
-    image = np.random.random_sample((512, 512, 3))
+    image = np.random.randint(low=0, high=255, size=(512, 512, 3))
     qrcode = detector(image)
     assert qrcode is None
+
+
+def test_bar_code_detector(mock_image_folder):
+    detector = BarCodeDetector()
+    for img in os.listdir(mock_image_folder):
+        image = DocumentFile.from_images(os.path.join(mock_image_folder, img))[0]
+        barcode = detector(image)
+        assert len(barcode) == 0
+
+
+def test_face_detector(mock_image_folder):
+    detector = FaceDetector(n_faces=1)
+    for img in os.listdir(mock_image_folder):
+        image = DocumentFile.from_images(os.path.join(mock_image_folder, img))[0]
+        faces = detector(image)
+        assert len(faces) <= 1
