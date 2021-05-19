a1308ce33356f162ceda329fdaa27124c932a9a0
charlesmindee <charles@mindee.co>
2021-05-19 15:56:15 +0200

    fix: np img

--- test/test_artefacts.py
+++ test/test_artefacts.py
@@ -1,11 +1,10 @@
 import os
 from doctr.models.artefacts import QRCodeDetector
-from doctr.documents import DocumentFile
+import numpy as np
 
 
 def test_qr_code_detector(mock_image_folder):
     detector = QRCodeDetector()
-    for img in os.listdir(mock_image_folder):
-        image = DocumentFile.from_images(os.path.join(mock_image_folder, img))[0]
-        qrcode = detector(image)
-        assert qrcode is None
+    image = np.random.random_sample((512, 512))
+    qrcode = detector(image)
+    assert qrcode is None
