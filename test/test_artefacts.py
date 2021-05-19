e0d523b9d038b0549841eaed3633193087c81736
charlesmindee <charles@mindee.co>
2021-05-19 16:32:37 +0200

    fix: np img

--- test/test_artefacts.py
+++ test/test_artefacts.py
@@ -1,7 +1,6 @@
 import os
 import numpy as np
 from doctr.models.artefacts import BarCodeDetector, FaceDetector, QRCodeDetector
-from doctr.documents import DocumentFile
 
 
 def test_qr_code_detector(mock_image_folder):
@@ -13,15 +12,13 @@ def test_qr_code_detector(mock_image_folder):
 
 def test_bar_code_detector(mock_image_folder):
     detector = BarCodeDetector()
-    for img in os.listdir(mock_image_folder):
-        image = DocumentFile.from_images(os.path.join(mock_image_folder, img))[0]
-        barcode = detector(image)
-        assert len(barcode) == 0
+    image = np.random.randint(low=0, high=255, size=(512, 512, 3), dtype=np.uint8)
+    barcode = detector(image)
+    assert len(barcode) == 0
 
 
 def test_face_detector(mock_image_folder):
     detector = FaceDetector(n_faces=1)
-    for img in os.listdir(mock_image_folder):
-        image = DocumentFile.from_images(os.path.join(mock_image_folder, img))[0]
-        faces = detector(image)
-        assert len(faces) <= 1
+    image = np.random.randint(low=0, high=255, size=(512, 512, 3), dtype=np.uint8)
+    faces = detector(image)
+    assert len(faces) <= 1
