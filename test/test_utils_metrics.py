93e1f8bf9b84c4de7a6b2e9cac86c467f5295d9d
fg-mindee <fg@mindee.co>
2021-05-20 22:21:00 +0200

    test: Updated unittests

--- test/test_utils_metrics.py
+++ test/test_utils_metrics.py
@@ -55,12 +55,12 @@ def test_box_iou(box1, box2, iou, abs_tol):
 )
 def test_localization_confusion(gts, preds, iou_thresh, recall, precision, mean_iou):
 
-    metric = metrics.LocalizationConfusion()
+    metric = metrics.LocalizationConfusion(iou_thresh)
     for _gts, _preds in zip(gts, preds):
         metric.update(np.asarray(_gts), np.zeros((0, 4)) if _preds is None else np.asarray(_preds))
-    assert metric.summary(iou_thresh) == (recall, precision, mean_iou)
+    assert metric.summary() == (recall, precision, mean_iou)
     metric.reset()
-    assert metric.num_gts == metric.num_preds == len(metric.ious) == metric.tot_iou == 0
+    assert metric.num_gts == metric.num_preds == metric.matches == metric.tot_iou == 0
 
 
 @pytest.mark.parametrize(
@@ -103,7 +103,7 @@ def test_localization_confusion(gts, preds, iou_thresh, recall, precision, mean_
 def test_ocr_metric(
     gt_boxes, gt_words, pred_boxes, pred_words, iou_thresh, recall, precision, mean_iou
 ):
-    metric = metrics.OCRMetric()
+    metric = metrics.OCRMetric(iou_thresh)
     for _gboxes, _gwords, _pboxes, _pwords in zip(gt_boxes, gt_words, pred_boxes, pred_words):
         metric.update(
             np.asarray(_gboxes),
@@ -111,9 +111,10 @@ def test_ocr_metric(
             _gwords,
             _pwords
         )
-    _recall, _precision, _mean_iou = metric.summary(iou_thresh)
+    _recall, _precision, _mean_iou = metric.summary()
     assert _recall == recall
     assert _precision == precision
     assert _mean_iou == mean_iou
     metric.reset()
-    assert metric.num_gts == metric.num_preds == metric.tot_iou == len(metric.ious) == len(metric.text_matches) == 0
+    assert metric.num_gts == metric.num_preds == metric.tot_iou == 0
+    assert metric.raw_matches == metric.caseless_matches == metric.unidecode_matches == metric.unicase_matches == 0
