3a0d04ad1537444545ef1cb934d611496bf343c6
fg-mindee <fg@mindee.co>
2021-05-20 21:43:33 +0200

    test: Updated unittests

--- test/test_utils_metrics.py
+++ test/test_utils_metrics.py
@@ -4,39 +4,25 @@ from doctr.utils import metrics
 
 
 @pytest.mark.parametrize(
-    "gt, pred, ignore_case, ignore_accents, result",
+    "gt, pred, raw, caseless, unidecode, unicase",
     [
-        [['grass', '56', 'True', 'STOP'], ['grass', '56', 'true', 'stop'], True, False, 1.0],
-        [['grass', '56', 'True', 'STOP'], ['grass', '56', 'true', 'stop'], False, False, .5],
-        [['éléphant'], ['elephant'], False, True, 1.0],
+        [['grass', '56', 'True', 'EUR'], ['grass', '56', 'true', '€'], .5, .75, .75, 1],
+        [['éléphant', 'ça'], ['elephant', 'ca'], 0, 0, 1, 1],
     ],
 )
-def test_exact_match(gt, pred, ignore_case, ignore_accents, result):
-    metric = metrics.ExactMatch(ignore_case, ignore_accents)
+def test_text_match(gt, pred, raw, caseless, unidecode, unicase):
+    metric = metrics.TextMatch()
     with pytest.raises(AssertionError):
         metric.summary()
-    if ignore_accents:
-        with pytest.raises(NotImplementedError):
-            metric.update(gt, pred)
-    else:
-        metric.update(gt, pred)
-        assert metric.summary() == result
-        metric.reset()
-        assert metric.matches == metric.total == 0
 
+    with pytest.raises(AssertionError):
+        metric.update(['a', 'b'], ['c'])
 
-@pytest.mark.parametrize(
-    "mat, row_indices, col_indices",
-    [
-        [[[1, 0, 0], [0, 1, 0], [0, 0, 1]], [0, 1, 2], [0, 1, 2]],  # Perfect diagonal
-        [[[1, 0, 0], [0, .4, 0], [0, 0, 1]], [0, 2], [0, 2]],  # below threshold
-        [[[1, 0, 0], [0, 1, 0], [0, 0, 0.5], [0, 0, 1]], [0, 1, 3], [0, 1, 2]],  # rectangular matrix
-    ],
-)
-def test_assign_pairs(mat, row_indices, col_indices):
-    gt_idxs, pred_idxs = metrics.assign_pairs(np.asarray(mat))
-    assert all(a == b for a, b in zip(row_indices, gt_idxs)), print(gt_idxs)
-    assert all(a == b for a, b in zip(col_indices, pred_idxs)), print(pred_idxs)
+    metric.update(gt, pred)
+    assert metric.summary() == dict(raw=raw, caseless=caseless, unidecode=unidecode, unicase=unicase)
+
+    metric.reset()
+    assert metric.raw == metric.caseless == metric.unidecode == metric.unicase == metric.total == 0
 
 
 @pytest.mark.parametrize(
@@ -69,43 +55,55 @@ def test_box_iou(box1, box2, iou, abs_tol):
 )
 def test_localization_confusion(gts, preds, iou_thresh, recall, precision, mean_iou):
 
-    metric = metrics.LocalizationConfusion(iou_thresh)
+    metric = metrics.LocalizationConfusion()
     for _gts, _preds in zip(gts, preds):
         metric.update(np.asarray(_gts), np.zeros((0, 4)) if _preds is None else np.asarray(_preds))
-    assert metric.summary() == (recall, precision, mean_iou)
+    assert metric.summary(iou_thresh) == (recall, precision, mean_iou)
     metric.reset()
-    assert metric.num_matches == metric.num_gts == metric.num_preds == 0
+    assert metric.num_gts == metric.num_preds == len(metric.ious) == metric.tot_iou == 0
 
 
 @pytest.mark.parametrize(
-    "gt_boxes, gt_words, pred_boxes, pred_words, iou_thresh, max_dist, recall, precision, mean_iou, mean_distance",
+    "gt_boxes, gt_words, pred_boxes, pred_words, iou_thresh, recall, precision, mean_iou",
     [
         [  # Perfect match
             [[[0, 0, .5, .5]]], [["elephant"]],
             [[[0, 0, .5, .5]]], [["elephant"]],
-            0.5, 0, 1, 1, 1, 0
+            0.5,
+            {"raw": 1, "caseless": 1, "unidecode": 1, "unicase": 1},
+            {"raw": 1, "caseless": 1, "unidecode": 1, "unicase": 1},
+            1,
         ],
         [  # Bad match
             [[[0, 0, .5, .5]]], [["elefant"]],
             [[[0, 0, .5, .5]]], [["elephant"]],
-            0.5, 1, 0, 0, 1, 2
+            0.5,
+            {"raw": 0, "caseless": 0, "unidecode": 0, "unicase": 0},
+            {"raw": 0, "caseless": 0, "unidecode": 0, "unicase": 0},
+            1,
         ],
         [  # Good match
-            [[[0, 0, 1, 1]]], [["home"]],
-            [[[0, 0, .5, .5], [.6, .6, .7, .7]]], [["hom", "e"]],
-            0.2, 1, 1, 0.5, 0.125, 1
+            [[[0, 0, 1, 1]]], [["EUR"]],
+            [[[0, 0, .5, .5], [.6, .6, .7, .7]]], [["€", "e"]],
+            0.2,
+            {"raw": 0, "caseless": 0, "unidecode": 1, "unicase": 1},
+            {"raw": 0, "caseless": 0, "unidecode": .5, "unicase": .5},
+            0.125,
         ],
         [  # No preds on 2nd sample
-            [[[0, 0, .5, .5]], [[0, 0, .5, .5]]], [["elephant"], ["elephant"]],
+            [[[0, 0, .5, .5]], [[0, 0, .5, .5]]], [["Elephant"], ["elephant"]],
             [[[0, 0, .5, .5]], None], [["elephant"], []],
-            0.5, 0, 0.5, 1, 1, 0
+            0.5,
+            {"raw": 0, "caseless": .5, "unidecode": 0, "unicase": .5},
+            {"raw": 0, "caseless": 1, "unidecode": 0, "unicase": 1},
+            1,
         ],
     ],
 )
 def test_ocr_metric(
-    gt_boxes, gt_words, pred_boxes, pred_words, iou_thresh, max_dist, recall, precision, mean_iou, mean_distance
+    gt_boxes, gt_words, pred_boxes, pred_words, iou_thresh, recall, precision, mean_iou
 ):
-    metric = metrics.OCRMetric(iou_thresh, max_dist)
+    metric = metrics.OCRMetric()
     for _gboxes, _gwords, _pboxes, _pwords in zip(gt_boxes, gt_words, pred_boxes, pred_words):
         metric.update(
             np.asarray(_gboxes),
@@ -113,7 +111,9 @@ def test_ocr_metric(
             _gwords,
             _pwords
         )
-    assert metric.summary() == (recall, precision, mean_iou, mean_distance)
+    _recall, _precision, _mean_iou = metric.summary(iou_thresh)
+    assert _recall == recall
+    assert _precision == precision
+    assert _mean_iou == mean_iou
     metric.reset()
-    assert metric.num_reco_matches == metric.num_det_matches == metric.num_gts == metric.num_preds == 0
-    assert metric.tot_iou == metric.tot_dist == 0.
+    assert metric.num_gts == metric.num_preds == metric.tot_iou == len(metric.ious) == len(metric.text_matches) == 0
