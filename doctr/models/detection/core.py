3b104deb65cd9ed4be210fa517d5aba103715da0
F-G Fernandez <76527547+fg-mindee@users.noreply.github.com>
2021-06-03 14:08:59 +0200

    refactor: Removed unused imports (#295)

--- doctr/models/detection/core.py
+++ doctr/models/detection/core.py
@@ -3,7 +3,7 @@
 # This program is licensed under the Apache License version 2.
 # See LICENSE or go to <https://www.apache.org/licenses/LICENSE-2.0.txt> for full license details.
 
-from numpy.core.defchararray import startswith
+
 import tensorflow as tf
 from tensorflow import keras
 import numpy as np
