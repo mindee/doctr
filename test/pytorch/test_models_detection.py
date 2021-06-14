f132833cbd19bf0c23feb0832b5e657b1e54310e
fg-mindee <fg@mindee.co>
2021-06-14 17:39:35 +0200

    test: Updated pytorch unittests

--- test/pytorch/test_models_detection.py
+++ test/pytorch/test_models_detection.py
@@ -1,7 +1,8 @@
 import pytest
 import torch
+import numpy as np
 
-from doctr.models.detection import differentiable_binarization_pt as db
+from doctr.models import detection
 
 
 @pytest.mark.parametrize(
@@ -14,15 +15,24 @@ from doctr.models.detection import differentiable_binarization_pt as db
 )
 def test_detection_models(arch_name, input_shape, output_size, out_prob):
     batch_size = 2
-    model = db.__dict__[arch_name](pretrained=False).eval()
+    model = detection.__dict__[arch_name](pretrained=False).eval()
     assert isinstance(model, torch.nn.Module)
     input_tensor = torch.rand((batch_size, *input_shape))
-
-    with torch.no_grad():
-        out = model(input_tensor, return_model_output=True)
+    target = [
+        dict(boxes=np.array([[.5, .5, 1, 1], [0.5, 0.5, .8, .8]], dtype=np.float32), flags=[True, False]),
+        dict(boxes=np.array([[.5, .5, 1, 1], [0.5, 0.5, .8, .9]], dtype=np.float32), flags=[True, False])
+    ]
+    out = model(input_tensor, target, return_model_output=True, return_boxes=True)
     assert isinstance(out, dict)
-    assert len(out) == 1
+    assert len(out) == 3
     # Check proba map
     assert out['out_map'].shape == (batch_size, *output_size)
     if out_prob:
         assert torch.all((out['out_map'] >= 0) & (out['out_map'] <= 1))
+    # Check boxes
+    for boxes in out['preds'][0]:
+        assert boxes.shape[1] == 5
+        assert np.all(boxes[:, :2] < boxes[:, 2:4])
+        assert np.all(boxes[:, :4] >= 0) and np.all(boxes[:, :4] <= 1)
+    # Check loss
+    assert isinstance(out['loss'], torch.Tensor)
