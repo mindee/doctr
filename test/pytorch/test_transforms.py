9c9c25f985f5447ef6c36555436b514fe3ab7cec
fg-mindee <fg@mindee.co>
2021-06-15 14:12:13 +0200

    test: Added unittest for PyTorch transforms

---
+++ test/pytorch/test_transforms.py
@@ -0,0 +1,41 @@
+import pytest
+import math
+
+import torch
+from doctr import transforms as T
+
+
+def test_resize():
+    output_size = (32, 32)
+    transfo = T.Resize(output_size)
+    input_t = torch.ones((3, 64, 64), dtype=torch.float32)
+    out = transfo(input_t)
+
+    assert torch.all(out == 1)
+    assert out.shape[-2:] == output_size
+    assert repr(transfo) == f"Resize(output_size={output_size}, interpolation='bilinear')"
+
+    transfo = T.Resize(output_size, preserve_aspect_ratio=True)
+    input_t = torch.ones((3, 32, 64), dtype=torch.float32)
+    out = transfo(input_t)
+
+    assert not torch.all(out == 1)
+    # Asymetric padding
+    # import ipdb; ipdb.set_trace()
+    assert torch.all(out[:, -1] == 0) and torch.all(out[:, 0] == 1)
+    assert out.shape[-2:] == output_size
+
+    # Symetric padding
+    transfo = T.Resize(output_size, preserve_aspect_ratio=True, symmetric_pad=True)
+    assert repr(transfo) == (f"Resize(output_size={output_size}, interpolation='bilinear', "
+                             f"preserve_aspect_ratio=True, symmetric_pad=True)")
+    out = transfo(input_t)
+    # Asymetric padding
+    assert torch.all(out[:, -1] == 0) and torch.all(out[:, 0] == 0)
+
+    # Inverse aspect ratio
+    input_t = torch.ones((3, 64, 32), dtype=torch.float32)
+    out = transfo(input_t)
+
+    assert not torch.all(out == 1)
+    assert out.shape[-2:] == output_size
