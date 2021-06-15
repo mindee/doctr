d182536f2ece23fe8f4e24d78a3f4590024bcb47
fg-mindee <fg@mindee.co>
2021-06-15 23:26:25 +0200

    refactor: Refactored file_utils

--- doctr/file_utils.py
+++ doctr/file_utils.py
@@ -6,15 +6,7 @@
 # Adapted from https://github.com/huggingface/transformers/blob/master/src/transformers/file_utils.py
 
 import os
-import sys
 import logging
-import importlib.util
-
-if sys.version_info < (3, 8):
-    import importlib_metadata
-else:
-    import importlib.metadata as importlib_metadata
-
 
 __all__ = ['is_tf_available', 'is_torch_available']
 
@@ -26,51 +18,31 @@ USE_TORCH = os.environ.get("USE_TORCH", "0").upper()
 
 
 if USE_TORCH in ENV_VARS_TRUE_AND_AUTO_VALUES and USE_TF not in ENV_VARS_TRUE_VALUES:
-    _torch_available = importlib.util.find_spec("torch") is not None
-    if _torch_available:
-        try:
-            _torch_version = importlib_metadata.version("torch")
-            logging.info(f"PyTorch version {_torch_version} available.")
-        except importlib_metadata.PackageNotFoundError:
-            _torch_available = False
+    _torch_available = True
+    try:
+        import torch
+        logging.info(f"PyTorch version {torch.__version__} available.")
+    except ModuleNotFoundError:
+        _torch_available = False
 else:
     logging.info("Disabling PyTorch because USE_TF is set")
     _torch_available = False
 
 
 if USE_TF in ENV_VARS_TRUE_AND_AUTO_VALUES and USE_TORCH not in ENV_VARS_TRUE_VALUES:
-    _tf_available = importlib.util.find_spec("tensorflow") is not None
-    if _tf_available:
-        candidates = (
-            "tensorflow",
-            "tensorflow-cpu",
-            "tensorflow-gpu",
-            "tf-nightly",
-            "tf-nightly-cpu",
-            "tf-nightly-gpu",
-            "intel-tensorflow",
-            "tensorflow-rocm",
-            "tensorflow-macos",
-        )
-        _tf_version = None
-        # For the metadata, we have to look for both tensorflow and tensorflow-cpu
-        for pkg in candidates:
-            try:
-                _tf_version = importlib_metadata.version(pkg)
-                break
-            except importlib_metadata.PackageNotFoundError:
-                pass
-        _tf_available = _tf_version is not None
-    if _tf_available:
-        if int(_tf_version.split('.')[0]) < 2:  # type: ignore[union-attr]
-            logging.info(f"TensorFlow found but with version {_tf_version}. Transformers requires version 2 minimum.")
+    _tf_available = True
+    try:
+        import tensorflow as tf
+        if int(tf.__version__.split('.')[0]) < 2:
+            logging.info(f"TensorFlow found but with version {tf.__version__}. DocTR requires version 2 minimum.")
             _tf_available = False
         else:
-            logging.info(f"TensorFlow version {_tf_version} available.")
+            logging.info(f"TensorFlow version {tf.__version__} available.")
+    except ModuleNotFoundError:
+        _tf_available = False
 else:
     logging.info("Disabling Tensorflow because USE_TORCH is set")
     _tf_available = False
-
 
 def is_torch_available():
     return _torch_available
