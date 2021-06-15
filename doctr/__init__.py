16a12220e41109254eeecbf4c51eee9061e251ea
fg-mindee <fg@mindee.co>
2021-06-15 16:58:33 +0200

    chore: Fixed core import

--- doctr/__init__.py
+++ doctr/__init__.py
@@ -1,4 +1,4 @@
-from .file_utils import is_tf_available
+from .file_utils import is_tf_available, is_torch_available
 from .version import __version__  # noqa: F401
 from . import documents, models, transforms, utils
 
