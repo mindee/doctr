839fd7de41cabd87ef0cb7f684d102e22c5aeee6
fg-mindee <fg@mindee.co>
2021-06-16 01:59:33 +0200

    chore: Fixed imports

--- doctr/models/detection/__init__.py
+++ doctr/models/detection/__init__.py
@@ -1,8 +1,4 @@
+from .core import *
 from .differentiable_binarization import *
 from .linknet import *
 from .zoo import *
-
-from doctr import is_tf_available
-
-if is_tf_available():
-    from .core import *
