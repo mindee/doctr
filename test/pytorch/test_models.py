2b5bef46c6f253ed6283d1ed74a93597aa75d987
fg-mindee <fg@mindee.co>
2021-06-15 15:19:57 +0200

    test: Updated unittest

--- test/pytorch/test_models.py
+++ test/pytorch/test_models.py
@@ -25,29 +25,28 @@ def test_preprocessor(mock_pdf):
     processor = models.PreProcessor(output_size=(512, 512), batch_size=batch_size)
     batched_docs = processor([page for doc in docs for page in doc])
 
-    # # Number of batches
-    # assert len(batched_docs) == math.ceil(8 * num_docs / batch_size)
-    # # Total number of samples
-    # assert sum(batch.shape[0] for batch in batched_docs) == 8 * num_docs
-    # # Batch size
-    # assert all(batch.shape[0] == batch_size for batch in batched_docs[:-1])
-    # assert batched_docs[-1].shape[0] == batch_size if (8 * num_docs) % batch_size == 0 else (8 * num_docs) % batch_size
-    # # Data type
-    # assert all(batch.dtype == tf.float32 for batch in batched_docs)
-    # # Image size
-    # assert all(batch.shape[1:] == (3, 512, 512) for batch in batched_docs)
-    # # Test with non-full last batch
-    # batch_size = 16
-    # processor = models.PreProcessor(output_size=(512, 512), batch_size=batch_size)
-    # batched_docs = processor([page for doc in docs for page in doc])
-    # assert batched_docs[-1].shape[0] == (8 * num_docs) % batch_size
+    # Number of batches
+    assert len(batched_docs) == math.ceil(8 * num_docs / batch_size)
+    # Total number of samples
+    assert sum(batch.shape[0] for batch in batched_docs) == 8 * num_docs
+    # Batch size
+    assert all(batch.shape[0] == batch_size for batch in batched_docs[:-1])
+    assert batched_docs[-1].shape[0] == batch_size if (8 * num_docs) % batch_size == 0 else (8 * num_docs) % batch_size
+    # Data type
+    assert all(batch.dtype == tf.float32 for batch in batched_docs)
+    # Image size
+    assert all(batch.shape[1:] == (3, 512, 512) for batch in batched_docs)
+    # Test with non-full last batch
+    batch_size = 16
+    processor = models.PreProcessor(output_size=(512, 512), batch_size=batch_size)
+    batched_docs = processor([page for doc in docs for page in doc])
+    assert batched_docs[-1].shape[0] == (8 * num_docs) % batch_size
 
-    # # Repr
-    # assert len(repr(processor).split('\n')) == 9
+    # Repr
+    assert len(repr(processor).split('\n')) == 9
 
-    # # Assymetric
-    # processor = models.PreProcessor(output_size=(256, 128), batch_size=batch_size, preserve_aspect_ratio=True)
-    # batched_docs = processor([page for doc in docs for page in doc])
-    # # Image size
-    # assert all(batch.shape[1:] == (3, 256, 128) for batch in batched_docs)
-
+    # Assymetric
+    processor = models.PreProcessor(output_size=(256, 128), batch_size=batch_size, preserve_aspect_ratio=True)
+    batched_docs = processor([page for doc in docs for page in doc])
+    # Image size
+    assert all(batch.shape[1:] == (3, 256, 128) for batch in batched_docs)
