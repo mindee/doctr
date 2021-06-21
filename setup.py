2c24b2b42139b820ce01e437ba61bf16677f84dc
fg-mindee <fg@mindee.co>
2021-06-21 15:42:49 +0200

    chore: Added tqdm to requirements

--- setup.py
+++ setup.py
@@ -54,6 +54,7 @@ _deps = [
     "tensorflow-cpu>=2.4.0",
     "torch>=1.8.0",
     "torchvision>=0.9.0",
+    "tqdm>=4.30.0",
 ]
 
 deps = {b: a for a, b in (re.findall(r"^(([^!=<>]+)(?:[!=<>].*)?$)", x)[0] for x in _deps)}
@@ -75,6 +76,7 @@ install_requires = [
     deps["mplcursors"],
     deps["weasyprint"],
     deps["unidecode"],
+    deps["tqdm"],
 ]
 
 extras = {}
