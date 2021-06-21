31e5442f45687cb0917d27e1476689aae7c849dd
fg-mindee <fg@mindee.co>
2021-06-21 16:09:15 +0200

    fix: Fixed datasets imports

--- doctr/datasets/__init__.py
+++ doctr/datasets/__init__.py
@@ -1,10 +1,13 @@
 from .utils import *
 from .vocabs import *
-
-from .loader import *
 from .funsd import *
 from .cord import *
 from .detection import *
 from .recognition import *
 from .ocr import *
 from .sroie import *
+
+from doctr.file_utils import is_tf_available
+
+if is_tf_available():
+    from .loader import *
