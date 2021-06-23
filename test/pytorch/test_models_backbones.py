8557bf605c7627056c605d7245ac31878d133280
fg-mindee <fg@mindee.co>
2021-06-23 15:08:35 +0200

    test: Optimized backbone testing

--- test/pytorch/test_models_backbones.py
+++ test/pytorch/test_models_backbones.py
@@ -13,9 +13,10 @@ from doctr.models import backbones
 def test_classification_architectures(arch_name, input_shape, output_size):
     # Model
     batch_size = 2
-    model = backbones.__dict__[arch_name](pretrained=True)
+    model = backbones.__dict__[arch_name](pretrained=True).eval()
     # Forward
-    out = model(torch.rand((batch_size, *input_shape), dtype=torch.float32))
+    with torch.no_grad():
+        out = model(torch.rand((batch_size, *input_shape), dtype=torch.float32))
     # Output checks
     assert isinstance(out, torch.Tensor)
     assert out.numpy().shape == (batch_size, *output_size)
