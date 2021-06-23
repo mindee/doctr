37fadcfd6aac8dd049817be1e8b4961f13036b5a
fg-mindee <fg@mindee.co>
2021-06-23 15:07:53 +0200

    test: Fixed dtype

--- test/pytorch/test_models_backbones.py
+++ test/pytorch/test_models_backbones.py
@@ -15,7 +15,7 @@ def test_classification_architectures(arch_name, input_shape, output_size):
     batch_size = 2
     model = backbones.__dict__[arch_name](pretrained=True)
     # Forward
-    out = model(torch.rand((batch_size, *input_shape), dtype=tf.float32))
+    out = model(torch.rand((batch_size, *input_shape), dtype=torch.float32))
     # Output checks
     assert isinstance(out, torch.Tensor)
     assert out.numpy().shape == (batch_size, *output_size)
