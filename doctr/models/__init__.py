2d507c7736caff969334d738c1b3e4550a5aae68
fg-mindee <fg@mindee.co>
2021-06-27 21:17:47 +0200

    chore: Updated imports

--- doctr/models/__init__.py
+++ doctr/models/__init__.py
@@ -3,13 +3,13 @@ from .detection import *
 from . import artefacts
 from . import utils
 from ._utils import *
+from .backbones import *
+from .recognition import *
 
 
 from doctr.file_utils import is_tf_available
 
 if is_tf_available():
-    from .backbones import *
-    from .recognition import *
     from .core import *
     from .export import *
     from .zoo import *
