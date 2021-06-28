08081623a0421d219f5996ae10c198730d5b5bee
fg-mindee <fg@mindee.co>
2021-06-28 23:54:14 +0200

    feat: Enabled recognition zoo

--- doctr/models/recognition/__init__.py
+++ doctr/models/recognition/__init__.py
@@ -1,9 +1,9 @@
 from .core import *
 from .crnn import *
 from .sar import *
+from .zoo import *
 
 from doctr.file_utils import is_tf_available, is_torch_available
 
 if is_tf_available():
-    from .zoo import *
     from .master import *
