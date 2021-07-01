6e8750f86046b791259adb02a78ea4316a459b84
fg-mindee <fg@mindee.co>
2021-07-01 18:13:22 +0200

    test: Split unittests into common, tensorflow and pytorch

---
+++ test/common/test_datasets.py
@@ -0,0 +1,13 @@
+import pytest
+
+from doctr import datasets
+
+
+def test_visiondataset():
+    url = 'https://data.deepai.org/mnist.zip'
+    with pytest.raises(ValueError):
+        datasets.datasets.VisionDataset(url, download=False)
+
+    dataset = datasets.datasets.VisionDataset(url, download=True, extract_archive=True)
+    assert len(dataset) == 0
+    assert repr(dataset) == 'VisionDataset()'
