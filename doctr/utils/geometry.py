fb8f21b838324a068046f0e18c44c603be704d0e
Charles Gaillard <charles@mindee.co>
2021-07-01 15:41:47 +0200

    feat: add filter_boxes method (#332)
    * feat: add filter_boxes method
    
    * fix: unused import
    
    * feat: add ioa
    
    * fix: removed filter_boxes
    
    * refacto: merging fn
    
    * fix: mypy

--- doctr/utils/geometry.py
+++ doctr/utils/geometry.py
@@ -3,7 +3,7 @@
 # This program is licensed under the Apache License version 2.
 # See LICENSE or go to <https://www.apache.org/licenses/LICENSE-2.0.txt> for full license details.
 
-from typing import List
+from typing import List, Union
 import numpy as np
 import cv2
 from .common_types import BoundingBox, Polygon4P, RotatedBbox
@@ -36,9 +36,22 @@ def polygon_to_rbbox(polygon: Polygon4P) -> RotatedBbox:
     return fit_rbbox(cnt)
 
 
-def resolve_enclosing_bbox(bboxes: List[BoundingBox]) -> BoundingBox:
-    x, y = zip(*[point for box in bboxes for point in box])
-    return (min(x), min(y)), (max(x), max(y))
+def resolve_enclosing_bbox(bboxes: Union[List[BoundingBox], np.ndarray]) -> Union[BoundingBox, np.ndarray]:
+    """Compute enclosing bbox either from:
+
+    - an array of boxes: (*, 5), where boxes have this shape:
+    (xmin, ymin, xmax, ymax, score)
+
+    - a list of BoundingBox
+
+    Return a (1, 5) array (enclosing boxarray), or a BoundingBox
+    """
+    if isinstance(bboxes, np.ndarray):
+        xmin, ymin, xmax, ymax, score = np.split(bboxes, 5, axis=1)
+        return np.array([xmin.min(), ymin.min(), xmax.max(), ymax.max(), score.mean()])
+    else:
+        x, y = zip(*[point for box in bboxes for point in box])
+        return (min(x), min(y)), (max(x), max(y))
 
 
 def resolve_enclosing_rbbox(rbboxes: List[RotatedBbox]) -> RotatedBbox:
