6c3d0419b2066f43c8f7b7ea1845abc22d17c92b
charlesmindee <charles@mindee.co>
2021-07-02 14:45:19 +0200

    fix: test

--- test/common/test_models_artefacts.py
+++ test/common/test_models_artefacts.py
@@ -6,7 +6,7 @@ import cv2
 
 def test_qr_code_detector(mock_image_folder):
     detector = QRCodeDetector()
-    image = np.random.randint(low=0, high=255, size=(512, 512, 3), dtype=np.uint8)
+    image = np.random.randint(low=0, high=255, size=(256, 256, 3), dtype=np.uint8)
     qrcode = detector(image)
     assert qrcode is None
 
