eb9e9b53d8bb601c45cbdf81009984a7d320f678
charlesmindee <charles@mindee.co>
2021-07-05 07:03:10 +0000

    [skip ci] Documentation updates

--- v0.1.0/_static/js/custom.js
+++ v0.1.0/_static/js/custom.js
@@ -3,11 +3,12 @@
 
 // These two things need to be updated at each release for the version selector.
 // Last stable version
-const stableVersion = "v0.2.1"
+const stableVersion = "v0.3.0"
 // Dictionary doc folder to label. The last stable version should have an empty key.
 const versionMapping = {
     "latest": "latest",
-    "": "v0.2.1 (stable)",
+    "": "v0.3.0 (stable)",
+    "v0.2.1": "v0.2.1",
     "v0.2.0": "v0.2.0",
     "v0.1.1": "v0.1.1",
     "v0.1.0": "v0.1.0",
