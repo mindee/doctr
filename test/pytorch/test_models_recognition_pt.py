ca242b341905e1f2f2bede5fcdb442ef32a38572
fg-mindee <fg@mindee.co>
2021-07-06 20:03:58 +0200

    test: Added unittests

--- test/pytorch/test_models_recognition_pt.py
+++ test/pytorch/test_models_recognition_pt.py
@@ -8,18 +8,19 @@ from doctr.models import recognition
     "arch_name, input_shape",
     [
         ["crnn_vgg16_bn", (3, 32, 128)],
+        ["crnn_resnet31", (3, 32, 128)],
         ["sar_resnet31", (3, 32, 128)],
-        ["master", (3, 32, 128)],
+        ["master", (3, 48, 160)],
     ],
 )
 def test_recognition_models(arch_name, input_shape, mock_vocab):
     batch_size = 4
-    reco_model = recognition.__dict__[arch_name](vocab=mock_vocab, pretrained=False, input_shape=input_shape).eval()
-    assert isinstance(reco_model, torch.nn.Module)
+    model = recognition.__dict__[arch_name](vocab=mock_vocab, pretrained=False, input_shape=input_shape).eval()
+    assert isinstance(model, torch.nn.Module)
     input_tensor = torch.rand((batch_size, *input_shape))
     target = ["i", "am", "a", "jedi"]
 
-    out = reco_model(input_tensor, target, return_model_output=True, return_preds=True)
+    out = model(input_tensor, target, return_model_output=True, return_preds=True)
     assert isinstance(out, dict)
     assert len(out) == 3
     assert isinstance(out['preds'], list)
@@ -45,3 +46,28 @@ def test_reco_postprocessors(post_processor, input_shape, mock_vocab):
     assert all(char in mock_vocab for word, _ in decoded for char in word)
     # Repr
     assert repr(processor) == f'{post_processor}(vocab_size={len(mock_vocab)})'
+
+
+@pytest.mark.parametrize(
+    "arch_name",
+    [
+        "crnn_vgg16_bn",
+        "sar_vgg16_bn",
+        "sar_resnet31",
+        "crnn_resnet31",
+        "master"
+    ],
+)
+def test_recognition_zoo(arch_name):
+    batch_size = 2
+    # Model
+    predictor = recognition.zoo.recognition_predictor(arch_name, pretrained=False)
+    predictor.model.eval()
+    # object check
+    assert isinstance(predictor, recognition.RecognitionPredictor)
+    input_tensor = torch.rand((batch_size, 3, 128, 128))
+    with torch.no_grad():
+        out = predictor(input_tensor)
+    out = predictor(input_tensor)
+    assert isinstance(out, list) and len(out) == batch_size
+    assert all(isinstance(word, str) and isinstance(conf, float) for word, conf in out)
