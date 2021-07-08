16f42b0873b6dbe3bc26cf7d823a234b22a95e4f
fg-mindee <fg@mindee.co>
2021-07-08 11:50:25 +0200

    test: Removed legacy multithreading test

--- test/common/test_utils_multithreading.py
+++ test/common/test_utils_multithreading.py
@@ -5,9 +5,10 @@ from doctr.utils.multithreading import multithread_exec
 
 
 @pytest.mark.parametrize(
-    "input_list, fn, expected_list",
+    "input_seq, func, output_seq",
     [
         [[1, 2, 3], lambda x: 2 * x, [2, 4, 6]],
+        [[1, 2, 3], lambda x: x ** 2, [1, 4, 9]],
         [
             ['this is', 'show me', 'I know'],
             lambda x: x + ' the way',
@@ -15,6 +16,6 @@ from doctr.utils.multithreading import multithread_exec
         ],
     ],
 )
-def test_multithread_exec(input_list, fn, expected_list):
-    assert multithread_exec(fn, input_list) == expected_list
-    assert multithread_exec(fn, input_list, 0) == expected_list
+def test_multithread_exec(input_seq, func, output_seq):
+    assert multithread_exec(func, input_seq) == output_seq
+    assert multithread_exec(func, input_seq, 0) == output_seq
