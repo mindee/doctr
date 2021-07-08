93d812cd3ff2d9309d53253c526013a1073ac386
charlesmindee <charles@mindee.co>
2021-07-08 16:44:32 +0200

    refacto: dataset reco

--- doctr/datasets/recognition.py
+++ doctr/datasets/recognition.py
@@ -6,6 +6,7 @@
 import os
 import json
 from typing import Tuple, List, Optional, Callable, Any
+from pathlib import Path, PurePath
 
 from .datasets import AbstractDataset
 
@@ -17,32 +18,35 @@ class RecognitionDataset(AbstractDataset):
 
     Example::
         >>> from doctr.datasets import RecognitionDataset
-        >>> train_set = RecognitionDataset(img_folder=True, labels_path="/path/to/labels.json")
+        >>> train_set = RecognitionDataset(data_folder="/path/to/folder")
         >>> img, target = train_set[0]
 
     Args:
-        img_folder: path to the images folder
-        labels_path: path to the json file containing all labels (character sequences)
+        data_folder: folder containing folder(s) of images and labels as json files, each image folder 
+        must be named "images" and be in the same subdirectory than the corresponding "labels.json"
         sample_transforms: composable transformations that will be applied to each image
     """
     def __init__(
         self,
-        img_folder: str,
-        labels_path: str,
+        data_folder: str,
         sample_transforms: Optional[Callable[[Any], Any]] = None,
         **kwargs: Any,
     ) -> None:
-        super().__init__(img_folder, **kwargs)
+        super().__init__(data_folder, **kwargs)
         self.sample_transforms = (lambda x: x) if sample_transforms is None else sample_transforms
 
         self.data: List[Tuple[str, str]] = []
-        with open(labels_path) as f:
-            labels = json.load(f)
-        for img_path in os.listdir(self.root):
-            # File existence check
-            if not os.path.exists(os.path.join(self.root, img_path)):
-                raise FileNotFoundError(f"unable to locate {os.path.join(self.root, img_path)}")
-            label = labels.get(img_path)
-            if not isinstance(label, str):
-                raise KeyError("Image is not in referenced in label file")
-            self.data.append((img_path, label))
+        # Recursively find json files
+        json_files = Path(self.root).glob('**/*.json')
+        for json_file in json_files:
+            with json_file.open() as f:
+                labels = json.load(f)
+            imgs_path = PurePath(PurePath.parent(json_file), "images")
+            for img_path in os.listdir(imgs_path):
+                # File existence check
+                if not os.path.exists(os.path.join(imgs_path, img_path)):
+                    raise FileNotFoundError(f"unable to locate {os.path.join(imgs_path, img_path)}")
+                label = labels.get(img_path)
+                if not isinstance(label, str):
+                    raise KeyError("Image is not in referenced in label file")
+                self.data.append((img_path, label))
