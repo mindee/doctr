fc0da167c255e92b911b765f0237b0a1b43cb6d1
charlesmindee <charles@mindee.co>
2021-07-12 17:58:16 +0200

    fix: multi set

--- references/recognition/train_pytorch.py
+++ references/recognition/train_pytorch.py
@@ -85,8 +85,8 @@ def main(args):
     # Load val data generator
     st = time.time()
     val_set = RecognitionDataset(
-        img_folder=os.path.join(args.val_data_path, 'images'),
-        labels_path=os.path.join(args.val_data_path, 'labels.json'),
+        img_folder=os.path.join(args.val_path, 'images'),
+        labels_path=os.path.join(args.val_path, 'labels.json'),
         sample_transforms=T.Resize((args.input_size, 4 * args.input_size), preserve_aspect_ratio=True),
     )
     val_loader = DataLoader(
@@ -124,37 +124,23 @@ def main(args):
     st = time.time()
 
     # Load train data generator
-    if Path(os.path.join(args.train_data_path, 'labels.json')).is_file():
-        train_set = RecognitionDataset(
-            img_folder=os.path.join(args.train_data_path, 'images'),
-            labels_path=os.path.join(args.train_data_path, 'labels.json'),
-            sample_transforms=Compose([
-                T.Resize((args.input_size, 4 * args.input_size), preserve_aspect_ratio=True),
-                # Augmentations
-                T.RandomApply(T.ColorInversion(), .1),
-                ColorJitter(brightness=0.3, contrast=0.3, saturation=0.3, hue=0.02),
-            ]),
-        )
-
-    # If multiple paths provided, merge datasets
-    else:
-        train_set = RecognitionDataset(
-            img_folder=os.path.join(args.train_data_path, os.listdir(args.train_data_path)[0], 'images'),
-            labels_path=os.path.join(args.train_data_path, os.listdir(args.train_data_path)[0], 'labels.json'),
-            sample_transforms=Compose([
-                T.Resize((args.input_size, 4 * args.input_size), preserve_aspect_ratio=True),
-                # Augmentations
-                T.RandomApply(T.ColorInversion(), .1),
-                ColorJitter(brightness=0.3, contrast=0.3, saturation=0.3, hue=0.02),
-            ]),
-        )
-        for folder in os.listdir(args.train_data_path)[1:]:
-            train_set.merge_dataset(
-                RecognitionDataset(
-                    img_folder=os.path.join(args.train_data_path, folder, 'images'),
-                    labels_path=os.path.join(args.train_data_path, folder, 'labels.json'),
-                )
-            )
+    base_path = Path(args.train_path)
+    parts = [base_path] if base_path.joinpath('labels.json').is_file() else [
+        base_path.joinpath(sub) for sub in os.listdir(base_path)
+    ]
+    train_set = RecognitionDataset(
+        parts[0].joinpath('images'),
+        parts[0].joinpath('labels.json'),
+        sample_transforms=Compose([
+            T.Resize((args.input_size, 4 * args.input_size), preserve_aspect_ratio=True),
+            # Augmentations
+            T.RandomApply(T.ColorInversion(), .1),
+            ColorJitter(brightness=0.3, contrast=0.3, saturation=0.3, hue=0.02),
+        ]),
+    )
+    if len(parts) > 1:
+        for subfolder in parts[1:]:
+            train_set.merge_dataset(RecognitionDataset(subfolder.joinpath('images'), subfolder.joinpath('labels.json')))
 
     train_loader = DataLoader(
         train_set,
@@ -235,8 +221,8 @@ def parse_args():
     parser = argparse.ArgumentParser(description='DocTR train text-recognition model (PyTorch)',
                                      formatter_class=argparse.ArgumentDefaultsHelpFormatter)
 
-    parser.add_argument('train_data_path', type=str, help='path to train data folder(s)')
-    parser.add_argument('val_data_path', type=str, help='path to val data folder')
+    parser.add_argument('train_path', type=str, help='path to train data folder(s)')
+    parser.add_argument('val_path', type=str, help='path to val data folder')
     parser.add_argument('model', type=str, help='text-recognition model to train')
     parser.add_argument('--name', type=str, default=None, help='Name of your training experiment')
     parser.add_argument('--epochs', type=int, default=10, help='number of epochs to train the model on')
