bad8e9fce6c58f461443b1f6df4c6ac8493b4705
fg-mindee <fg@mindee.co>
2021-07-13 16:30:11 +0200

    test: Added dtype check in unittests

--- test/tensorflow/test_models_recognition_tf.py
+++ test/tensorflow/test_models_recognition_tf.py
@@ -27,10 +27,11 @@ def test_recognition_models(arch_name, input_shape):
     out = reco_model(input_tensor, target, return_model_output=True, return_preds=True)
     assert isinstance(out, dict)
     assert len(out) == 3
+    assert isinstance(out['out_map'], tf.Tensor)
+    assert out['out_map'].dtype == tf.float32
     assert isinstance(out['preds'], list)
     assert len(out['preds']) == batch_size
     assert all(isinstance(word, str) and isinstance(conf, float) and 0 <= conf <= 1 for word, conf in out['preds'])
-    assert isinstance(out['out_map'], tf.Tensor)
     assert isinstance(out['loss'], tf.Tensor)
 
 
