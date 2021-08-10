1bce82d5ae731ad450629aeb963f4a957063328e
charlesmindee <charles@mindee.co>
2021-08-10 12:02:07 +0200

    feat: add preprocessing

--- export.py
+++ export.py
@@ -3,10 +3,11 @@
 # This program is licensed under the Apache License version 2.
 # See LICENSE or go to <https://www.apache.org/licenses/LICENSE-2.0.txt> for full license details.
 
+from doctr.models.preprocessor import PreProcessor
 import tensorflow as tf
 from tensorflow.keras import Model
 from doctr.models import detection, recognition
-from typing import Tuple
+from typing import Tuple, List
 
 
 class ExportModel(tf.Module):
@@ -36,3 +37,20 @@ def export_detection(arch_name: str, export_path: str, input_shape: Tuple[int, i
     _ = module.predict(input_t)
     # Save
     tf.saved_model.save(module, export_path, signatures={"out_map": module.predict})
+
+
+# PREPROCESSING: bytes --> np array --> preprocessor --> processed_batches
+
+def raw_bytes_to_img(raw_bytes: str, height: int, width: int):
+    #decode raw
+    image = tf.io.decode_raw(raw_bytes, out_type=tf.float32)
+    #reshape flattened img
+    image = tf.reshape(image, (height, width, 3))
+    return image
+
+
+def preprocess_raw_images(raw_images: Tuple[List[str], List[int], List[int]], preprocessor: PreProcessor):
+    raw_bytes, heights, widths = raw_images
+    images = tf.vectorized_map(fn=raw_bytes_to_img, elems=(raw_bytes, heights, widths))
+    processed_batches = preprocessor(images)
+    return processed_batches
