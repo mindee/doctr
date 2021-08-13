86fcb43da38f919c93ec5d2831f43ca60069b4fc
fg-mindee <fg@mindee.co>
2021-08-13 12:10:47 +0200

    fix: Fixed font family of generated image

--- doctr/datasets/classification/base.py
+++ doctr/datasets/classification/base.py
@@ -4,14 +4,14 @@
 # See LICENSE or go to <https://www.apache.org/licenses/LICENSE-2.0.txt> for full license details.
 
 from PIL import ImageDraw, Image, ImageFont
-from typing import Any, Tuple, Optional, Callable
+from typing import Any, Tuple, Optional, Callable, List
 
 from doctr.io.image import tensor_from_pil
 from ..datasets import AbstractDataset
 
 
-def synthesize_char_img(char: str, size: int = 32, font_family: str = "FreeMono.ttf") -> Image:
-    """Generate a synthetic character image with white background and black text
+def synthesize_char_img(char: str, size: int = 32, font_family: Optional[str] = None) -> Image:
+    """Generate a synthetic character image with black background and white text
 
     Args:
         char: the character to render as an image
@@ -25,12 +25,15 @@ def synthesize_char_img(char: str, size: int = 32, font_family: str = "FreeMono.
     if len(char) != 1:
         raise AssertionError('expected a single character input')
 
-    img = Image.new('RGB', (size, size), color=(255, 255, 255))
+    img = Image.new('RGB', (size, size), color=(0, 0, 0))
     d = ImageDraw.Draw(img)
 
     # Draw the character
-    font = ImageFont.truetype(font_family, size)
-    d.text((4, 0), char, font=font, fill=(0, 0, 0))
+    if font_family is None:
+        font = ImageFont.load_default()
+    else:
+        font = ImageFont.truetype(font_family, size)
+    d.text((4, 0), char, font=font, fill=(255, 255, 255))
 
     return img
 
@@ -43,12 +46,16 @@ class _CharacterGenerator(AbstractDataset):
         num_samples: int,
         cache_samples: bool = False,
         sample_transforms: Optional[Callable[[Any], Any]] = None,
+        font_family: Optional[str] = None,
     ) -> None:
         self.sample_transforms = sample_transforms
         self.vocab = vocab
         self._num_samples = num_samples
+        self.font_family = font_family
 
-        self._data = [synthesize_char_img(char) for char in self.vocab] if cache_samples else []
+        self._data = List[Image.Image]
+        if cache_samples:
+            self._data = [synthesize_char_img(char, font_family=self.font_family) for char in self.vocab]
 
     def __len__(self) -> int:
         return self._num_samples
@@ -59,7 +66,7 @@ class _CharacterGenerator(AbstractDataset):
         if len(self._data) > 0:
             pil_img = self._data[target].copy()
         else:
-            pil_img = synthesize_char_img(self.vocab[target])
+            pil_img = synthesize_char_img(self.vocab[target], font_family=self.font_family)
         img = tensor_from_pil(pil_img)
 
         return img, target
