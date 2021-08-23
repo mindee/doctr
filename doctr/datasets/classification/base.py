f657a56ad252fcb75d8196ed3b8f03d6f4651295
Charles Gaillard <charles@mindee.co>
2021-08-23 18:36:45 +0200

    fix: default font writer for character classification dataset (#418)
    * feat: add notebooks
    
    * fix: unused files
    
    * fix: default font
    
    * fix: space
    
    * fix: os
    
    * os test
    
    * fix: os error
    
    * fix: message

--- doctr/datasets/classification/base.py
+++ doctr/datasets/classification/base.py
@@ -5,6 +5,8 @@
 
 from PIL import ImageDraw, Image, ImageFont
 from typing import Any, Tuple, Optional, Callable, List
+import logging
+import platform
 
 from doctr.io.image import tensor_from_pil
 from ..datasets import AbstractDataset
@@ -30,7 +32,13 @@ def synthesize_char_img(char: str, size: int = 32, font_family: Optional[str] = 
 
     # Draw the character
     if font_family is None:
-        font = ImageFont.load_default()
+        try:
+            font = ImageFont.truetype("FreeMono.ttf" if platform.system() == "Linux" else "Arial.ttf", size)
+        except OSError:
+            font = ImageFont.load_default()
+            logging.warning("unable to load specific font families. Loading default PIL font,"
+                            "font size issues may be expected."
+                            "To prevent this, it is recommended to specify the value of 'font_family'.")
     else:
         font = ImageFont.truetype(font_family, size)
     d.text((4, 0), char, font=font, fill=(255, 255, 255))
