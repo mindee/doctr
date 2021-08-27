fd950fe206bae85e18b67ce49a4f7984a3fadbd8
fg-mindee <fg@mindee.co>
2021-08-27 18:17:27 +0200

    fix: Disabled rotation cases in predictor

--- doctr/models/core.py
+++ doctr/models/core.py
@@ -52,14 +52,19 @@ class OCRPredictor(NestedObject):
         # Localize text elements
         boxes = self.det_predictor(pages, **kwargs)
         # Crop images, rotate page if necessary
-        crops = [crop for page, (_boxes, angle) in zip(pages, boxes) for crop in
-                 self.extract_crops_fn(rotate_image(page, -angle, False), _boxes[:, :-1])]  # type: ignore[operator]
+        if self.doc_builder.rotated_bbox:
+            crops = [crop for page, (_boxes, angle) in zip(pages, boxes) for crop in
+                     self.extract_crops_fn(rotate_image(page, -angle, False), _boxes[:, :-1])]  # type: ignore[operator]
+        else:
+            crops = [crop for page, (_boxes, _) in zip(pages, boxes) for crop in
+                     self.extract_crops_fn(page, _boxes[:, :-1])]  # type: ignore[operator]
         # Identify character sequences
         word_preds = self.reco_predictor(crops, **kwargs)
 
         # Rotate back boxes if necessary
         boxes, angles = zip(*boxes)
-        boxes = [rotate_boxes(boxes_page, angle) for boxes_page, angle in zip(boxes, angles)]
+        if self.doc_builder.rotated_bbox:
+            boxes = [rotate_boxes(boxes_page, angle) for boxes_page, angle in zip(boxes, angles)]
         out = self.doc_builder(boxes, word_preds, [page.shape[:2] for page in pages])
         return out
 
