651ec7298efaf5b15aab9bc99637c2aa7a61955d
fg-mindee <fg@mindee.co>
2021-09-15 18:51:53 +0200

    docs: Cleaned documentation

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -14,6 +14,7 @@ import sphinx_rtd_theme
 #
 import os
 import sys
+from datetime import datetime
 sys.path.insert(0, os.path.abspath('../..'))
 import doctr
 
@@ -21,13 +22,13 @@ import doctr
 
 master_doc = 'index'
 project = 'doctr'
-copyright = '2021, Mindee'
+_copyright_str = f"-{datetime.now().year}" if datetime.now().year > 2021 else ""
+copyright = f"2021{_copyright_str}, Mindee"
 author = 'François-Guillaume Fernandez, Charles Gaillard'
 
 # The full version, including alpha/beta/rc tags
 version = doctr.__version__
 release = doctr.__version__ + '-git'
-
 
 # -- General configuration ---------------------------------------------------
 
