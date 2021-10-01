f311464aa89d03dcfbbb0946336bd3c06be80488
mzeidhassan <review@reviewrator.com>
2021-10-01 12:04:51 -0600

    make it flake-8 compliant

--- doctr/datasets/vocabs.py
+++ doctr/datasets/vocabs.py
@@ -15,11 +15,11 @@ VOCABS: Dict[str, str] = {
     'punctuation': string.punctuation,
     'currency': '£€¥¢฿',
     'ancient_greek': 'αβγδεζηθικλμνξοπρστυφχψωΑΒΓΔΕΖΗΘΙΚΛΜΝΞΟΠΡΣΤΥΦΧΨΩ',
-    'arabic_letters': 'ءآأؤإئابةتثجحخدذرزسشصضطظعغـ‌‍‎‏فقكلمنهوىي',
-    'arabic_farsi_letters': 'ٯٰپچژڜڢڤڥڧڨکگی',
-    'hindi_numbers': '٠١٢٣٤٥٦٧٨٩',
+    'arabic_letters': 'ءآأؤإئابةتثجحخدذرزسشصضطظعغـفقكلمنهوىي',
+    'persian_letters': 'پچڢڤگ',
+    'hindi_digits': '٠١٢٣٤٥٦٧٨٩',
     'arabic_diacritics': 'ًٌٍَُِّْ',
-    'arabic_unique_punctuation': '؟؛«»—'
+    'arabic_punctuation': '؟؛«»—'
 }
 
 VOCABS['latin'] = VOCABS['digits'] + VOCABS['ascii_letters'] + VOCABS['punctuation']
@@ -29,4 +29,5 @@ VOCABS['french'] = VOCABS['english'] + 'àâéèêëîïôùûüçÀÂÉÈÊËÎ
 VOCABS['portuguese'] = VOCABS['english'] + 'áàâãéêëíïóôõúüçÁÀÂÃÉËÍÏÓÔÕÚÜÇ' + '¡¿'
 VOCABS['spanish'] = VOCABS['english'] + 'áéíóúüñÁÉÍÓÚÜÑ' + '¡¿'
 VOCABS['german'] = VOCABS['english'] + 'äöüßÄÖÜẞ'
-VOCABS['arabic'] = VOCABS['arabic_letters'] + VOCABS['arabic_farsi_letters'] + VOCABS['hindi_numbers'] + VOCABS['digits'] + VOCABS['arabic_diacritics'] + VOCABS['arabic_unique_punctuation'] + VOCABS['punctuation']
+VOCABS['arabic'] = (VOCABS['digits'] + VOCABS['hindi_digits'] + VOCABS['arabic_letters'] + VOCABS['persian_letters'] +
+                    VOCABS['arabic_diacritics'] + VOCABS['arabic_punctuation'] + VOCABS['punctuation'])
