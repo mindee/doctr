a1c0e73f34eefa548d9c3d4a75e18fad4b2da04b
fg-mindee <fg@mindee.co>
2021-10-21 17:04:11 +0200

    feat: Makes rotated bbox mode automatic

--- references/detection/utils.py
+++ references/detection/utils.py
@@ -9,7 +9,7 @@ import matplotlib.pyplot as plt
 from typing import List, Dict
 
 
-def plot_samples(images, targets: List[Dict[str, np.ndarray]], rotation: bool = False) -> None:
+def plot_samples(images, targets: List[Dict[str, np.ndarray]]) -> None:
     # Unnormalize image
     nb_samples = 4
     _, axes = plt.subplots(2, nb_samples, figsize=(20, 5))
@@ -22,13 +22,15 @@ def plot_samples(images, targets: List[Dict[str, np.ndarray]], rotation: bool = 
         boxes = targets[idx]['boxes'][np.logical_not(targets[idx]['flags'])]
         boxes[:, [0, 2]] = boxes[:, [0, 2]] * img.shape[1]
         boxes[:, [1, 3]] = boxes[:, [1, 3]] * img.shape[0]
-        for box in boxes.round().astype(int):
-            if rotation:
-                box = cv2.boxPoints(((box[0], box[2]), (box[1], box[3]), box[4]))
+        boxes[:, :4] = boxes[:, :4].round().astype(int)
+
+        for box in boxes:
+            if boxes.shape[1] == 5:
+                box = cv2.boxPoints(((int(box[0]), int(box[1])), (int(box[2]), int(box[3])), -box[4]))
                 box = np.int0(box)
                 cv2.fillPoly(target, [box], 1)
             else:
-                target[box[1]: box[3] + 1, box[0]: box[2] + 1] = 1
+                target[int(box[1]): int(box[3]) + 1, int(box[0]): int(box[2]) + 1] = 1
 
         axes[0][idx].imshow(img)
         axes[0][idx].axis('off')
