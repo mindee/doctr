9b784777b966f4c9f3c1ad8840230d7889ae8f82
fg-mindee <fg@mindee.co>
2021-11-12 19:16:02 +0100

    docs: Added notebooks to the documentation

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -55,7 +55,7 @@ templates_path = ['_templates']
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
 # This pattern also affects html_static_path and html_extra_path.
-exclude_patterns = [u'_build', 'Thumbs.db', '.DS_Store']
+exclude_patterns = [u'_build', 'Thumbs.db', '.DS_Store', 'notebooks/*.rst']
 
 
 # The name of the Pygments (syntax highlighting) style to use.
