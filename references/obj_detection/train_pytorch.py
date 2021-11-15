cd0cdfe9c001876f16b1b3dfdbde26b1b7330f2b
SiddhantBahuguna <90187114+SiddhantBahuguna@users.noreply.github.com>
2021-11-15 18:22:06 +0100

    chore: Fixed training script
    1) Training script is working normally.
    2) Early stopping criterion and augmentations:To be added.

--- references/obj_detection/train_pytorch.py
+++ references/obj_detection/train_pytorch.py
@@ -7,132 +7,168 @@ import os
 
 os.environ['USE_TORCH'] = '1'
 
-import time
-
-import numpy as np
 import torch
-import torch.nn as nn
 import torch.optim as optim
-from torch.utils.data import DataLoader
-from tqdm import tqdm
+from torch.utils.data import DataLoader, RandomSampler, SequentialSampler
+import torchvision
+from fastprogress.fastprogress import master_bar, progress_bar
+from torchvision.ops import MultiScaleRoIAlign
+import wandb
 
 from doctr.datasets import DocArtefacts
-from model import faster_model
-from utilities import *
+from utils import val_metric
 
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
 
-def fit_one_epoch(model_, images, targets):
-    x = images.to(device)
-    height, width = x.shape[0], x.shape[1]
-    for i in targets["boxes"]:
-        i[0] = round(i[0] * width)
-        i[2] = round(i[2] * width)
-        i[1] = round(i[1] * height)
-        i[2] = round(i[3] * height)
-    target_ = [{i: torch.tensor(j, dtype=torch.float32).to(device)} for i, j in targets.items()]
-    loss_dict = model_(x, target_)
-    loss = sum([l for k, l in loss_dict.items()])
-    return loss
+def absolute(images, targets):
+    height, width = images.shape[2], images.shape[3]
+
+    for idx in range(images.shape[0]):
+        targets[idx]['boxes'][:, 0::2] = (targets[idx]['boxes'][:, 0::2] * width).round()
+        targets[idx]['boxes'][:, 1::2] = (targets[idx]['boxes'][:, 1::2] * height).round()
+
+    targets = [{
+        "boxes": torch.from_numpy(t['boxes']).to(device, dtype=torch.float32),
+        "labels": torch.tensor(t['labels']).to(device, dtype=torch.long)}
+        for t in targets
+    ]
+
+    return targets
 
 
-def evaluate(images, targets, val_metric, model_):
-    x, y = images.to(device), targets
-    targets = [torch.stack(j, dim=1)[0].to(device) for i, j in y.items()]
-    pq_metric, seg_quality, recall, precision = val_metric(targets=targets, model=model_, x=x)
-    return pq_metric, seg_quality, recall, precision
+def fit_one_epoch(model_, train_loader, optimizer, scheduler, mb, ):
+    model_.train()
+    train_iter = iter(train_loader)
+    # Iterate over the batches of the dataset
+    for _ in progress_bar(range(len(train_loader)), parent=mb):
+        images, targets = next(train_iter)
+        optimizer.zero_grad()
+        target_ = absolute(images, targets)
+        loss_dict = model_(images, target_)
+        loss = sum([l for k, l in loss_dict.items()])
+        loss.backward()
+        optimizer.step()
+        mb.child.comment = f'Train_loss: {loss.item()}'
+    scheduler.step()
 
 
-def train_faster(optimizer, scheduler, train_dataloader, val_dataloader, mymodel: nn.Module, root_dir: str,
-                 label_path: str, num_epochs: int,
-                 val_metric):
-    """Training script
-    Args:
-        root_dir: location of image folder
-        label_path: location of label folder
-        num_epochs: number of epochs in training
-    Returns:
-        torch checkpoint
-    """
-    epch_pq = 0.0
-    epch_precision = 0.0
-    epch_recall = 0.0
-    epch_seg = 0.0
-    epch_train_loss = None
-    epch_loss = None
-    for i in range(num_epochs):
-        mean_loss = []
-        mean_pq = []
-        mean_precision = []
-        mean_recall = []
-        mean_seg = []
-        mymodel.train()
-        tk0 = tqdm(train_dataloader, total=int(len(train_dataloader)))
-        for images, targets in tk0:
-            tk0.set_description(f"Epoch:{i}_Train")
-            optimizer.zero_grad()
-            loss = fit_one_epoch(mymodel, images, targets, device)
-            mean_loss.append(loss)
-            loss.backward()
-            optimizer.step()
-            lo1 = loss.cpu().clone().detach().numpy()
-            tk0.set_postfix(loss=lo1, eph_loss=epch_train_loss, prec=epch_precision, rec=epch_recall, seg=epch_seg,
-                            pq=epch_pq)
-            time.sleep(0.1)
-        epch_loss = torch.sum(torch.stack(mean_loss, dim=0), dim=0) / len(train_dataloader)
-        epch_train_loss = epch_loss.cpu().clone().detach().numpy()
-        tk1 = tqdm(val_dataloader, total=int(len(val_dataloader)))
-        scheduler.step()
-        for images, targets in tk1:
-            tk1.set_description(f"Epoch:{i}_Val")
-            mymodel.eval()
-            pq_metric, seg_quality, recall, precision = evaluate(images, targets, val_metric, mymodel)
-            mean_pq.append(pq_metric)
-            mean_seg.append(seg_quality)
-            mean_recall.append(recall)
-            mean_precision.append(precision)
-        epch_pq = np.sum(np.array(mean_pq)) / len(val_dataloader)
-        epch_precision = np.sum(np.array(mean_precision)) / len(val_dataloader)
-        epch_recall = np.sum(np.array(mean_recall)) / len(val_dataloader)
-        epch_seg = np.sum(np.array(mean_seg)) / len(val_dataloader)
+@torch.no_grad()
+def evaluate(model_, val_loader, val_metric_, mb):
+    model_.eval()
+    val_iter = iter(val_loader)
+    pq, seg, rec, pre = 0, 0, 0, 0
+    for _ in progress_bar(range(len(val_loader)), parent=mb):
+        images, targets = next(val_iter)
+        images = images.to(device)
+        targets = [absolute(images_=images, targets_=targets)]
+        pq_metric, seg_quality, recall, precision = val_metric_(targets=targets, model=model_, x=images)
+        pq += pq_metric
+        seg += seg_quality
+        rec += recall
+        pre += precision
+    epch_pq = pq / len(val_loader)
+    epch_precision = pre / len(val_loader)
+    epch_recall = rec / len(val_loader)
+    epch_seg = seg / len(val_loader)
+    return epch_pq, epch_seg, epch_recall, epch_precision
 
 
 def main(args):
-    model = faster_model
+    model = torchvision.models.detection.__dict__[args.arch](pretrained=True)
+    # Filter keys
+    state_dict = {k: v for k, v in model.state_dict().items() if not k.startswith('roi_heads.')}
+    defaults = {"min_size": 800, "max_size": 1300,
+                "box_fg_iou_thresh": 0.5,
+                "box_bg_iou_thresh": 0.5,
+                "box_detections_per_img": 150, "box_score_thresh": 0.15, "box_positive_fraction": 0.35,
+                "box_nms_thresh": 0.2,
+                "rpn_pre_nms_top_n_train": 2000, "rpn_pre_nms_top_n_test": 1000,
+                "rpn_post_nms_top_n_train": 2000, "rpn_post_nms_top_n_test": 1000,
+                "rpn_nms_thresh": 0.2,
+                "rpn_batch_size_per_image": 250
+                }
+    kwargs = {**defaults}
+    faster_model = torchvision.models.detection.__dict__[args.arch](pretrained=False, num_classes=5, **kwargs)
+    faster_model.load_state_dict(state_dict, strict=False)
+    faster_model.roi_heads.box_roi_pool = MultiScaleRoIAlign(featmap_names=['0', '1', '2', '3'], output_size=(7, 7),
+                                                             sampling_ratio=2)
+    anchor_sizes = ((16), (64), (128), (264))
+    aspect_ratios = ((0.5, 1.0, 2.0, 3.0,)) * len(anchor_sizes)
+    faster_model.rpn.anchor_generator.sizes = anchor_sizes
+    faster_model.rpn.anchor_generator.aspect_ratios = aspect_ratios
     optimizer = optim.SGD(model.parameters(), lr=args.lr)
     scheduler = optim.lr_scheduler.StepLR(optimizer, step_size=8, gamma=0.7)
     train_set = DocArtefacts(train=True, download=True)
     val_set = DocArtefacts(train=False, download=True)
-    train_dataloader = DataLoader(train_set, batch_size=1, num_workers=14, shuffle=False)
-    val_dataloader = DataLoader(val_set, batch_size=1, num_workers=14, shuffle=False)
-    train_faster(optimizer=optimizer, scheduler=scheduler, train_dataloader=train_dataloader,
-                 val_dataloader=val_dataloader, mymodel=model,
-                 num_epochs=args.epochs, val_metric=val_metric)
+    train_dataloader = DataLoader(train_set, batch_size=args.batch_size, num_workers=args.workers,
+                                  sampler=RandomSampler(train_set), pin_memory=True, collate_fn=train_set.collate_fn)
+    val_dataloader = DataLoader(val_set, batch_size=args.batch_size, num_workers=args.workers,
+                                sampler=SequentialSampler(val_set), pin_memory=True, collate_fn=val_set.collate_fn)
+
+    # W&B
+    if args.wb:
+        run = wandb.init(
+            name="Artefacts",
+            project="object-detection",
+            config={
+                "learning_rate": args.lr,
+                "epochs": args.epochs,
+                "weight_decay": args.weight_decay,
+                "batch_size": args.batch_size,
+                "architecture": args.arch,
+                "input_size": args.input_size,
+                "optimizer": "sgd",
+                "framework": "pytorch",
+                "scheduler": args.sched,
+                "pretrained": args.pretrained,
+            }
+        )
+
+    mb = master_bar(range(args.epochs))
+
+    for epoch in mb:
+        fit_one_epoch(model_=faster_model, train_loader=train_dataloader, optimizer=optimizer,
+                      scheduler=scheduler, mb=mb)
+        epch_pq, epch_seg, epch_recall, epch_precision = evaluate(model_=faster_model, val_loader=val_dataloader,
+                                                                  val_metric_=val_metric, mb=mb)
+        mb.write(
+            f"Epoch {epoch + 1}/{args.epochs} - "
+            f"Recall: {epch_recall:.2%} | Precision: {epch_precision:.2%} "
+            f"|Segmentation: {epch_seg:.2%}| Panoptic_Q: {epch_pq:.2%}) |")
+        # W&B
+        if args.wb:
+            wandb.log({
+                'recall': epch_recall,
+                'precision': epch_precision,
+                'seg': epch_seg,
+                'pq': epch_pq,
+            })
+
+    if args.wb:
+        run.finish()
 
 
 def parse_args():
     import argparse
     parser = argparse.ArgumentParser(description='DocTR training script for object detection (PyTorch)',
                                      formatter_class=argparse.ArgumentDefaultsHelpFormatter)
-    # parser.add_argument('train_path', type=str, help='path to training data folder')
-    # parser.add_argument('val_path', type=str, help='path to validation data folder')
-    # parser.add_argument('arch', type=str, help='text-detection model to train')
+    parser.add_argument('arch', type=str, help='text-detection model to train')
     parser.add_argument('--name', type=str, default=None, help='Name of your training experiment')
     parser.add_argument('--epochs', type=int, default=10, help='number of epochs to train the model on')
-    # parser.add_argument('-b', '--batch_size', type=int, default=2, help='batch size for training')
+    parser.add_argument('-b', '--batch_size', type=int, default=2, help='batch size for training')
     parser.add_argument('--device', default=None, type=int, help='device')
     # parser.add_argument('--input_size', type=int, default=1024, help='model input size, H = W')
-    parser.add_argument('--lr', type=float, default=0.001, help='learning rate for the optimizer (Adam)')
+    parser.add_argument('--lr', type=float, default=0.001, help='learning rate for the optimizer (SGD)')
     parser.add_argument('--wd', '--weight-decay', default=0, type=float, help='weight decay', dest='weight_decay')
-    parser.add_argument('-j', '--workers', type=int, default=None, help='number of workers used for dataloading')
+    parser.add_argument('-j', '--workers', type=int, default=14, help='number of workers used for dataloading')
     parser.add_argument('--resume', type=str, default=None, help='Path to your checkpoint')
     parser.add_argument('--wb', dest='wb', action='store_true',
                         help='Log to Weights & Biases')
     parser.add_argument('--pretrained', dest='pretrained', action='store_true',
                         help='Load pretrained parameters before starting the training')
     parser.add_argument('--sched', type=str, default='cosine', help='scheduler to use')
-
+    args = parser.parse_args()
     return args
 
 
