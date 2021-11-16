4f35fffc1268ea1ba1d2ff42ffc1bc6a37f094a9
fg-mindee <fg@mindee.co>
2021-11-16 11:26:12 +0100

    test: Fixed unittests of preprocessor

--- tests/pytorch/test_models_preprocessor_pt.py
+++ tests/pytorch/test_models_preprocessor_pt.py
@@ -10,13 +10,11 @@ from doctr.models.preprocessor import PreProcessor
     [
         [2, (128, 128), np.full((3, 256, 128, 3), 255, dtype=np.uint8), 1, .5],  # numpy uint8
         [2, (128, 128), np.ones((3, 256, 128, 3), dtype=np.float32), 1, .5],  # numpy fp32
-        [2, (128, 128), np.ones((3, 256, 128, 3), dtype=np.float16), 1, .5],  # numpy fp16
         [2, (128, 128), torch.full((3, 3, 256, 128), 255, dtype=torch.uint8), 1, .5],  # torch uint8
         [2, (128, 128), torch.ones((3, 3, 256, 128), dtype=torch.float32), 1, .5],  # torch fp32
         [2, (128, 128), torch.ones((3, 3, 256, 128), dtype=torch.float16), 1, .5],  # torch fp16
         [2, (128, 128), [np.full((256, 128, 3), 255, dtype=np.uint8)] * 3, 2, .5],  # list of numpy uint8
         [2, (128, 128), [np.ones((256, 128, 3), dtype=np.float32)] * 3, 2, .5],  # list of numpy fp32
-        [2, (128, 128), [np.ones((256, 128, 3), dtype=np.float16)] * 3, 2, .5],  # list of numpy fp16
         [2, (128, 128), [torch.full((3, 256, 128), 255, dtype=torch.uint8)] * 3, 2, .5],  # list of torch uint8
         [2, (128, 128), [torch.ones((3, 256, 128), dtype=torch.float32)] * 3, 2, .5],  # list of torch fp32
         [2, (128, 128), [torch.ones((3, 256, 128), dtype=torch.float16)] * 3, 2, .5],  # list of torch fp32
