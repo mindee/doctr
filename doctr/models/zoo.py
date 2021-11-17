a78d30b7296b341e96074eb3aa0ecb7e75b586bd
charlesmindee <charles@mindee.co>
2021-11-17 12:17:29 +0100

    feat: add 2 new rot flags in ocr_predictor

--- doctr/models/zoo.py
+++ doctr/models/zoo.py
@@ -12,7 +12,15 @@ from .recognition.zoo import recognition_predictor
 __all__ = ["ocr_predictor"]
 
 
-def _predictor(det_arch: str, reco_arch: str, pretrained: bool, det_bs=2, reco_bs=128) -> OCRPredictor:
+def _predictor(
+    det_arch: str,
+    reco_arch: str,
+    pretrained: bool,
+    assume_straight_pages: bool = True,
+    export_as_straight_boxes: bool = False,
+    det_bs: int = 2,
+    reco_bs: int = 128,
+) -> OCRPredictor:
 
     # Detection
     det_predictor = detection_predictor(det_arch, pretrained=pretrained, batch_size=det_bs)
@@ -20,13 +28,15 @@ def _predictor(det_arch: str, reco_arch: str, pretrained: bool, det_bs=2, reco_b
     # Recognition
     reco_predictor = recognition_predictor(reco_arch, pretrained=pretrained, batch_size=reco_bs)
 
-    return OCRPredictor(det_predictor, reco_predictor)
+    return OCRPredictor(det_predictor, reco_predictor, assume_straight_pages, export_as_straight_boxes)
 
 
 def ocr_predictor(
     det_arch: str = 'db_resnet50',
     reco_arch: str = 'crnn_vgg16_bn',
     pretrained: bool = False,
+    assume_straight_pages: bool = True,
+    export_as_straight_boxes: bool = False,
     **kwargs: Any
 ) -> OCRPredictor:
     """End-to-end OCR architecture using one model for localization, and another for text recognition.
@@ -39,11 +49,18 @@ def ocr_predictor(
         >>> out = model([input_page])
 
     Args:
-        arch: name of the architecture to use ('db_sar_vgg', 'db_sar_resnet', 'db_crnn_vgg', 'db_crnn_resnet')
+        det_arch: name of the detection architecture to use ('db_resnet50', 'db_mobilenet_v3_large')
+        reco_arch: name of the recognition architecture to use ('crnn_vgg16_bn', 'sar_resnet31')
         pretrained: If True, returns a model pre-trained on our OCR dataset
+        assume_straight_pages: if you only pass straight pages with straight boxes, activate this arg.
+            to speed up the pipeline!
+        export_as_straight_boxes: output format of boxes: if True, boxes are always straight rectangles,
+            otherwise predicted boxes are rotated rectangles (except if you pass assume_straight_pages=True) 
 
     Returns:
         OCR predictor
     """
 
-    return _predictor(det_arch, reco_arch, pretrained, **kwargs)
+    return _predictor(
+        det_arch, reco_arch, pretrained, assume_straight_pages, export_as_straight_boxes, **kwargs
+    )
