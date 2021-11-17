a936512414b98e425dac3ed8e1a30bbb4dcf6031
fg-mindee <fg@mindee.co>
2021-11-17 19:30:32 +0100

    fix: Fixed decode_sequence

--- doctr/datasets/utils.py
+++ doctr/datasets/utils.py
@@ -7,7 +7,9 @@ import string
 import unicodedata
 from collections import Sequence
 from functools import partial
-from typing import Any, List, Optional, Sequence, Union
+from typing import Any, List, Optional
+from typing import Sequence as SequenceType
+from typing import Union
 
 import numpy as np
 
@@ -67,7 +69,7 @@ def encode_string(
 
 
 def decode_sequence(
-    input_seq: Union[np.array, Sequence[int]],
+    input_seq: Union[np.array, SequenceType[int]],
     mapping: str,
 ) -> str:
     """Given a predefined mapping, decode the sequence of numbers to a string
@@ -80,7 +82,9 @@ def decode_sequence(
         A string, decoded from input_seq
     """
 
-    if not isinstance(input_seq, Sequence) or input_seq.dtype != np.int_ or input_seq.max() >= len(mapping):
+    if not isinstance(input_seq, (Sequence, np.ndarray)):
+        raise TypeError("Invalid sequence type")
+    if isinstance(input_seq, np.ndarray) and (input_seq.dtype != np.int_ or input_seq.max() >= len(mapping)):
         raise AssertionError("Input must be an array of int, with max less than mapping size")
 
     return ''.join(map(mapping.__getitem__, input_seq))
