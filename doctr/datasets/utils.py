9f5d0f802caee7531ea9af48e36eaea4ab17881b
fg-mindee <fg@mindee.co>
2021-11-17 19:25:39 +0100

    feat: Added support of basic sequence to decode_sequence

--- doctr/datasets/utils.py
+++ doctr/datasets/utils.py
@@ -5,8 +5,9 @@
 
 import string
 import unicodedata
+from collections import Sequence
 from functools import partial
-from typing import Any, List, Optional
+from typing import Any, List, Optional, Sequence, Union
 
 import numpy as np
 
@@ -66,23 +67,23 @@ def encode_string(
 
 
 def decode_sequence(
-    input_array: np.array,
+    input_seq: Union[np.array, Sequence[int]],
     mapping: str,
 ) -> str:
     """Given a predefined mapping, decode the sequence of numbers to a string
 
     Args:
-        input_array: array to decode
+        input_seq: array to decode
         mapping: vocabulary (string), the encoding is given by the indexing of the character sequence
 
     Returns:
-        A string, decoded from input_array
+        A string, decoded from input_seq
     """
 
-    if not input_array.dtype == np.int_ or input_array.max() >= len(mapping):
+    if not isinstance(input_seq, Sequence) or input_seq.dtype != np.int_ or input_seq.max() >= len(mapping):
         raise AssertionError("Input must be an array of int, with max less than mapping size")
 
-    return ''.join(map(mapping.__getitem__, input_array))
+    return ''.join(map(mapping.__getitem__, input_seq))
 
 
 def encode_sequences(
