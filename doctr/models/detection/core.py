300b1a6342ccdeea54306dfbf800a04e31a06aff
charlesmindee <charles@mindee.co>
2021-11-19 17:30:43 +0100

    fix: style

--- doctr/models/detection/core.py
+++ doctr/models/detection/core.py
@@ -3,7 +3,7 @@
 # This program is licensed under the Apache License version 2.
 # See LICENSE or go to <https://www.apache.org/licenses/LICENSE-2.0.txt> for full license details.
 
-from typing import List, Tuple
+from typing import List
 
 import cv2
 import numpy as np
--- doctr/models/predictor/base.py
+++ doctr/models/predictor/base.py
@@ -3,12 +3,10 @@
 # This program is licensed under the Apache License version 2.
 # See LICENSE or go to <https://www.apache.org/licenses/LICENSE-2.0.txt> for full license details.
 
-from typing import Any, ChainMap, List, Tuple, Callable
-
 import numpy as np
+from typing import List, Tuple
 
 from doctr.models.builder import DocumentBuilder
-
 from .._utils import extract_crops, extract_rcrops
 
 __all__ = ['_OCRPredictor']
