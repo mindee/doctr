0aa4bb149a1bf40c2fe85f2a06b20a486057db55
F-G Fernandez <76527547+fg-mindee@users.noreply.github.com>
2021-11-22 16:26:14 +0100

    chore: Applied post release modifications (#642)
    * chore: Updated package version
    
    * docs: Updated README badge
    
    * docs: Updated the documentation

--- docs/build.sh
+++ docs/build.sh
@@ -38,5 +38,6 @@ deploy_doc "650c4ad4" v0.2.0
 deploy_doc "1bbdb072" v0.2.1
 deploy_doc "3f051346" v0.3.0
 deploy_doc "369a787d" v0.3.1
-deploy_doc "51663ddf" # v0.4.0 Latest stable release
+deploy_doc "51663ddf" v0.4.0
+deploy_doc "74ff9ffb" # v0.4.1 Latest stable release
 rm -rf _build _static
