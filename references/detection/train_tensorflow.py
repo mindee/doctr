ci: Fixed silent isort failure (#655)
* ci: Investigating silent isort failure

* ci: Updated isort job

* ci: Updated python version for isort job

* ci: Revert to python 3.7

* ci: Tries to fix the CI

* ci: Added doctr to known first party

* ci: Pursue investigation

* chore: Relocated isort config

* ci: Updated isort CI job

* style: Fixed import order

* chore: Updated isort config

references/detection/train_tensorflow.py
@@ -15,20 +15,18 @@ import time
 
 import numpy as np
 import tensorflow as tf
-from fastprogress.fastprogress import master_bar, progress_bar
-
 import wandb
+from fastprogress.fastprogress import master_bar, progress_bar
 
 gpu_devices = tf.config.experimental.list_physical_devices('GPU')
 if any(gpu_devices):
     tf.config.experimental.set_memory_growth(gpu_devices[0], True)
 
-from utils import plot_samples
-
 from doctr import transforms as T
 from doctr.datasets import DataLoader, DetectionDataset
 from doctr.models import detection
 from doctr.utils.metrics import LocalizationConfusion
+from utils import plot_samples
 
 
 def fit_one_epoch(model, train_loader, batch_transforms, optimizer, mb):