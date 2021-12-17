108ccadc566df0d705250a0a046f1ac8763484ab
SiddhantBahuguna <90187114+SiddhantBahuguna@users.noreply.github.com>
2021-12-17 17:53:37 +0100

    chore: Fixed issues in evaluation script

--- scripts/detect_artefacts.py
+++ scripts/detect_artefacts.py
@@ -17,13 +17,12 @@ import logging
 import cv2
 import numpy as np
 import torch
-import torchvision
 from PIL import Image
 from pyzbar.pyzbar import decode
 from pyzxing import BarCodeReader
-from torchvision.ops import MultiScaleRoIAlign
 
 from doctr.utils.metrics import LocalizationConfusion
+from doctr.models import obj_detection
 
 
 def pre_filter(scores: List, labels: List, ):
@@ -169,7 +168,11 @@ def inference_script(artefact, root_dir, test_list, code_KIE, save_image, save_t
         kold = cv2.cvtColor(kold, cv2.COLOR_BGR2GRAY)
         imm = cv2.cvtColor(im_read, cv2.COLOR_BGR2RGB)
         imm = imm / 255
-        imm2 = [torch.tensor(imm, dtype=torch.float32).permute(2, 0, 1)]
+
+        if torch.cuda.is_available():
+            imm2 = [torch.tensor(imm, dtype=torch.float32).permute(2, 0, 1).cuda()]
+        else:
+            imm2 = [torch.tensor(imm, dtype=torch.float32).permute(2, 0, 1)]
         pred = artefact(imm2)
         tg, tg2, tg1 = post_nms(pred, imm2)  # box, score, label
         tg = [list(i) for i in tg]
@@ -221,33 +224,9 @@ def inference_script(artefact, root_dir, test_list, code_KIE, save_image, save_t
 
 
 def main(args):
-    torch.backends.cudnn.benchmark = True
+    print(args)
 
-    # Filter keys
-    state_dict = {
-        k: v for k, v in torchvision.models.detection.__dict__[args.arch](pretrained=True).state_dict().items()
-        if not k.startswith('roi_heads.')
-    }
-    defaults = {"min_size": 800, "max_size": 1300,
-                "box_fg_iou_thresh": 0.5,
-                "box_bg_iou_thresh": 0.5,
-                "box_detections_per_img": 150, "box_score_thresh": 0.15, "box_positive_fraction": 0.35,
-                "box_nms_thresh": 0.2,
-                "rpn_pre_nms_top_n_train": 2000, "rpn_pre_nms_top_n_test": 1000,
-                "rpn_post_nms_top_n_train": 2000, "rpn_post_nms_top_n_test": 1000,
-                "rpn_nms_thresh": 0.2,
-                "rpn_batch_size_per_image": 250
-                }
-    kwargs = {**defaults}
-
-    model = torchvision.models.detection.__dict__[args.arch](pretrained=False, num_classes=5, **kwargs)
-    model.load_state_dict(state_dict, strict=False)
-    model.roi_heads.box_roi_pool = MultiScaleRoIAlign(featmap_names=['0', '1', '2', '3'], output_size=(7, 7),
-                                                      sampling_ratio=2)
-    anchor_sizes = ((16), (64), (128), (264))
-    aspect_ratios = ((0.5, 1.0, 2.0, 3.0,)) * len(anchor_sizes)
-    model.rpn.anchor_generator.sizes = anchor_sizes
-    model.rpn.anchor_generator.aspect_ratios = aspect_ratios
+    model = obj_detection.__dict__[args.arch](num_classes=5)
     # GPU
     if isinstance(args.device, int):
         if not torch.cuda.is_available():
@@ -262,37 +241,30 @@ def main(args):
     if torch.cuda.is_available():
         torch.cuda.set_device(args.device)
         model = model.cuda()
-    root_dir = args.root_dir
-    save_to_folder = args.save_to
-    checkpoint_path = args.checkpoint_path
-    save_image = args.plot
-    code_KIE = args.KIE
-    save_j = args.save_json
-    if_save = args.json
-    model.load_state_dict(torch.load(checkpoint_path))
+    model.load_state_dict(torch.load(args.checkpoint_path))
     model.eval()
-    test_list = os.listdir(args.root_dir)
-    inf_dic = inference_script(model, root_dir, test_list, code_KIE,
-                               save_image, save_to_folder)
-    if if_save:
-        with open(save_j, 'w', encoding='utf-8') as f:
+    inf_dic = inference_script(model, args.root_dir, os.listdir(args.root_dir), args.KIE,
+                               args.plot, args.save_to)
+    if args.json:
+        with open(args.save_json, 'w', encoding='utf-8') as f:
             json.dump(inf_dic, f)
 
 
 def parse_args():
     parser = argparse.ArgumentParser(description="Artefact Detection Training",
                                      formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+    parser.add_argument('arch', type=str, help='text-detection model to train')
     parser.add_argument('root_dir', type=str, help='path to image folder')
-    parser.add_argument('checkpoint_path', type=str, help='path to torch checkpoint')
+    parser.add_argument('--device', default=None, type=int, help='device')
     parser.add_argument('-save_to', type=str, required=False,
                         help="Use this along with --plot True to save images to the desired location")
     parser.add_argument('-save_json', type=str, required=False,
                         help="Use this along with --json True")
-    parser.add_argument('--json', nargs='?', const=False, type=bool,
+    parser.add_argument('--json', default=False, type=bool,
                         help="Optional: If user wants to save json output")
-    parser.add_argument('--KIE', nargs='?', const=False, type=bool,
+    parser.add_argument('--KIE', default=False, type=bool,
                         help="Optional:If user wants to extract information from codes")
-    parser.add_argument('--plot', nargs="?", const=False, type=bool,
+    parser.add_argument('--plot', default=False, type=bool,
                         help="Optional:If the user wants to plot and save the predictions")
     args = parser.parse_args()
     if args.plot and (args.save_to is None):
