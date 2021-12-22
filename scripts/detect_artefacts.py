e90d9b2ab0eb9e82b790b8aaf7d766ac1c177b89
SiddhantBahuguna <90187114+SiddhantBahuguna@users.noreply.github.com>
2021-12-22 20:18:01 +0100

    chore: Fixed minor issues to improve the plot predictions

--- scripts/detect_artefacts.py
+++ scripts/detect_artefacts.py
@@ -17,28 +17,33 @@ from matplotlib.pyplot import figure
 import torch
 
 from doctr.models import obj_detection
+from doctr.io.image import read_img_as_tensor
 
 
 def plot_predictions(image, tg_boxes, tg_labels, cl_map, cm):
     for ind_2, val_2 in enumerate(tg_boxes):
-        cv2.rectangle(image, (int(val_2[0]), int(val_2[1])), (int(val_2[2]), int(val_2[3])),
-                      cm[str(int(tg_labels[ind_2]))][0], 2)
-        text_size, _ = cv2.getTextSize(cl_map[str(int(tg_labels[ind_2]))], cv2.FONT_HERSHEY_SIMPLEX, 2, 2)
+        # Bounding box around artefacts
+        cv2.rectangle(image, (val_2[0], val_2[1]), (val_2[2], val_2[3]),
+                      cm[tg_labels[ind_2]], 2)
+        text_size, _ = cv2.getTextSize(cl_map[int(tg_labels[ind_2])], cv2.FONT_HERSHEY_SIMPLEX, 2, 2)
         text_w, text_h = text_size
-        cv2.rectangle(image, (int(val_2[0]), int(val_2[1])), (int(val_2[0]) + text_w, int(val_2[1]) - text_h),
-                      cm[str(int(tg_labels[ind_2]))][0], -1)
-        cv2.putText(image, cl_map[str(int(tg_labels[ind_2]))], (int(val_2[0]), int(val_2[1])),
+        # Filled rectangle above bounding box
+        cv2.rectangle(image, (val_2[0], val_2[1]), (val_2[0] + text_w, val_2[1] - text_h),
+                      cm[tg_labels[ind_2]], -1)
+        # Text bearing the name of the artefact detected
+        cv2.putText(image, cl_map[int(tg_labels[ind_2])], (int(val_2[0]), int(val_2[1])),
                     cv2.FONT_HERSHEY_SIMPLEX, 2, (255, 255, 255), 3)
     figure(figsize=(9, 7), dpi=100)
+    plt.axis('off')
     plt.imshow(image)
     plt.show()
 
 
+@torch.no_grad()
 def main(args):
     print(args)
 
-    model = obj_detection.__dict__[args.arch](pretrained=True, num_classes=5)
-    model.eval()
+    model = obj_detection.__dict__[args.arch](pretrained=True, num_classes=5).eval()
     # GPU
     if isinstance(args.device, int):
         if not torch.cuda.is_available():
@@ -54,28 +59,25 @@ def main(args):
         torch.cuda.set_device(args.device)
         model = model.cuda()
 
-    cm = {'1': [(0, 0, 150)], '2': [(0, 0, 0)], '3': [(0, 150, 0)], '4': [(150, 0, 0)]}
-    cl_map = {'1': "QR_Code", "2": "Bar_Code", "3": "Logo", "4": "Photo"}
-    for val in os.listdir(args.root_dir):
-        im_read = cv2.imread(os.path.join(args.root_dir, val))
-        imm = cv2.cvtColor(im_read, cv2.COLOR_BGR2RGB)
-        imm2 = imm / 255
-        if torch.cuda.is_available():
-            imm2 = [torch.tensor(imm2, dtype=torch.float32).permute(2, 0, 1).cuda()]
-        else:
-            imm2 = [torch.tensor(imm2, dtype=torch.float32).permute(2, 0, 1)]
-        pred = model(imm2)
-        tg_labels = pred[0]['labels'].detach().cpu().numpy()
-        tg_boxes = pred[0]['boxes'].detach().cpu().numpy()
-        tg_boxes = [list(i) for i in tg_boxes]
-        plot_predictions(imm, tg_boxes, tg_labels, cl_map, cm)
+    cm = {1: (0, 0, 150), 2: (0, 0, 0), 3: (0, 150, 0), 4: (150, 0, 0)}
+    cl_map = {1: "QR_Code", 2: "Bar_Code", 3: "Logo", 4: "Photo"}
+    img = read_img_as_tensor(args.img_path).unsqueeze(0)
+    if torch.cuda.is_available():
+        img = img.cuda()
+    pred = model(img)
+    tg_labels = pred[0]['labels'].detach().cpu().numpy()
+    tg_labels = tg_labels.round().astype(int)
+    tg_boxes = pred[0]['boxes'].detach().cpu().numpy()
+    tg_boxes = tg_boxes.round().astype(int)
+    img = img.cpu().permute(0, 2, 3, 1).numpy()[0].copy()
+    plot_predictions(img, tg_boxes, tg_labels, cl_map, cm)
 
 
 def parse_args():
-    parser = argparse.ArgumentParser(description="Artefact Detection Training",
+    parser = argparse.ArgumentParser(description="Artefact detection model to use",
                                      formatter_class=argparse.ArgumentDefaultsHelpFormatter)
     parser.add_argument('arch', type=str, help='text-detection model to train')
-    parser.add_argument('root_dir', type=str, help='path to image folder')
+    parser.add_argument('img_path', type=str, help='path to the image')
     parser.add_argument('--device', default=None, type=int, help='device')
     args = parser.parse_args()
     return args
