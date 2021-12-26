f7e139e2d15a8f72e860b8a89e0bdfd67204bcf4
fg-mindee <fg@mindee.co>
2021-12-26 16:45:54 +0100

    feat: Added WordGenerator dataset

--- doctr/datasets/classification/base.py
+++ doctr/datasets/classification/base.py
@@ -105,3 +105,57 @@ class _CharacterGenerator(AbstractDataset):
         img = tensor_from_pil(pil_img)
 
         return img, target
+
+
+class _WordGenerator(AbstractDataset):
+
+    def __init__(
+        self,
+        vocab: str,
+        min_chars: int,
+        max_chars: int,
+        num_samples: int,
+        cache_samples: bool = False,
+        font_family: Optional[Union[str, List[str]]] = None,
+        img_transforms: Optional[Callable[[Any], Any]] = None,
+        sample_transforms: Optional[Callable[[Any, Any], Tuple[Any, Any]]] = None,
+    ) -> None:
+        self.vocab = vocab
+        self.wordlen_range = (min_chars, max_chars)
+        self._num_samples = num_samples
+        self.font_family = font_family if isinstance(font_family, list) else [font_family]
+        # Validate fonts
+        if isinstance(font_family, list):
+            for font in self.font_family:
+                try:
+                    _ = get_font(font, 10)
+                except OSError:
+                    raise ValueError(f"unable to locate font: {font}")
+        self.img_transforms = img_transforms
+        self.sample_transforms = sample_transforms
+
+        self._data: List[Image.Image] = []
+        if cache_samples:
+            _words = [self._generate_string(*self.wordlen_range) for _ in range(num_samples)]
+            self._data = [
+                (synthesize_text_img(text, font_family=random.choice(self.font_family)), text)
+                for text in _words
+            ]
+
+    def _generate_string(self, min_chars: int, max_chars: int) -> str:
+        num_chars = random.randint(min_chars, max_chars)
+        return "".join(random.choice(self.vocab) for _ in range(num_chars))
+
+    def __len__(self) -> int:
+        return self._num_samples
+
+    def _read_sample(self, index: int) -> Tuple[Any, int]:
+        # Samples are already cached
+        if len(self._data) > 0:
+            pil_img, target = self._data[index]
+        else:
+            target = self._generate_string(*self.wordlen_range)
+            pil_img = synthesize_text_img(target, font_family=random.choice(self.font_family))
+        img = tensor_from_pil(pil_img)
+
+        return img, target
--- doctr/datasets/classification/pytorch.py
+++ doctr/datasets/classification/pytorch.py
@@ -5,9 +5,9 @@
 
 from torch.utils.data._utils.collate import default_collate
 
-from .base import _CharacterGenerator
+from .base import _CharacterGenerator, _WordGenerator
 
-__all__ = ['CharacterGenerator']
+__all__ = ['CharacterGenerator', 'WordGenerator']
 
 
 class CharacterGenerator(_CharacterGenerator):
@@ -20,6 +20,30 @@ class CharacterGenerator(_CharacterGenerator):
 
     Args:
         vocab: vocabulary to take the character from
+        num_samples: number of samples that will be generated iterating over the dataset
+        cache_samples: whether generated images should be cached firsthand
+        font_family: font to use to generate the text images
+        img_transforms: composable transformations that will be applied to each image
+        sample_transforms: composable transformations that will be applied to both the image and the target
+    """
+
+    def __init__(self, *args, **kwargs) -> None:
+        super().__init__(*args, **kwargs)
+        setattr(self, 'collate_fn', default_collate)
+
+
+class WordGenerator(_WordGenerator):
+    """Implements a character image generation dataset
+
+    Example::
+        >>> from doctr.datasets import WordGenerator
+        >>> ds = WordGenerator(vocab='abdef')
+        >>> img, target = ds[0]
+
+    Args:
+        vocab: vocabulary to take the character from
+        min_chars: minimum number of characters in a word
+        max_chars: maximum number of characters in a word
         num_samples: number of samples that will be generated iterating over the dataset
         cache_samples: whether generated images should be cached firsthand
         font_family: font to use to generate the text images
