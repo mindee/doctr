f254275abfa5b4e1347df6ab1156fb4a0f2244d2
fg-mindee <fg@mindee.co>
2021-12-28 22:13:24 +0100

    fix: Fixed RandomCrop

--- doctr/transforms/functional/pytorch.py
+++ doctr/transforms/functional/pytorch.py
@@ -69,19 +69,23 @@ def rotate(
 def crop_detection(
     img: torch.Tensor,
     boxes: np.ndarray,
-    crop_box: Tuple[int, int, int, int]
+    crop_box: Tuple[float, float, float, float]
 ) -> Tuple[torch.Tensor, np.ndarray]:
     """Crop and image and associated bboxes
 
     Args:
         img: image to crop
         boxes: array of boxes to clip, absolute (int) or relative (float)
-        crop_box: box (xmin, ymin, xmax, ymax) to crop the image. Absolute coords.
+        crop_box: box (xmin, ymin, xmax, ymax) to crop the image. Relative coords.
 
     Returns:
         A tuple of cropped image, cropped boxes, where the image is not resized.
     """
-    xmin, ymin, xmax, ymax = crop_box
+    if any(val < 0 or val > 1 for val in crop_box):
+        raise AssertionError("coordinates of arg `crop_box` should be relative")
+    h, w = img.shape[-2:]
+    xmin, ymin = int(round(crop_box[0] * (w - 1))), int(round(crop_box[1] * (h - 1)))
+    xmax, ymax = int(round(crop_box[2] * (w - 1))), int(round(crop_box[3] * (h - 1)))
     croped_img = F.crop(
         img, ymin, xmin, ymax - ymin, xmax - xmin
     )
