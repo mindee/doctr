e5ca2d536864ccff1460ede9bf3da66b13a2ab9a
F-G Fernandez <76527547+fg-mindee@users.noreply.github.com>
2022-01-03 14:55:03 +0100

    docs: Updated headers of python files (#781)
    * docs: Updated headers of api
    
    * docs: Updated header of demo
    
    * docs: Updated headers of references
    
    * docs: Updated headers of scripts
    
    * docs: Updated headers of setup
    
    * test: Updated header check
    
    * docs: Updated headers of core library

--- api/tests/routes/test_recognition.py
+++ api/tests/routes/test_recognition.py
@@ -1,8 +1,3 @@
-# Copyright (C) 2021, Mindee.
-
-# This program is licensed under the Apache License version 2.
-# See LICENSE or go to <https://www.apache.org/licenses/LICENSE-2.0.txt> for full license details.
-
 import pytest
 
 
