19454772667153da833c1cce845a736ecd98f36f
felixdittrich92 <felixdittrich92@gmail.com>
2022-01-06 09:16:55 +0100

    make flake happy

--- doctr/datasets/imgur5k.py
+++ doctr/datasets/imgur5k.py
@@ -4,7 +4,6 @@
 # See LICENSE or go to <https://www.apache.org/licenses/LICENSE-2.0.txt> for full license details.
 
 import json
-import math
 import os
 from pathlib import Path
 from typing import Any, Dict, List, Tuple
