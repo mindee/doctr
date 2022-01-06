f50f6475504ff6c395e21722c8c0475d7ce733bc
felixdittrich92 <felixdittrich92@gmail.com>
2022-01-06 09:14:40 +0100

    update box computation

--- doctr/datasets/imgur5k.py
+++ doctr/datasets/imgur5k.py
@@ -9,6 +9,7 @@ import os
 from pathlib import Path
 from typing import Any, Dict, List, Tuple
 
+import cv2
 import numpy as np
 
 from .datasets import AbstractDataset
@@ -57,68 +58,40 @@ class IMGUR5K(AbstractDataset):
         np_dtype = np.float32
 
         img_names = os.listdir(img_folder)
-        annotation_file = json.load(open(label_path))
         train_samples = int(len(img_names) * 0.9)
         set_slice = slice(train_samples) if self.train else slice(train_samples, None)
 
+        with open(label_path) as f:
+            annotation_file = json.load(f)
+
         for img_name in img_names[set_slice]:
             img_path = Path(img_folder, img_name)
+            img_id = img_name.split(".")[0]
 
             # File existence check
             if not os.path.exists(os.path.join(self.root, img_name)):
                 raise FileNotFoundError(f"unable to locate {os.path.join(self.root, img_name)}")
 
-            try:  # some files have no annotations
-                ann_ids = annotation_file['index_to_ann_map'][img_name.split('.')[0]]
-            except KeyError:
+            # some files have no annotations
+            if img_id not in annotation_file['index_to_ann_map'].keys():
                 continue
+            ann_ids = annotation_file['index_to_ann_map'][img_id]
             annotations = [annotation_file['ann_id'][a_id] for a_id in ann_ids]
-            labels = []
-            _bboxes = []
+
+            labels: List[str] = []
+            box_targets: List[np.ndarray] = []
             for ann in annotations:
                 if ann['word'] != '.':
                     labels.append(ann['word'])
                     # x_center, y_center, width, height, angle
-                    _bboxes.append([float(val) for val in ann['bounding_box'].strip('[ ]').split(', ')])
+                    _box = [float(val) for val in ann['bounding_box'].strip('[ ]').split(', ')]
+                    # (x, y) coordinates of top left, top right, bottom right, bottom left corners
+                    box_targets.append(cv2.boxPoints(((_box[0], _box[1]), (_box[2], _box[3]), _box[4])))
 
-            # TODO: rework !!!
-            box_targets = []
-            if use_polygons:
-                # (x, y) coordinates of top left, top right, bottom right, bottom left corners
-                for box in _bboxes:
-                    xc, yc, w, h, a = box
-                    c = math.cos(box[4])
-                    s = math.sin(box[4])
-                    r1x = -w / 2 * c - h / 2 * s
-                    r1y = -w / 2 * s + h / 2 * c
-                    r2x = w / 2 * c - h / 2 * s
-                    r2y = w / 2 * s + h / 2 * c
-
-                    box_targets.append([[box[0] + r1x, box[1] + r1y],
-                                        [box[0] + r2x, box[1] + r2y],
-                                        [box[0] - r2x, box[1] - r2y],
-                                        [box[0] - r1x, box[1] - r1y]])
-
-            else:
+            if not use_polygons:
                 # xmin, ymin, xmax, ymax
-                for box in _bboxes:
-                    xc, yc, w, h, a = box
-                    new_w = w * math.cos(a * (math.pi / 180)) + h * math.sin(a * (math.pi / 180))
-                    new_h = h * math.cos(a * (math.pi / 180)) + w * math.sin(a * (math.pi / 180))
-
-                    x_min = float(math.floor(xc - new_w / 2))
-                    y_min = float(math.floor(yc - new_h / 2))
-                    x_max = float(math.floor(x_min + new_w))
-                    y_max = float(math.floor(y_min + new_h))
-
-                    box_targets.append([x_min, y_min, x_max, y_max])  # type: ignore[list-item]
-
-            #import cv2
-            #for box in box_targets:
-            #    img = cv2.imread(str(img_path))
-            #    cv2.rectangle(img, (int(box[0]), int(box[1])), (int(box[2]), int(box[3])), (0, 0, 255), 2)
-            #    cv2.imshow('test', img)
-            #    cv2.waitKey(0)
+                box_targets = [np.array([min(box_target[:, 0]), min(box_target[:, 1]), max(
+                    box_target[:, 0]), max(box_target[:, 1])], dtype=np_dtype) for box_target in box_targets]
 
             self.data.append((img_path, dict(boxes=np.asarray(box_targets, dtype=np_dtype), labels=labels)))
 
