598f03b5e45b98e9fe64938cda199bf58b5d5130
fg-mindee <fg@mindee.co>
2022-01-10 12:27:26 +0100

    ci: Renamed PR label check script

--- .github/verify_pr_labels.py
+++ .github/verify_pr_labels.py
@@ -7,8 +7,7 @@ Note: we ping the merger only, not the reviewers, as the reviewers can sometimes
 with no labeling responsibility, so we don't want to bother them.
 """
 
-import sys
-from typing import Any, Optional, Set, Tuple
+from typing import Any, Set, Tuple
 
 import requests
 
@@ -51,30 +50,18 @@ def query_repo(cmd: str, *, accept) -> Any:
     return response.json()
 
 
-def get_pr_number(commit_hash: str) -> Optional[int]:
-    # See https://docs.github.com/en/rest/reference/repos#list-pull-requests-associated-with-a-commit
-    data = query_repo(f"commits/{commit_hash}/pulls", accept="application/vnd.github.groot-preview+json")
-    if not data:
-        return None
-    return data[0]["number"]
-
-
 def get_pr_merger_and_labels(pr_number: int) -> Tuple[str, Set[str]]:
     # See https://docs.github.com/en/rest/reference/pulls#get-a-pull-request
     data = query_repo(f"pulls/{pr_number}", accept="application/vnd.github.v3+json")
-    merger = data["merged_by"]["login"]
+    merger = data.get("merged_by", {}).get("login")
     labels = {label["name"] for label in data["labels"]}
     return merger, labels
 
 
 def main(args):
-    pr_number = get_pr_number(args.commit_hash)
-    if not pr_number:
-        sys.exit(0)
-
-    merger, labels = get_pr_merger_and_labels(pr_number)
+    merger, labels = get_pr_merger_and_labels(args.pr)
     is_properly_labeled = bool(PRIMARY_LABELS.intersection(labels) and SECONDARY_LABELS.intersection(labels))
-    if not is_properly_labeled:
+    if isinstance(merger, str) and not is_properly_labeled:
         print(f"@{merger}")
 
 
@@ -83,7 +70,7 @@ def parse_args():
     parser = argparse.ArgumentParser(description='PR label checker',
                                      formatter_class=argparse.ArgumentDefaultsHelpFormatter)
 
-    parser.add_argument('commit_hash', type=str, help='Hash of the commit')
+    parser.add_argument('pr', type=int, help='PR number')
     args = parser.parse_args()
 
     return args
