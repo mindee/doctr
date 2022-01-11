fix: Clip in rotation transform + eval_straight mode for training (#786)
* fix: clip + eval straight

* fix: removed file

* fix: vector

* feat: add torch training script

doctr/transforms/functional/pytorch.py
@@ -71,7 +71,7 @@ def rotate_sample(
     rotated_geoms[..., 0] = rotated_geoms[..., 0] / rotated_img.shape[2]
     rotated_geoms[..., 1] = rotated_geoms[..., 1] / rotated_img.shape[1]
 
-    return rotated_img, rotated_geoms
+    return rotated_img, np.clip(rotated_geoms, 0, 1)
 
 
 def crop_detection(