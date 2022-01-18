d5a7cc948d1ec89d0d1422c2b8358b3126e22fd1
fg-mindee <fg@mindee.co>
2022-01-18 17:16:36 +0100

    refactor: Avoids instantiating crop orientation predictor when unnecessary

--- doctr/models/predictor/tensorflow.py
+++ doctr/models/predictor/tensorflow.py
@@ -53,7 +53,7 @@ class OCRPredictor(NestedObject, _OCRPredictor):
         self.doc_builder = DocumentBuilder(export_as_straight_boxes=export_as_straight_boxes)
         self.assume_straight_pages = assume_straight_pages
         self.straighten_pages = straighten_pages
-        self.crop_orientation_predictor = crop_orientation_predictor(pretrained=True)
+        self.crop_orientation_predictor = None if assume_straight_pages else crop_orientation_predictor(pretrained=True)
 
     def __call__(
         self,
