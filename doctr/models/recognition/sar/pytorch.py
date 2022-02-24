d2fc800ec161b7bce516f02f6117c34e0d8961a2
fg-mindee <fg@mindee.co>
2022-02-24 19:33:54 +0100

    fix: Fixed SAR

--- doctr/models/recognition/sar/pytorch.py
+++ doctr/models/recognition/sar/pytorch.py
@@ -113,16 +113,17 @@ class SARDecoder(nn.Module):
         gt: Optional[torch.Tensor] = None,  # (N, L)
     ) -> torch.Tensor:
 
-        if gt is None:
+        if isinstance(gt, torch.Tensor) and self.training:
+            # (N, L) --> (N, L, embedding_units)
+            symbol = self.embed(gt)
+        else:
             # Initialize with the index of virtual START symbol (placed after <eos> so that the one-hot is only zeros)
             symbol = torch.zeros(features.shape[0], device=features.device, dtype=torch.long)
             # (N, embedding_units)
             symbol = self.embed(symbol)
             # (N, L, embedding_units)
             symbol = symbol.unsqueeze(1).expand(-1, self.max_length + 1, -1)
-        else:
-            # (N, L) --> (N, L, embedding_units)
-            symbol = self.embed(gt)
+
         # (N, L + 1, embedding_units)
         symbol = torch.cat((holistic.unsqueeze(1), symbol), dim=1)
 
@@ -143,13 +144,13 @@ class SARDecoder(nn.Module):
             logits = torch.cat((logits, glimpse), -1)
             # (N, L + 1, vocab_size + 1)
             decoded = self.output_dense(logits)
-            if gt is None:
+            if isinstance(gt, torch.Tensor) and self.training:
+                char_logits = decoded[:, 1:]
+            else:
                 char_logits[:, t] = decoded[:, t + 1]
                 if t < decoding_iter - 1:
                     # update symbol with predicted logits for t + 1 step
                     symbol[:, t + 2] = self.embed(decoded[:, t + 1].argmax(-1))
-            else:
-                char_logits = decoded[:, 1:]
 
         return char_logits
 
