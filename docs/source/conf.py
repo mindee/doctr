dc857202701c926e9000e69a4413781cdf0f2cef
felixdittrich92 <felixdittrich92@gmail.com>
2022-03-16 16:15:10 +0100

    apply changes

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -114,6 +114,25 @@ html_static_path = ['_static']
 # A list of files that should not be packed into the epub file.
 epub_exclude_files = ['search.html']
 
+# Add googleanalytics id
+# ref: https://github.com/sphinx-contrib/googleanalytics/blob/master/sphinxcontrib/googleanalytics.py
+def add_ga_javascript(app, pagename, templatename, context, doctree):
+
+    metatags = context.get('metatags', '')
+    metatags += """<script type="text/javascript">
+      var _gaq = _gaq || [];
+      _gaq.push(['_setAccount', '%s']);
+      _gaq.push(['_trackPageview']);
+      (function() {
+        var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+        ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+        var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+      })();
+    </script>""" % 'G-40DVRMX8T4'
+    context['metatags'] = metatags
+
+
 def setup(app):
     app.add_css_file('css/mindee.css')
     app.add_js_file('js/custom.js')
+    app.connect('html-page-context', add_ga_javascript)
