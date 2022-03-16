d5677dee6e8fed1e4293b35bd894973e82fc3d1b
felixdittrich92 <felixdittrich92@gmail.com>
2022-03-16 09:13:12 +0100

    revert

--- doctr/file_utils.py
+++ doctr/file_utils.py
@@ -10,8 +10,6 @@ import logging
 import os
 import sys
 
-from .version import __version__
-
 if sys.version_info < (3, 8):
     import importlib_metadata
 else:
@@ -25,22 +23,18 @@ ENV_VARS_TRUE_AND_AUTO_VALUES = ENV_VARS_TRUE_VALUES.union({"AUTO"})
 
 USE_TF = os.environ.get("USE_TF", "AUTO").upper()
 USE_TORCH = os.environ.get("USE_TORCH", "AUTO").upper()
-USE_DOCTR_LOGGER = os.environ.get("USE_DOCTR_LOGGER", "OFF").upper()
 
-if USE_DOCTR_LOGGER in ENV_VARS_TRUE_VALUES:
-    # config doctr logger
-    logging.getLogger().setLevel(level=logging.INFO)
-    logging.getLogger().addHandler(logging.StreamHandler(sys.stderr))
 
 if USE_TORCH in ENV_VARS_TRUE_AND_AUTO_VALUES and USE_TF not in ENV_VARS_TRUE_VALUES:
     _torch_available = importlib.util.find_spec("torch") is not None
     if _torch_available:
         try:
             _torch_version = importlib_metadata.version("torch")
+            logging.info(f"PyTorch version {_torch_version} available.")
         except importlib_metadata.PackageNotFoundError:
-            logging.error("PyTorch is installed but not available. Please check your installation.")
             _torch_available = False
 else:
+    logging.info("Disabling PyTorch because USE_TF is set")
     _torch_available = False
 
 
@@ -71,22 +65,16 @@ if USE_TF in ENV_VARS_TRUE_AND_AUTO_VALUES and USE_TORCH not in ENV_VARS_TRUE_VA
         if int(_tf_version.split('.')[0]) < 2:  # type: ignore[union-attr]
             logging.info(f"TensorFlow found but with version {_tf_version}. DocTR requires version 2 minimum.")
             _tf_available = False
+        else:
+            logging.info(f"TensorFlow version {_tf_version} available.")
 else:
+    logging.info("Disabling Tensorflow because USE_TORCH is set")
     _tf_available = False
 
 
 if not _torch_available and not _tf_available:
     raise ModuleNotFoundError("DocTR requires either TensorFlow or PyTorch to be installed. Please ensure one of them"
                               " is installed and that either USE_TF or USE_TORCH is enabled.")
-
-if _torch_available and not _tf_available:
-    logging.info(f"\n\033[1mDocTR version {__version__}:\tPyTorch {_torch_version} is enabled.\033[0m\n")
-elif _tf_available and not _torch_available:
-    logging.info(f"\n\033[1mDocTR version {__version__}:\tTensorFlow {_tf_version} is enabled.\033[0m\n")
-else:
-    logging.info(f"\n\033[1mDocTR version {__version__}:\tBoth PyTorch and TensorFlow are available.\n "
-                 "\t\t\tWill use TensorFlow by default.\n"
-                 "\t\t\tTo use PyTorch, set `USE_TORCH=1` in your environment variables.\033[0m\n") # noqa: E128, E261
 
 
 def is_torch_available():
