fc40ecdbc129d344ca616f4b514072ce389f2acf
felixdittrich92 <felixdittrich92@gmail.com>
2022-03-16 21:09:59 +0100

    apply changes

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -115,24 +115,25 @@ html_static_path = ['_static']
 epub_exclude_files = ['search.html']
 
 # Add googleanalytics id
-# ref: https://github.com/sphinx-contrib/googleanalytics/blob/master/sphinxcontrib/googleanalytics.py
+# ref: https://github.com/orenhecht/googleanalytics/blob/master/sphinxcontrib/googleanalytics.py
 def add_ga_javascript(app, pagename, templatename, context, doctree):
 
     metatags = context.get('metatags', '')
-    metatags += """<script type="text/javascript">
-      var _gaq = _gaq || [];
-      _gaq.push(['_setAccount', '%s']);
-      _gaq.push(['_trackPageview']);
-      (function() {
-        var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
-        ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
-        var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
-      })();
-    </script>""" % 'G-40DVRMX8T4'
+    metatags += """
+    <!-- Global site tag (gtag.js) - Google Analytics -->
+<script async src="https://www.googletagmanager.com/gtag/js?id={0}"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){{dataLayer.push(arguments);}}
+  gtag('js', new Date());
+  gtag('config', '{0}');
+</script>
+    """.format(app.config.googleanalytics_id)
     context['metatags'] = metatags
 
 
 def setup(app):
+    app.add_config_value('googleanalytics_id', 'G-40DVRMX8T4', 'html')
     app.add_css_file('css/mindee.css')
     app.add_js_file('js/custom.js')
     app.connect('html-page-context', add_ga_javascript)
