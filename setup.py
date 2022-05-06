452d8ef3fb92319c1566d066808998fc81eba96f
felixdittrich92 <felixdittrich92@gmail.com>
2022-05-06 09:06:21 +0200

    check version

--- setup.py
+++ setup.py
@@ -60,7 +60,7 @@ _deps = [
     "tensorflow-addons>=0.13.0",
     "rapidfuzz>=1.6.0",
     "keras<2.7.0",
-    "tf2onnx>=1.9.2",
+    "tf2onnx>=1.8.5",
     "huggingface-hub>=0.4.0",
     # Testing
     "pytest>=5.3.2",
