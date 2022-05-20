b8ce1aa65fd88fe0f6fbaeb00381f615f9573e32
felix <felixdittrich92@gmail.com>
2022-05-20 15:03:03 +0200

    up

--- doctr/models/recognition/sar/pytorch.py
+++ doctr/models/recognition/sar/pytorch.py
@@ -118,7 +118,7 @@ class SARDecoder(nn.Module):
         self,
         features: torch.Tensor,  # (N, C, H, W)
         holistic: torch.Tensor,  # (N, C)
-        gt: Optional[torch.Tensor] = None  # (N, L)
+        gt: Optional[torch.Tensor] = None,  # (N, L)
     ) -> torch.Tensor:
 
         if gt is not None:
