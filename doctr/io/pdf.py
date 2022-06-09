b932632ed38691d6b58fd34401854342e74f0e7b
geisserml <geisserml@gmail.com>
2022-06-09 14:59:03 +0200

    Correct typing and add password docs

--- doctr/io/pdf.py
+++ doctr/io/pdf.py
@@ -4,7 +4,7 @@
 # See LICENSE or go to <https://www.apache.org/licenses/LICENSE-2.0.txt> for full license details.
 
 from pathlib import Path
-from typing import Any, List
+from typing import Any, List, Optional
 
 import numpy as np
 import pypdfium2 as pdfium
@@ -14,7 +14,12 @@ from doctr.utils.common_types import AbstractFile
 __all__ = ['read_pdf']
 
 
-def read_pdf(file: AbstractFile, scale: float = 2, password: str = None, **kwargs: Any) -> List[np.ndarray]:
+def read_pdf(
+        file: AbstractFile,
+        scale: float = 2,
+        password: Optional[str] = None,
+        **kwargs: Any,
+    ) -> List[np.ndarray]:
     """Read a PDF file and convert it into an image in numpy format
 
     >>> from doctr.documents import read_pdf
@@ -23,6 +28,7 @@ def read_pdf(file: AbstractFile, scale: float = 2, password: str = None, **kwarg
     Args:
         file: the path to the PDF file
         scale: rendering scale (1 corresponds to 72dpi)
+        password: a password to unlock the document, if encrypted
         kwargs: additional parameters to :meth:`pypdfium2.PdfDocument.render_topil`
 
     Returns:
