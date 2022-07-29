7d2556306d071b0fcd5b1048dd74010b3a19a9e8
Olivier Dulcy <106678676+odulcy-mindee@users.noreply.github.com>
2022-07-29 10:37:37 +0200

    Fix: docu not reachable in api template docker  (#997)

--- api/tests/conftest.py
+++ api/tests/conftest.py
@@ -3,26 +3,26 @@
 # This program is licensed under the Apache License version 2.
 # See LICENSE or go to <https://www.apache.org/licenses/LICENSE-2.0.txt> for full license details.
 
-import pytest
+import pytest_asyncio
 import requests
 from httpx import AsyncClient
 
 from app.main import app
 
 
-@pytest.fixture(scope="session")
+@pytest_asyncio.fixture(scope="session")
 def mock_recognition_image(tmpdir_factory):
     url = 'https://user-images.githubusercontent.com/76527547/117133599-c073fa00-ada4-11eb-831b-412de4d28341.jpeg'
     return requests.get(url).content
 
 
-@pytest.fixture(scope="session")
+@pytest_asyncio.fixture(scope="session")
 def mock_detection_image(tmpdir_factory):
     url = 'https://user-images.githubusercontent.com/76527547/117319856-fc35bf00-ae8b-11eb-9b51-ca5aba673466.jpg'
     return requests.get(url).content
 
 
-@pytest.fixture(scope="function")
+@pytest_asyncio.fixture(scope="function")
 async def test_app_asyncio():
     # for httpx>=20, follow_redirects=True (cf. https://github.com/encode/httpx/releases/tag/0.20.0)
     async with AsyncClient(app=app, base_url="http://test") as ac:
