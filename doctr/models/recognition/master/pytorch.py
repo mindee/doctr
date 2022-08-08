d0675b4b041feab830574501d17655dd52f6828b
Felix <felixdittrich92@gmail.com>
2022-08-08 09:29:15 +0200

    change import

--- doctr/models/recognition/master/pytorch.py
+++ doctr/models/recognition/master/pytorch.py
@@ -13,8 +13,8 @@ from torchvision.models._utils import IntermediateLayerGetter
 
 from doctr.datasets import VOCABS
 from doctr.models.classification import magc_resnet31
-from doctr.models.modules.transformer.pytorch import Decoder, PositionalEncoding
 
+from ...modules.transformer.pytorch import Decoder, PositionalEncoding
 from ...utils.pytorch import load_pretrained_params
 from .base import _MASTER, _MASTERPostProcessor
 
