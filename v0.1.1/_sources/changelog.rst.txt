1b39b2eb6df01824d1feed71245a6cbfe39f4b43
felixdittrich92 <felixdittrich92@gmail.com>
2022-09-29 12:40:12 +0000

    [skip ci] Documentation updates

--- v0.1.1/_sources/changelog.rst.txt
+++ v0.1.1/_sources/changelog.rst.txt
@@ -1,6 +1,10 @@
 Changelog
 =========
 
+v0.6.0 (2022-09-29)
+-------------------
+Release note: `v0.6.0 <https://github.com/mindee/doctr/releases/tag/v0.6.0>`_
+
 v0.5.1 (2022-03-22)
 -------------------
 Release note: `v0.5.1 <https://github.com/mindee/doctr/releases/tag/v0.5.1>`_
--- v0.4.1/_static/js/custom.js
+++ v0.4.1/_static/js/custom.js
@@ -3,11 +3,12 @@
 
 // These two things need to be updated at each release for the version selector.
 // Last stable version
-const stableVersion = "v0.5.1"
+const stableVersion = "v0.6.0"
 // Dictionary doc folder to label. The last stable version should have an empty key.
 const versionMapping = {
     "latest": "latest",
-    "": "v0.5.1 (stable)",
+    "": "v0.6.0 (stable)",
+    "v0.5.1": "v0.5.1",
     "v0.5.0": "v0.5.0",
     "v0.4.1": "v0.4.1",
     "v0.4.0": "v0.4.0",
