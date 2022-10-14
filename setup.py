75c1cd32e588c3eb1e646c2756c847c735a10843
Amine <amine.sadeq@mindee.co>
2022-10-14 10:00:00 +0200

    Up major version to 1.0.0

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ from pathlib import Path
 from setuptools import setup
 
 PKG_NAME = "python-doctr"
-VERSION = os.getenv("BUILD_VERSION", "0.6.1a0")
+VERSION = os.getenv("BUILD_VERSION", "1.0.1a0")
 
 
 if __name__ == "__main__":
