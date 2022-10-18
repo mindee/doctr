b48a2434c49cb1899aaec0e2947e1e707967246d
Amine <amine.sadeq@mindee.co>
2022-10-18 11:01:44 +0200

    add assert on length of tuple

--- doctr/datasets/datasets/tensorflow.py
+++ doctr/datasets/datasets/tensorflow.py
@@ -26,6 +26,7 @@ class AbstractDataset(_AbstractDataset):
             assert "boxes" in target, "Target should contain 'boxes' key"
             assert "labels" in target, "Target should contain 'labels' key"
         elif isinstance(target, tuple):
+            assert len(target) == 2
             assert isinstance(target[0], str) or isinstance(
                 target[0], np.ndarray
             ), "first element of the tuple should be a string or a numpy array"
