d8991d9067068b1b5268e07a30f03cce6fb64d4d
Amine <amine.sadeq@mindee.co>
2022-10-26 18:04:01 +0200

    sort imports to avoid cyclic imports

--- doctr/__init__.py
+++ doctr/__init__.py
@@ -1,3 +1,3 @@
-from . import datasets, io, models, transforms, utils
+from . import io, datasets, models, transforms, utils
 from .file_utils import is_tf_available, is_torch_available
 from .version import __version__  # noqa: F401
