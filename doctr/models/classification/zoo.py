b095c175fda3322ae7d33cf8dced1627feedb702
charlesmindee <charles@mindee.co>
2023-01-16 15:28:53 +0100

    fix: headers

--- doctr/models/classification/zoo.py
+++ doctr/models/classification/zoo.py
@@ -1,4 +1,4 @@
-# Copyright (C) 2021-2022, Mindee.
+# Copyright (C) 2021-2023, Mindee.
 
 # This program is licensed under the Apache License 2.0.
 # See LICENSE or go to <https://opensource.org/licenses/Apache-2.0> for full license details.
