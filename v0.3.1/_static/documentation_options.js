0d74636f0c3ab0ba3db53bd236ddae6e7981c379
felixdittrich92 <felixdittrich92@gmail.com>
2023-04-20 20:34:01 +0000

    [skip ci] Documentation updates

--- v0.3.1/_static/documentation_options.js
+++ v0.3.1/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '0.7.1a0-git',
+    VERSION: '0.3.1a0-git',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
