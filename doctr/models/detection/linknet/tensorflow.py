9013f61ff744806a661f14a14312e785eb985d81
Olivier Dulcy <106678676+odulcy-mindee@users.noreply.github.com>
2023-06-05 17:16:59 +0200

    feat: :sparkles: TF Detection linknet_resnet50_ckpt (#1211)

--- doctr/models/detection/linknet/tensorflow.py
+++ doctr/models/detection/linknet/tensorflow.py
@@ -45,7 +45,7 @@ default_cfgs: Dict[str, Dict[str, Any]] = {
         "mean": (0.798, 0.785, 0.772),
         "std": (0.264, 0.2749, 0.287),
         "input_shape": (1024, 1024, 3),
-        "url": None,
+        "url": "https://doctr-static.mindee.com/models?id=v0.6.0/linknet_resnet50-cd299262.zip&src=0",
     },
 }
 
