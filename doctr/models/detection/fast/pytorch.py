cd35227dc6318b5f71c3c19810ee7e93fe9a6264
nikokks <playe.nicolas@gmail.com>
2023-09-13 08:26:02 +0200

    [skip ci] correcting some stuff for Fast Torch Model on style and quality tests

--- doctr/models/detection/fast/pytorch.py
+++ doctr/models/detection/fast/pytorch.py
@@ -3,7 +3,6 @@
 # This program is licensed under the Apache License 2.0.
 # See LICENSE or go to <https://opensource.org/licenses/Apache-2.0> for full license details.
 
-import time
 from typing import Any, Callable, Dict, List, Optional
 
 import torch
@@ -40,59 +39,100 @@ default_cfgs: Dict[str, Dict[str, Any]] = {
 
 
 class FAST(nn.Module):
-    def __init__(self, backbone, neck, detection_head):
-        super(FAST, self).__init__()
-        self.backbone = backbone  # okay
-        self.neck = neck  # okay
-        self.det_head = detection_head
+    def __init__(self,
+                 feat_extractor,
+                 bin_thresh: float = 0.1,
+                 head_chans: int = 32,
+                 assume_straight_pages: bool = True,
+                 exportable: bool = False,
+                 cfg: Optional[Dict[str, Any]] = None,
+                 class_names: List[str] = [CLASS_NAME],
+                 ) -> None:
+        super().__init__()
+        self.class_names = class_names
+        num_classes: int = len(self.class_names)
+        self.cfg = cfg
+        self.exportable = exportable
+        self.assume_straight_pages = assume_straight_pages
 
-    def _upsample(self, x, size, scale=1):
-        _, _, H, W = size
-        return F.interpolate(x, size=(H // scale, W // scale), mode="bilinear")
+        self.feat_extractor = feat_extractor
+
+        self.feat_extractor.train()
+
+        self.fpn = FASTNeck()
+        
+        self.classifier = FASTHead()
+
+        self.postprocessor = FastPostProcessor(
+            assume_straight_pages=self.assume_straight_pages, bin_thresh=bin_thresh
+        )
+        
+        # AJUSTER LES INITIALISATION POUR LE MODELE
+        for n, m in self.named_modules():
+            # Don't override the initialization of the backbone
+            if n.startswith("feat_extractor."):
+                continue
+            if isinstance(m, (nn.Conv2d, nn.ConvTranspose2d)):
+                nn.init.kaiming_normal_(m.weight.data, mode="fan_out", nonlinearity="relu")
+                if m.bias is not None:
+                    m.bias.data.zero_()
+            elif isinstance(m, nn.BatchNorm2d):
+                m.weight.data.fill_(1.0)
+                m.bias.data.zero_()
 
     def forward(
-        self, imgs, gt_texts=None, gt_kernels=None, training_masks=None, gt_instances=None, img_metas=None, cfg=None
-    ):
+        self
+        x: torch.Tensor,
+        
+        # MODIFIER LES PARAMETRES CI-DESSOUS POUR LES PARAMETRES PLUS BAS
+        gt_texts=None, gt_kernels=None, training_masks=None, gt_instances=None, img_metas=None, cfg=None
+        
+        target: Optional[List[np.ndarray]] = None,
+        return_model_output: bool = False,
+        return_preds: bool = False,
+    ) -> Dict[str, torch.Tensor]:
         outputs = dict()
 
         if not self.training:
             torch.cuda.synchronize()
-            start = time.time()
 
         # backbone
-        f = self.backbone(imgs)
+        f = self.backbone(x)
 
         if not self.training:
             torch.cuda.synchronize()
-            outputs.update(dict(backbone_time=time.time() - start))
-            start = time.time()
 
         # reduce channel
         f = self.neck(f)
 
         if not self.training:
             torch.cuda.synchronize()
-            outputs.update(dict(neck_time=time.time() - start))
-            start = time.time()
 
         # detection
-        det_out = self.det_head(f)
+        det_out = self.classifier(f)
 
         if not self.training:
             torch.cuda.synchronize()
-            outputs.update(dict(det_head_time=time.time() - start))
 
         if self.training:
-            det_out = self._upsample(det_out, imgs.size(), scale=1)
+            det_out = self._upsample(det_out, x.size(), scale=1)
+            
+            # MODIFEIER SELF.DET_HEAD.LOSS en SELF.LOSS
             det_loss = self.det_head.loss(det_out, gt_texts, gt_kernels, training_masks, gt_instances)
             outputs.update(det_loss)
         else:
-            det_out = self._upsample(det_out, imgs.size(), scale=4)
+            det_out = self._upsample(det_out, x.size(), scale=4)
+            
+            # MODIFIER SELF.DET_HEAD.GET_RESULTS en SELF.GET_RESULTS ou self.postprocessing
             det_res = self.det_head.get_results(det_out, img_metas, cfg, scale=2)
             outputs.update(det_res)
 
         return outputs
 
+    def _upsample(self, x, size, scale=1):
+        _, _, H, W = size
+        return F.interpolate(x, size=(H // scale, W // scale), mode="bilinear")
+        
 
 class FASTHead(nn.Module):
     def __init__(self):
@@ -181,9 +221,11 @@ def _fast(
 ) -> FAST:
     pretrained_backbone = pretrained_backbone and not pretrained
 
-    feat_extractor = backbone_fn(pretrained_backbone)
+    backbone = backbone_fn(pretrained_backbone)
     neck = FASTNeck()
     head = FASTHead()
+    
+    feat_extractor = backbone
 
     if not kwargs.get("class_names", None):
         kwargs["class_names"] = default_cfgs[arch].get("class_names", [CLASS_NAME])
@@ -191,7 +233,7 @@ def _fast(
         kwargs["class_names"] = sorted(kwargs["class_names"])
 
     # Build the model
-    model = FAST(feat_extractor, neck, head, cfg=default_cfgs[arch], **kwargs)
+    model = FAST(feat_extractor=feat_extractor, cfg=default_cfgs[arch], **kwargs)
     # Load pretrained parameters
     if pretrained:
         # The number of class_names is not the same as the number of classes in the pretrained model =>
