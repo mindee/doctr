f197337f4a94cfb202bee87d20bd7b05f2b93eae
HamzaGbada <gbadahamza18@gmail.com>
2023-10-15 22:32:12 +0100

    [UPDATE] wildreceipt img_folder

--- doctr/datasets/wildreceipt.py
+++ doctr/datasets/wildreceipt.py
@@ -27,10 +27,10 @@ class WILDRECEIPT(AbstractDataset):
 
     >>> # NOTE: You need to download/generate the dataset from the repository.
     >>> from doctr.datasets import WILDRECEIPT
-    >>> train_set = WILDRECEIPT(train=True, img_folder="/path/to/wildreceipt/image_files",
+    >>> train_set = WILDRECEIPT(train=True, img_folder="/path/to/wildreceipt/",
     >>>                     label_path="/path/to/wildreceipt/train.txt")
     >>> img, target = train_set[0]
-    >>> test_set = WILDRECEIPT(train=False, img_folder="/path/to/wildreceipt/image_files",
+    >>> test_set = WILDRECEIPT(train=False, img_folder="/path/to/wildreceipt/",
     >>>                    label_path="/path/to/wildreceipt/test.txt")
     >>> img, target = test_set[0]
 
@@ -59,15 +59,12 @@ class WILDRECEIPT(AbstractDataset):
         if not os.path.exists(label_path) or not os.path.exists(img_folder):
             raise FileNotFoundError(f"unable to locate {label_path if not os.path.exists(label_path) else img_folder}")
 
-        tmp_root = os.path.join(self.root, 'wildreceipt/')
+        tmp_root = img_folder
         self.train = train
         np_dtype = np.float32
         self.data: List[Tuple[str, Dict[str, Any]]] = []
 
-        self.filename = "train.txt" if self.train else "test.txt"
-        file_path = os.path.join(tmp_root, self.filename)
-
-        with open(file_path, 'r') as file:
+        with open(label_path, 'r') as file:
             data = file.read()
         # Split the text file into separate JSON strings
         json_strings = data.strip().split('\n')
@@ -75,7 +72,7 @@ class WILDRECEIPT(AbstractDataset):
         _targets = []
         for json_string in json_strings:
             json_data = json.loads(json_string)
-            file_name = json_data['file_name']
+            img_path = json_data['file_name']
             annotations = json_data['annotations']
             for annotation in annotations:
                 coordinates = annotation['box']
@@ -91,37 +88,37 @@ class WILDRECEIPT(AbstractDataset):
                         dtype=np_dtype
                     )
                 else:
-                    box = _convert_xmin_ymin(coordinates)
-                _targets = [(_convert_xmin_ymin(annotation['box']), annotation['text'].lower(), annotation['label'])
-                            for annotation in annotations]
-                if _targets:
-                    box_targets, text_units, labels = zip(*_targets)
+                    box = self._convert_xmin_ymin(coordinates)
+                _targets.append((annotation['text'], box))
+            text_targets, box_targets = zip(*_targets)
 
-                    self.data.append((
-                        file_name,
-                        dict(boxes=np.asarray(box_targets, dtype=int), labels=list(labels),
-                             text_units=list(text_units)),
-                    ))
+            if recognition_task:
+                crops = crop_bboxes_from_image(
+                    img_path=os.path.join(tmp_root, img_path), geoms=np.asarray(box_targets, dtype=int).clip(min=0)
+                )
+                for crop, label in zip(crops, list(text_targets)):
+                    self.data.append((crop, label))
+            else:
+                self.data.append(
+                    (img_path, dict(boxes=np.asarray(box_targets, dtype=int).clip(min=0), labels=list(text_targets)))
+                )
         self.root = tmp_root
 
+    def extra_repr(self) -> str:
+        return f"train={self.train}"
 
-def extra_repr(self) -> str:
-    return f"train={self.train}"
+    def _read_from_folder(self, path: str) -> None:
+        for img_path in glob.glob(os.path.join(path, "*.png")):
+            with open(os.path.join(path, f"{os.path.basename(img_path)[:-4]}.txt"), "r") as f:
+                self.data.append((img_path, f.read()))
 
-
-def _read_from_folder(self, path: str) -> None:
-    for img_path in glob.glob(os.path.join(path, "*.png")):
-        with open(os.path.join(path, f"{os.path.basename(img_path)[:-4]}.txt"), "r") as f:
-            self.data.append((img_path, f.read()))
-
-
-@classmethod
-def _convert_xmin_ymin(box: List) -> List:
-    if len(box) == 4:
-        return box
-    x1, y1, x2, y2, x3, y3, x4, y4 = box
-    x_min = min(x1, x2, x3, x4)
-    x_max = max(x1, x2, x3, x4)
-    y_min = min(y1, y2, y3, y4)
-    y_max = max(y1, y2, y3, y4)
-    return [x_min, y_min, x_max, y_max]
+    @classmethod
+    def _convert_xmin_ymin(box: List) -> List:
+        if len(box) == 4:
+            return box
+        x1, y1, x2, y2, x3, y3, x4, y4 = box
+        x_min = min(x1, x2, x3, x4)
+        x_max = max(x1, x2, x3, x4)
+        y_min = min(y1, y2, y3, y4)
+        y_max = max(y1, y2, y3, y4)
+        return [x_min, y_min, x_max, y_max]
