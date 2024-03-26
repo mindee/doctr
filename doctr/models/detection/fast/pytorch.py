8c85c3654e4ae0a045a990d6f23973bc26d3483c
Olivier Dulcy <106678676+odulcy-mindee@users.noreply.github.com>
2024-03-26 13:27:03 +0100

    feat: :sparkles: PT fast small checkpoint (#1529)

--- doctr/models/detection/fast/pytorch.py
+++ doctr/models/detection/fast/pytorch.py
@@ -32,7 +32,7 @@ default_cfgs: Dict[str, Dict[str, Any]] = {
         "input_shape": (3, 1024, 1024),
         "mean": (0.798, 0.785, 0.772),
         "std": (0.264, 0.2749, 0.287),
-        "url": None,
+        "url": "https://doctr-static.mindee.com/models?id=v0.8.1/fast_small-10952cc1.pt&src=0",
     },
     "fast_base": {
         "input_shape": (3, 1024, 1024),
