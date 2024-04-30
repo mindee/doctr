5c498888578ef9c0a3f54a71c05a18bf6252349f
Olivier Dulcy <olivier.dulcy@mindee.co>
2024-04-30 15:15:51 +0200

    feat: :sparkles: improve mobilenet_v3_page_orientation checkpoint

--- doctr/models/classification/mobilenet/pytorch.py
+++ doctr/models/classification/mobilenet/pytorch.py
@@ -64,7 +64,7 @@ default_cfgs: Dict[str, Dict[str, Any]] = {
         "std": (0.299, 0.296, 0.301),
         "input_shape": (3, 512, 512),
         "classes": [0, -90, 180, 90],
-        "url": "https://doctr-static.mindee.com/models?id=v0.8.1/mobilenet_v3_small_page_orientation-1409045c.pt&src=0",
+        "url": "https://doctr-static.mindee.com/models?id=v0.8.1/mobilenet_v3_small_page_orientation-8e60325c.pt&src=0",
     },
 }
 
