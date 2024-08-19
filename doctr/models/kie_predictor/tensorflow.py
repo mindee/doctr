1a8b82a50be2aaf2d109e6de17ea56770a36aa78
felix <felixdittrich92@gmail.com>
2024-08-19 09:34:46 +0200

    fix straighten pages

--- doctr/models/kie_predictor/tensorflow.py
+++ doctr/models/kie_predictor/tensorflow.py
@@ -99,6 +99,9 @@ class KIEPredictor(NestedObject, _KIEPredictor):
             origin_pages_orientations = None
         if self.straighten_pages:
             pages = self._straighten_pages(pages, seg_maps, general_pages_orientations, origin_pages_orientations)
+            # update page shapes after straightening
+            origin_page_shapes = [page.shape[:2] for page in pages]
+
             # Forward again to get predictions on straight pages
             loc_preds = self.det_predictor(pages, **kwargs)  # type: ignore[assignment]
 
