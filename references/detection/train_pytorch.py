0ded805eca88edf6ec8317931cd4fd6a160d59ee
felix <felixdittrich92@gmail.com>
2025-02-26 16:02:46 +0100

    test only ignore log

--- references/detection/train_pytorch.py
+++ references/detection/train_pytorch.py
@@ -309,7 +309,7 @@ def main(args):
     if rank == 0 and args.test_only:
         pbar.write("Running evaluation")
         val_loss, recall, precision, mean_iou = evaluate(
-            model, val_loader, batch_transforms, val_metric, args, amp=args.amp
+            model, val_loader, batch_transforms, val_metric, amp=args.amp, log=lambda **kwargs: None
         )
         pbar.write(
             f"Validation loss: {val_loss:.6} (Recall: {recall:.2%} | Precision: {precision:.2%} | "
